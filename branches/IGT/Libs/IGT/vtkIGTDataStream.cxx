2b959531b4570adbc00f57fdb0d076a991eed382
nobyhata <nobyhata@3bd1e089-480b-0410-8dfb-8563597acbee>
2007-01-11 15:13:55 +0000

    ENH: vtkIGTDataStream implementation part added
    git-svn-id: http://svn.slicer.org/Slicer4@2201 3bd1e089-480b-0410-8dfb-8563597acbee

--- branches/IGT/Libs/IGT/vtkIGTDataStream.cxx
+++ branches/IGT/Libs/IGT/vtkIGTDataStream.cxx
@@ -1,5 +1,6 @@
 
 #include "vtkIGTDataStream.h"
+#include "vtkIGTMatrixState.h"
 #include "vtkObjectFactory.h"
 
 // Constructors/Destructors
@@ -15,9 +16,9 @@ vtkIGTDataStream* vtkIGTDataStream::New()
   return new vtkIGTDataStream;
 }
 
-vtkIGTDataStream::vtkIGTDataStream ( ) {
-initAttributes();
-}
+vtkIGTDataStream::vtkIGTDataStream(){};
+
+
 
 vtkIGTDataStream::~vtkIGTDataStream ( ) { }
 
@@ -25,103 +26,37 @@ vtkIGTDataStream::~vtkIGTDataStream ( ) { }
 // Methods
 //  
 
-
-// Accessor methods
-//  
-
-
-// Public static attribute accessor methods
-//  
-
-
-// Public attribute accessor methods
-//  
-
-
-// Protected static attribute accessor methods
-//  
-
-
-// Protected attribute accessor methods
-//  
-
-
-// Private static attribute accessor methods
-//  
-
-
-// Private attribute accessor methods
-//  
-
-
-/**
- * Set the value of m_buffer_size
- * @param new_var the new value of m_buffer_size
- */
-void vtkIGTDataStream::setBuffer_size ( int new_var ) {
-  m_buffer_size = new_var;
-}
-
-/**
- * Get the value of m_buffer_size
- * @return the value of m_buffer_size
- */
-int vtkIGTDataStream::getBuffer_size ( ) {
-  return m_buffer_size;
-}
-
-/**
- * Set the value of m_LastInputNum
- * @param new_var the new value of m_LastInputNum
- */
-void vtkIGTDataStream::setLastInputNum ( int new_var ) {
-  m_LastInputNum = new_var;
-}
-
-/**
- * Get the value of m_LastInputNum
- * @return the value of m_LastInputNum
- */
-int vtkIGTDataStream::getLastInputNum ( ) {
-  return m_LastInputNum;
-}
-
-/**
- * Set the value of m_LastInputTime
- * @param new_var the new value of m_LastInputTime
- */
-void vtkIGTDataStream::setLastInputTime ( int new_var ) {
-  m_LastInputTime = new_var;
-}
-
-/**
- * Get the value of m_LastInputTime
- * @return the value of m_LastInputTime
- */
-int vtkIGTDataStream::getLastInputTime ( ) {
-  return m_LastInputTime;
-}
-
-// Other methods
-//  
-
-
-
-
-
-/**
- */
-void vtkIGTDataStream::create_MRML_node ( ) {
-
+void vtkIGTDataStream::Init (int numbuffer ) {
+  buffer_size = numbuffer;
+  stream_device_hash = new void* [DEVICE_MAX_NUM];
 }
 
 
 /**
  */
-void vtkIGTDataStream::update_mrml ( ) {
+int vtkIGTDataStream::register_stream_device ( int stream_type) {
+  
+  int this_device_id = num_registered_device +1;
+  vtkIGTMatrixState *p_matrix;
+  
+  switch (stream_type) {
+    case IGT_MATRIX_STREAM:
+      p_matrix = new vtkIGTMatrixState[buffer_size];
+      stream_device_hash[this_device_id] = (vtkIGTMatrixState*)p_matrix;
+      
+      break;
+      
+    case IGT_IMAGE_STREAM:
+      cout << "" ;
+      break;
+      
+    default:
+      cout << "";
+      return(-1);
+      
+    }
+  
+  return this_device_id;
 
-}
-
-void vtkIGTDataStream::initAttributes ( ) {
 }
 
