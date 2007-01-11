b2b1008ef334335b0eab0dae5dff249ec3f3007e
nobyhata <nobyhata@3bd1e089-480b-0410-8dfb-8563597acbee>
2007-01-11 21:36:27 +0000

    ENH: took out Hata's untable code from IGT/CMakeList.txt
    git-svn-id: http://svn.slicer.org/Slicer4/branches/IGT@2215 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/IGT/vtkIGTMatrixState.cxx
+++ Libs/IGT/vtkIGTMatrixState.cxx
@@ -18,86 +18,27 @@ vtkIGTMatrixState* vtkIGTMatrixState::New()
 }
 
 vtkIGTMatrixState::vtkIGTMatrixState ( ) {
-  initAttributes();
+  Matrix = new vtkMatrix4x4;
+
 }
 
 vtkIGTMatrixState::~vtkIGTMatrixState ( ) { }
 
+
 //  
 // Methods
-//  
 
+void vtkIGTMatrixState::Init() {
 
-// Accessor methods
-//  
+}
 
-
-// Public static attribute accessor methods
-//  
-
-
-// Public attribute accessor methods
-//  
-
-
-// Protected static attribute accessor methods
-//  
-
-
-// Protected attribute accessor methods
-//  
-
-
-// Private static attribute accessor methods
-//  
-
-
-// Private attribute accessor methods
-//  
-
-
-/**
- * Set the value of m_timestamp
- * @param new_var the new value of m_timestamp
- */
 void vtkIGTMatrixState::setTimestamp ( int new_var ) {
   m_timestamp = new_var;
 }
 
-/**
- * Get the value of m_timestamp
- * @return the value of m_timestamp
- */
+
 int vtkIGTMatrixState::getTimestamp ( ) {
   return m_timestamp;
 }
 
-// Other methods
-//  
-
-
-/**
- * obtain registration result from IGTRegistration and copy it to
- * userspace_to_IJK_matrix
- */
-void vtkIGTMatrixState::get_userspace_to_IJK_from_registration ( ) {
-
-}
-
-
-/**
- */
-void vtkIGTMatrixState::get_userspace_to_RAS_from_registration ( ) {
-
-}
-
-
-/**
- */
-void vtkIGTMatrixState::get_RAS_to_IJK_fom_Slicer ( ) {
-
-}
-
-void vtkIGTMatrixState::initAttributes ( ) {
-}
 
--- Libs/IGT/vtkIGTOpenTrackerStream.cxx
+++ Libs/IGT/vtkIGTOpenTrackerStream.cxx
@@ -23,44 +23,90 @@ vtkIGTOpenTrackerStream::vtkIGTOpenTrackerStream ( ) {
 
 vtkIGTOpenTrackerStream::~vtkIGTOpenTrackerStream ( ) { }
 
-//  
-// Methods
-//  
+
+int Initialize_Opentracker(char* configfile) {
 
 
-// Accessor methods
-//  
+
+}
+
+static void callbackF(const Node&,const Event &event, void ata) {
 
 
-// Other methods
-//  
+{
+    float position[3];
+    float orientation[4];
+    float norm[3];
+    float transnorm[3];
+    int j;
+
+    vtkIGTOpenTrackerStream *VOT=(vtkIGTOpenTrackerStream *)data;
+
+    // the original values are in the unit of meters
+    position[0]=(float)(event.getPosition())[0] * 1000.0; 
+    position[1]=(float)(event.getPosition())[1] * 1000.0;
+    position[2]=(float)(event.getPosition())[2] * 1000.0;
+
+    orientation[0]=(float)(event.getOrientation())[0];
+    orientation[1]=(float)(event.getOrientation())[1];
+    orientation[2]=(float)(event.getOrientation())[2];
+    orientation[3]=(float)(event.getOrientation())[3];
+
+    VOT->quaternion2xyz(orientation, norm, transnorm);
 
 
-/**
- */
-void vtkIGTOpenTrackerStream::initi_open_tracker ( ) {
+
+    for (j=0; j<3; j++) {
+        VOT->Matrix->SetElement(j,0,position[j]);
+        VOT->p[j] = position[j];
+    }
+
+
+    for (j=0; j<3; j++) {
+        VOT->Matrix->SetElement(j,1,norm[j]);
+        VOT->n[j] = norm[j];
+    }
+
+    for (j=0; j<3; j++) {
+        VOT->Matrix->SetElement(j,2,transnorm[j]);
+    }
+
+    for (j=0; j<3; j++) {
+        VOT->Matrix->SetElement(j,3,0);
+    }
+
+    for (j=0; j<3; j++) {
+        VOT->Matrix->SetElement(3,j,0);
+    }
+
+    VOT->Matrix->SetElement(3,3,1);
+
 
 }
 
 
-/**
- */
-void vtkIGTOpenTrackerStream::add_data_to_data_stream ( ) {
 
+
+void vtkIGTOpenTrackerStream::quaternion2xyz(float* orientation, float *normal, float *transnormal) 
+{
+    float q0, qx, qy, qz;
+
+    q0 = orientation[3];
+    qx = orientation[0];
+    qy = orientation[1];
+    qz = orientation[2]; 
+
+    transnormal[0] = 1-2*qy*qy-2*qz*qz;
+    transnormal[1] = 2*qx*qy+2*qz*q0;
+    transnormal[2] = 2*qx*qz-2*qy*q0;
+
+    normal[0] = 2*qx*qz+2*qy*q0;
+    normal[1] = 2*qy*qz-2*qx*q0;
+    normal[2] = 1-2*qx*qx-2*qy*qy;
 }
 
 
-/**
- */
-void vtkIGTOpenTrackerStream::callback_function ( ) {
-
-}
 
 
-/**
- */
-void vtkIGTOpenTrackerStream::read_configuration_file ( ) {
-
-}
 
 
