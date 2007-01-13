260e04ae5bcb7a796b91bf9a2e3b92f82cbc51fb
nobyhata <nobyhata@3bd1e089-480b-0410-8dfb-8563597acbee>
2007-01-13 16:25:14 +0000

    ENH: adding Doxygen comment to vtkIGTDataManager
    git-svn-id: http://svn.slicer.org/Slicer4@2241 3bd1e089-480b-0410-8dfb-8563597acbee

--- branches/IGT/Libs/IGT/vtkIGTDataManager.h
+++ branches/IGT/Libs/IGT/vtkIGTDataManager.h
@@ -1,3 +1,7 @@
+// .NAME vtkIGTDataManager - Central registry to provide control and I/O for
+//  trackers and imagers
+// .SECTION Description
+// vtkIGTDataManager registers arbitary number of trackers and imagers, created MRML nodes in the MRML secene. Designed and Coded by Nobuhiko Hata and Haying Liu, Jan 12, 2006 @ NA-MIC All Hands Meeting, Salt Lake City, UT
 
 #ifndef IGTDATAMANAGER_H
 #define IGTDATAMANAGER_H
@@ -16,36 +20,49 @@ class vtkIGTDataManager : public vtkObject
 {
 public:
 
-    // Constructors/Destructors
-    //  Magic lines for vtk and Slicer
+
     static vtkIGTDataManager *New();
     vtkTypeRevisionMacro(vtkIGTDataManager,vtkObject);
     void PrintSelf(ostream& os, vtkIndent indent);
 
+    //Description:
+    // Set MRML scene from the Slicer main routine
     vtkSetObjectMacro(MRMLScene,vtkMRMLScene);
+
+    //Description:
+    // Get MRML scene stored
     vtkGetObjectMacro(MRMLScene,vtkMRMLScene);
 
-    /**
-     * Constructor
-     @ param buffersize: size of buufer (
-     */
+
+
+    //Description:
+    //Contructor
     vtkIGTDataManager();
 
 
-    /**
-     * Empty Destructor
-     */
+    //Description:
+    //Destructor
+
     virtual ~vtkIGTDataManager ( );
 
+    //Description:
+    //
     char *GetMRMLId(int index);
-//    void UpdateMatrixData(int index, vtkIGTMatrixState state);
+
+    //Descripton:
+    //Reigster stream device (trackers and imagers). Parameters: int stram type (IGT_IMAGE_STREAM, IGT_MATRIX_DEVICE) defined in vtkIGTDataStream, vtkIGTDataStream
     void RegisterStreamDevice (int streamType, vtkIGTDataStream* datastream);
 
 protected:
 
+    //Descripton:
+    //Create MRML Node for registered device. It is usually called after RegisterStreamDevice
     void CreateMRMLNode(int streamType);
 
 private:
+
+    //Description.
+    //MRML scene passed from the Module instantiating this class
 
     vtkMRMLScene* MRMLScene;
 
