2fb26589ec83c99155c1bde25208835892275abc
hliu <hliu@3bd1e089-480b-0410-8dfb-8563597acbee>
2007-02-22 18:29:36 +0000

    ENH: moved opentracker stuff from vtkIGTDataManager to vtkIGTOpenTrackerStream.
    git-svn-id: http://svn.slicer.org/Slicer4@2672 3bd1e089-480b-0410-8dfb-8563597acbee

--- branches/IGT/Libs/IGT/vtkIGTDataManager.h
+++ branches/IGT/Libs/IGT/vtkIGTDataManager.h
@@ -7,30 +7,13 @@
 #define IGTDATAMANAGER_H
 
 
-#include <string>
-#include <vector>
-
 #include "vtkIGTWin32Header.h" 
 #include "vtkObject.h"
 #include "vtkMRMLScene.h"
-#include "vtkIGTMatrixState.h"
-#include "vtkIGTDataStream.h"
 
-#include "vtkMatrix4x4.h"
-#include "vtkTransform.h"
 
-#ifdef USE_OPENTRACKER
-#ifdef OT_VERSION_20
-#include "OpenTracker/OpenTracker.h"
-#include "OpenTracker/common/CallbackModule.h"
-#endif
-#ifdef OT_VERSION_13
-#include "OpenTracker.h"
-#include "common/CallbackModule.h"
-#endif
-
-using namespace ot;
-#endif
+#define IGT_MATRIX_STREAM 0
+#define IGT_IMAGE_STREAM 1
 
 
 class VTK_IGT_EXPORT vtkIGTDataManager : public vtkObject
@@ -42,11 +25,6 @@ public:
     vtkTypeRevisionMacro(vtkIGTDataManager,vtkObject);
     void PrintSelf(ostream& os, vtkIndent indent);
 
-    vtkSetMacro(Speed,int);
-    vtkSetMacro(MultiFactor,float);
-
-    vtkSetMacro(StartTimer,int);
-
 
     //Description:
     // Set MRML scene from the Slicer main routine
@@ -56,15 +34,9 @@ public:
     // Get MRML scene stored
     vtkGetObjectMacro(MRMLScene,vtkMRMLScene);
 
-    vtkSetObjectMacro(RegMatrix,vtkMatrix4x4);
-    vtkGetObjectMacro(RegMatrix,vtkMatrix4x4);
-
-    vtkGetObjectMacro(LocatorMatrix,vtkMatrix4x4);
-    vtkGetObjectMacro(LocatorNormalTransform,vtkTransform);
 
     /**
      * Constructor
-     @ param buffersize: size of buufer (
      */
     vtkIGTDataManager();
 
@@ -73,61 +45,18 @@ public:
     //Destructor
     virtual ~vtkIGTDataManager ( );
 
-    //Description:
-    //
-    char *GetMRMLModelId(int index);
 
     //Descripton:
-    //Reigster stream device (trackers and imagers). Parameters: int stram type (IGT_IMAGE_STREAM, IGT_MATRIX_DEVICE) defined in vtkIGTDataStream, vtkIGTDataStream
-    void RegisterStreamDevice (int streamType, vtkIGTDataStream* datastream);
-
-    void Init(char *configFile);
-    void StopPolling();
-    void PollRealtime();
-    void SetLocatorTransforms();
-    void ProcessTimerEvents();
-
-#ifdef USE_OPENTRACKER
-    static void callbackF(const Node&, const Event &event, void *data);
-#endif
-
-#ifdef USE_IGSTK
-    void callbackF(double*, double*);
-#endif
-
+    //Reigster stream.  
+    const char *RegisterStream(int streamType);
 
 
 private:
 
-    int Speed;
-    int StartTimer;
-    float MultiFactor;
-
     //Description.
     //MRML scene passed from the Module instantiating this class
     vtkMRMLScene *MRMLScene;
-    vtkMatrix4x4 *LocatorMatrix;
-    vtkMatrix4x4 *RegMatrix;
-    vtkTransform *LocatorNormalTransform;
-
-//BTX
-    std::vector<vtkIGTDataStream *> RegisteredDataStreams;
-    std::vector<int> StreamTypes;  // matrix or image
-    std::vector<char *> MRMLModelIds;  
-//ETX
-
-#ifdef USE_OPENTRACKER
-    Context *context;
-#endif
-
-    void CreateMRMLNode(int streamType);
-    void Normalize(float *a);
-    void Cross(float *a, float *b, float *c);
-    void ApplyTransform(float *position, float *norm, float *transnorm);
-    void CloseConnection();
-
-    void quaternion2xyz(float* orientation, float *normal, float *transnormal); 
-
+    static int index;
 
 };
 
