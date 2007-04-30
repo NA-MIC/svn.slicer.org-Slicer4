2a2bc078c26cb4d8f2f621d68cd5bbcd2366eddd
hliu <hliu@3bd1e089-480b-0410-8dfb-8563597acbee>
2007-04-30 15:11:49 +0000

    ENH: creating new IGT branch from Slicer3 trunk at revision 3294.
    git-svn-id: http://svn.slicer.org/Slicer4@3295 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ branches/IGT/Libs/MRML/vtkMRMLDiffusionTensorDisplayPropertiesNode.h
@@ -0,0 +1,386 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: ,v $
+  Date:      $Date: 2006/03/19 17:12:29 $
+  Version:   $Revision: 1.3 $
+
+  =========================================================================auto=*/
+// .NAME vtkMRMLDiffusionTensorDisplayPropertiesNode - MRML node for display of a diffusion tensor.
+// .SECTION Description
+// This node describes display properties at the (conceptual) single-tensor level.
+// A tensor can be displayed using various scalar invariants and glyphs.
+// This class is used by classes (vtkMRMLFiberBundleDisplayNode, 
+// vtkMRMLDiffusionTensorVolumeDisplayNode) that handle higher-level display
+// concepts for many diffusion tensors, such as choosing between scalars/glyphs/etc. 
+// for specific display needs.
+// This class inherits from the vtkMRMLColorNode->vtkMRMLColorTableNode superclasses,
+// used for vtkMRMLModelNodes and vtkMRMLVolumeNodes, in order to
+// provide specific lookup tables for the scalar invariant display.
+//
+
+#ifndef __vtkMRMLDiffusionTensorDisplayPropertiesNode_h
+#define __vtkMRMLDiffusionTensorDisplayPropertiesNode_h
+
+#include "vtkMRML.h"
+#include "vtkMRMLColorTableNode.h"
+#include "vtkPolyData.h"
+
+
+class VTK_MRML_EXPORT vtkMRMLDiffusionTensorDisplayPropertiesNode : public vtkMRMLColorTableNode
+{
+ public:
+  static vtkMRMLDiffusionTensorDisplayPropertiesNode *New();
+  vtkTypeMacro(vtkMRMLDiffusionTensorDisplayPropertiesNode,vtkMRMLColorTableNode);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  //--------------------------------------------------------------------------
+  // MRMLNode methods
+  //--------------------------------------------------------------------------
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+
+  // Description:
+  // Read node attributes from a MRML file in XML format.
+  virtual void ReadXMLAttributes( const char** atts);
+
+  // Description:
+  // Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+  // Description:
+  // Copy the node's attributes to this object.
+  virtual void Copy(vtkMRMLNode *node);
+
+  // Description:
+  // Get node XML tag name (like Volume, Model)
+  virtual const char* GetNodeTagName() {return "DiffusionTensorDisplayProperties";};
+
+  //--------------------------------------------------------------------------
+  // Display Information: Types of scalars that may be generated from tensors.
+  //--------------------------------------------------------------------------
+
+  //BTX
+  enum
+  {
+    Trace = 0,
+    Determinant = 1,
+    RelativeAnisotropy = 2,
+    FractionalAnisotropy = 3,
+    MaxEigenvalue = 4,
+    MidEigenvalue = 5,
+    MinEigenvalue = 6,
+    LinearMeasure = 7,
+    PlanarMeasure = 8,
+    SphericalMeasure = 9,
+    ColorOrientation = 10,
+    D11 = 11,
+    D22 = 12,
+    D33 = 13,
+    Mode = 14,
+    ColorMode = 15,
+    MaxEigenvalueProjX = 16,
+    MaxEigenvalueProjY = 17,
+    MaxEigenvalueProjZ = 18,
+    MaxEigenvec_ProjX =  19,
+    MaxEigenvec_ProjY =  20,
+    MaxEigenvec_ProjZ =  21,
+    ParallelDiffusivity =  22,
+    PerpendicularDiffusivity =  23,
+    ColorOrientationMiddleEigenvector = 24,
+    ColorOrientationMinEigenvector = 25,
+  };
+  //ETX
+
+  //--------------------------------------------------------------------------
+  // Display Information: Functions to choose scalar invariant
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Get type of scalar invariant (tensor-derived scalar, invariant to tensor 
+  // rotation) selected for display.
+  vtkGetMacro(ScalarInvariant, int);
+
+  // Description:
+  // Get type of scalar invariant (tensor-derived scalar, invariant to tensor 
+  // rotation) selected for display.
+  vtkSetMacro(ScalarInvariant, int);
+ 
+  // Description:
+  // Set scalar invariant to trace (sum of eigenvalues).
+  void SetScalarInvariantToTrace() {
+    this->SetScalarInvariant(this->Trace);
+  };
+
+  // Description:
+  // Set scalar invariant to FA (normalized variance of eigenvalues)
+  void SetScalarInvariantToFractionalAnisotropy() {
+    this->SetScalarInvariant(this->FractionalAnisotropy);
+  };  
+
+  // Description:
+  // Set scalar invariant to C_L (Westin's linear measure)
+  void SetScalarInvariantToLinearMeasure() {
+    this->SetScalarInvariant(this->LinearMeasure);
+  };  
+
+  // TO DO: add the rest of the scalars
+
+  //--------------------------------------------------------------------------
+  // Display Information: Types of glyph geometry that can be displayed
+  //--------------------------------------------------------------------------
+
+  //BTX
+  enum
+  {
+    Lines = 0,
+    Tubes = 1,
+    Ellipsoids = 2,
+    Superquadrics = 3,
+  };
+  //ETX
+
+  //--------------------------------------------------------------------------
+  // Display Information: Functions to choose the type of glyph geometry
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Get the type of glyph geometry (line, ellipsoid, etc.) 
+  vtkGetMacro(GlyphGeometry, int);
+
+  // Description:
+  // Set the type of glyph geometry (line, ellipsoid, etc.) 
+  vtkSetMacro(GlyphGeometry, int);
+
+  void SetGlyphGeometryToLines() {
+    this->SetGlyphGeometry(this->Lines);
+  };
+  void SetGlyphGeometryToTubes() {
+    this->SetGlyphGeometry(this->Tubes);
+  };
+  void SetGlyphGeometryToEllipsoids() {
+    this->SetGlyphGeometry(this->Ellipsoids);
+  };
+  void SetGlyphGeometryToSuperquadrics() {
+    this->SetGlyphGeometry(this->Superquadrics);
+  };
+
+  //--------------------------------------------------------------------------
+  // Display Information: Parameters of glyph geometry
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Get the scale factor applied to the glyphs.
+  vtkGetMacro(GlyphScaleFactor, double);
+
+  // Description:
+  // Set the scale factor applied to the glyphs.
+  vtkSetMacro(GlyphScaleFactor, double);
+
+  // Description:
+  // Whether the input tensors need eigensystem computation
+  vtkGetMacro(GlyphExtractEigenvalues, int);
+  vtkSetMacro(GlyphExtractEigenvalues, int);
+  vtkBooleanMacro(GlyphExtractEigenvalues, int);
+
+  //--------------------------------------------------------------------------
+  // Display Information: Eigenvector to display for lines/tubes glyphs
+  //--------------------------------------------------------------------------
+
+  //BTX
+  enum
+  {
+    Major = 1,
+    Middle = 2,
+    Minor = 3,
+  };
+  //ETX
+
+  // Description
+  // Which eigenvector to display with lines or tubes glyphs
+  vtkGetMacro(GlyphEigenvector, int);
+
+  // Description
+  // Which eigenvector to display with lines or tubes glyphs
+  vtkSetMacro(GlyphEigenvector, int);
+  
+  // Description
+  // Display major eigenvector with lines or tubes glyphs
+  void SetGlyphEigenvectorToMajor() {
+    this->SetGlyphEigenvector(this->Major);
+  };
+
+  // Description
+  // Display "middle" (second) eigenvector with lines or tubes glyphs
+  void SetGlyphEigenvectorToMiddle() {
+    this->SetGlyphEigenvector(this->Middle);
+  };
+
+  // Description
+  // Display minor eigenvector with lines or tubes glyphs
+  void SetGlyphEigenvectorToMinor() {
+    this->SetGlyphEigenvector(this->Minor);
+  };
+
+  
+  //--------------------------------------------------------------------------
+  // Display Information: Parameters of Lines glyph geometry
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Resolution of lines displayed as tensor glyphs
+  vtkGetMacro(LineGlyphResolution, int);
+  vtkSetMacro(LineGlyphResolution, int);
+
+
+  //--------------------------------------------------------------------------
+  // Display Information: Parameters of Tubes glyph geometry
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Get the radius of the tube glyph
+  vtkGetMacro(TubeGlyphRadius, double);
+
+  // Description:
+  // Set the radius of the tube glyph
+  vtkSetMacro(TubeGlyphRadius, double);
+
+  // Description:
+  // Number of sides of tube glyph (3 gives a triangular tube, etc.)
+  vtkGetMacro(TubeGlyphNumberOfSides, int);
+  vtkSetMacro(TubeGlyphNumberOfSides, int);
+
+  //--------------------------------------------------------------------------
+  // Display Information: Parameters of Ellipsoids glyph geometry
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Number of polygons used in longitude direction for sphere that will
+  // be scaled by tensor to form ellipsoid.
+  vtkGetMacro(EllipsoidGlyphThetaResolution, int);
+  vtkSetMacro(EllipsoidGlyphThetaResolution, int);
+
+  // Description:
+  // Number of polygons used in latitude direction for sphere that will
+  // be scaled by tensor to form ellipsoid.
+  vtkGetMacro(EllipsoidGlyphPhiResolution, int);
+  vtkSetMacro(EllipsoidGlyphPhiResolution, int);
+
+  //--------------------------------------------------------------------------
+  // Display Information: Parameters of Superquadrics glyph geometry
+  //--------------------------------------------------------------------------
+
+  // Description:
+  vtkGetMacro(SuperquadricGlyphGamma, double);
+  vtkSetMacro(SuperquadricGlyphGamma, double);
+
+  // Description:
+  vtkGetMacro(SuperquadricGlyphThetaResolution, int);
+  vtkSetMacro(SuperquadricGlyphThetaResolution, int);
+
+  // Description:
+  vtkGetMacro(SuperquadricGlyphPhiResolution, int);
+  vtkSetMacro(SuperquadricGlyphPhiResolution, int);
+
+  //--------------------------------------------------------------------------
+  // Display Information: Functions to choose the type of glyph coloring
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Get type of scalar invariant (tensor-derived scalar, invariant to tensor 
+  // rotation) selected for display.
+  vtkGetMacro(ColorGlyphBy, int);
+
+  // Description:
+  // Get type of scalar invariant (tensor-derived scalar, invariant to tensor 
+  // rotation) selected for display.
+  vtkSetMacro(ColorGlyphBy, int);
+ 
+  // Description:
+  // Set scalar invariant to trace (sum of eigenvalues).
+  void ColorGlyphByTrace() {
+    this->SetColorGlyphBy(this->Trace);
+  };
+
+  // Description:
+  // Set scalar invariant to FA (normalized variance of eigenvalues)
+  void ColorGlyphByFractionalAnisotropy() {
+    this->SetColorGlyphBy(this->FractionalAnisotropy);
+  };  
+
+  // Description:
+  // Set scalar invariant to FA (normalized variance of eigenvalues)
+  void ColorGlyphByLinearMeasure() {
+    this->SetColorGlyphBy(this->LinearMeasure);
+  };  
+
+  // TO DO: add the rest of the scalars
+
+  //--------------------------------------------------------------------------
+  // Convenience functions to get an appropriate glyph source
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Get a polydata object according to current glyph display settings
+  // (so a line, sphere, or tube) to use as a source for a glyphing filter.
+  vtkPolyData * GetGlyphSource ( );
+
+ protected:
+  vtkMRMLDiffusionTensorDisplayPropertiesNode();
+  ~vtkMRMLDiffusionTensorDisplayPropertiesNode();
+  vtkMRMLDiffusionTensorDisplayPropertiesNode(const vtkMRMLDiffusionTensorDisplayPropertiesNode&);
+  void operator=(const vtkMRMLDiffusionTensorDisplayPropertiesNode&);
+
+  // ---- Parameters that should be written to MRML --- //
+
+  // Scalar display parameters
+  int ScalarInvariant;
+
+  // Glyph general parameters
+  int GlyphGeometry;
+  int ColorGlyphBy;
+  double GlyphScaleFactor;
+  int GlyphEigenvector;
+  int GlyphExtractEigenvalues;
+
+  // Line Glyph parameters
+  int LineGlyphResolution;
+
+  // Tube Glyph parameters
+  double TubeGlyphRadius;
+  int TubeGlyphNumberOfSides;
+
+  // Ellipsoid Glyph parameters
+  int EllipsoidGlyphThetaResolution;
+  int EllipsoidGlyphPhiResolution;
+
+  // Superquadric Glyph parameters
+  double SuperquadricGlyphGamma;
+  int SuperquadricGlyphThetaResolution;
+  int SuperquadricGlyphPhiResolution;
+
+  // ---- End of parameters that should be written to MRML --- //
+
+ 
+  // ---- VTK objects for display --- //
+  vtkPolyData * GlyphSource;
+
+  // This is used internally to set a pointer to this polydata
+  // and reference count it.  
+  // TO DO: is this causing an extra modified event?
+  vtkSetObjectMacro( GlyphSource, vtkPolyData );
+
+  // TO DO: add specific lookup tables ranging from 0..1 for or -1 1
+  // for scalar invariants with those ranges
+
+  // TO DO: read/write MRML for all parameters
+
+};
+
+#endif
+
---
+++ branches/IGT/Libs/MRML/vtkMRMLFiberBundleDisplayNode.h
@@ -0,0 +1,368 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLFiberBundleDisplayNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:28 $
+  Version:   $Revision: 1.6 $
+
+  =========================================================================auto=*/
+// .NAME vtkMRMLFiberBundleDisplayNode - MRML node to represent display properties for tractography.
+// .SECTION Description
+// vtkMRMLFiberBundleDisplayNode nodes store display properties of trajectories 
+// from tractography in diffusion MRI data, including color type (by bundle, by fiber, 
+// or by scalar invariants), display on/off for tensor glyphs and display of 
+// trajectory as a line or tube.
+//
+
+#ifndef __vtkMRMLFiberBundleDisplayNode_h
+#define __vtkMRMLFiberBundleDisplayNode_h
+
+#include <string>
+
+#include "vtkMRML.h"
+#include "vtkMRMLModelDisplayNode.h"
+#include "vtkMRMLDiffusionTensorDisplayPropertiesNode.h"
+
+class VTK_MRML_EXPORT vtkMRMLFiberBundleDisplayNode : public vtkMRMLModelDisplayNode
+{
+ public:
+  static vtkMRMLFiberBundleDisplayNode *New (  );
+  vtkTypeMacro ( vtkMRMLFiberBundleDisplayNode,vtkMRMLModelDisplayNode );
+  void PrintSelf ( ostream& os, vtkIndent indent );
+  
+  //--------------------------------------------------------------------------
+  // MRMLNode methods
+  //--------------------------------------------------------------------------
+
+  virtual vtkMRMLNode* CreateNodeInstance (  );
+
+  // Description:
+  // Read node attributes from XML (MRML) file
+  virtual void ReadXMLAttributes ( const char** atts );
+
+  // Description:
+  // Write this node's information to a MRML file in XML format.
+  virtual void WriteXML ( ostream& of, int indent );
+
+
+  // Description:
+  // Copy the node's attributes to this object
+  virtual void Copy ( vtkMRMLNode *node );
+  
+  // Description:
+  // Get node XML tag name (like Volume, FiberBundle)
+  virtual const char* GetNodeTagName ( ) {return "FiberBundleDisplay";};
+
+  // Description:
+  // Updates this node if it depends on other nodes 
+  // when the node is deleted in the scene
+  virtual void UpdateReferences();
+
+  // Description:
+  // Finds the storage node and read the data
+  virtual void UpdateScene(vtkMRMLScene *scene);
+
+  // Description:
+  // Update the stored reference to another node in the scene
+  virtual void UpdateReferenceID(const char *oldID, const char *newID);
+
+  // Description:
+  // alternative method to propagate events generated in Display nodes
+  virtual void ProcessMRMLEvents ( vtkObject * /*caller*/, 
+                                   unsigned long /*event*/, 
+                                   void * /*callData*/ );
+
+  //--------------------------------------------------------------------------
+  // Display Information: Geometry to display (not mutually exclusive)
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Turn on/off visibility of trajectory (fiber) geometry.
+  vtkSetMacro ( FiberLineVisibility, int );
+  vtkGetMacro ( FiberLineVisibility, int );
+  vtkBooleanMacro ( FiberLineVisibility, int );
+  
+  // Description:
+  // Turn on/off visibility of tubed trajectory ("thick fibers") geometry.
+  vtkSetMacro ( FiberTubeVisibility , int );
+  vtkGetMacro ( FiberTubeVisibility , int );
+  vtkBooleanMacro ( FiberTubeVisibility , int );
+  
+  // Description:
+  // Turn on/off visibility of glyphs (tensors) along fibers.
+  vtkSetMacro ( FiberGlyphVisibility , int );
+  vtkGetMacro ( FiberGlyphVisibility , int );
+  vtkBooleanMacro ( FiberGlyphVisibility , int );
+
+    // Description:
+  // Turn on/off visibility of glyphs (tensors) along fibers.
+  vtkSetMacro ( TwoDimensionalVisibility , int );
+  vtkGetMacro ( TwoDimensionalVisibility , int );
+  vtkBooleanMacro ( TwoDimensionalVisibility , int );
+
+  //--------------------------------------------------------------------------
+  // Display Information: Color Mode
+  // 0) solid color by group 1) color by scalar invariant 
+  // 2) color by avg scalar invariant 3) color by other
+  //--------------------------------------------------------------------------
+
+  //BTX
+  enum
+  {
+    colorModeSolid = 0,
+    colorModeScalar = 1,
+    colorModeFunctionOfScalar = 2,
+    colorModeUseCellScalars = 3
+  };
+  //ETX
+
+  //--------------------------------------------------------------------------
+  // Display Information: ColorMode for lines
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Color mode for lines. The color modes are mutually exclusive.
+  vtkGetMacro ( ColorModeForFiberLines, int );
+  vtkSetMacro ( ColorModeForFiberLines, int );
+ 
+  // Description:
+  // Color by solid color (for example the whole fiber bundle red. blue, etc.)
+  void SetColorModeForFiberLinesToSolid ( ) {
+    this->SetColorModeForFiberLines ( this->colorModeSolid );
+  };
+
+  // Description:
+  // Color according to the tensors using various scalar invariants.
+  void SetColorModeForFiberLinesToScalar ( ) {
+    this->SetColorModeForFiberLines ( this->colorModeScalar );
+  };
+
+  // Description:
+  // Color according to the tensors using a function of scalar invariants along the tract.
+  // This enables coloring by average FA, for example.
+  void SetColorModeForFiberLinesToFunctionOfScalar ( ) {
+    this->SetColorModeForFiberLines ( this->colorModeFunctionOfScalar );
+  };
+
+  // Description:
+  // Use to color by the active cell scalars.  This is intended to support
+  // external processing of fibers, for example to label each with the distance
+  // of that fiber from an fMRI activation.  Then by making that information
+  // the active cell scalar field, this will allow coloring by that information.
+  // TO DO: make sure this information can be saved with the tract, save name of
+  // active scalar field if needed.
+  void SetColorModeForFiberLinesToUseCellScalars ( ) {
+    this->SetColorModeForFiberLines ( this->colorModeUseCellScalars );
+  };
+
+  //--------------------------------------------------------------------------
+  // Display Information: line properties
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Opacity of line trajectory
+  vtkSetMacro ( FiberLineOpacity , double );
+  vtkGetMacro ( FiberLineOpacity , double );
+
+  //--------------------------------------------------------------------------
+  // Display Information: ColorMode for tubes
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Color mode for tubes. The color modes are mutually exclusive.
+  vtkGetMacro ( ColorModeForFiberTubes, int );
+  vtkSetMacro ( ColorModeForFiberTubes, int );
+ 
+  // Description:
+  // Color by solid color (for example the whole fiber bundle red. blue, etc.)
+  void SetColorModeForFiberTubesToSolid ( ) {
+    this->SetColorModeForFiberTubes ( this->colorModeSolid );
+  };
+
+  // Description:
+  // Color according to the tensors using various scalar invariants.
+  void SetColorModeForFiberTubesToScalar ( ) {
+    this->SetColorModeForFiberTubes ( this->colorModeScalar );
+  };
+
+  // Description:
+  // Color according to the tensors using a function of scalar invariants along the tract.
+  // This enables coloring by average FA, for example.
+  void SetColorModeForFiberTubesToFunctionOfScalar ( ) {
+    this->SetColorModeForFiberTubes ( this->colorModeFunctionOfScalar );
+  };
+
+  // Description:
+  // Use to color by the active cell scalars.  This is intended to support
+  // external processing of fibers, for example to label each with the distance
+  // of that fiber from an fMRI activation.  Then by making that information
+  // the active cell scalar field, this will allow coloring by that information.
+  // TO DO: make sure this information can be saved with the tract, save name of
+  // active scalar field if needed.
+  void SetColorModeForFiberTubesToUseCellScalars ( ) {
+    this->SetColorModeForFiberTubes ( this->colorModeUseCellScalars );
+  };
+
+
+  //--------------------------------------------------------------------------
+  // Display Information: tube properties
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Opacity of tubed trajectory
+  vtkSetMacro ( FiberTubeOpacity , double );
+  vtkGetMacro ( FiberTubeOpacity , double );
+
+  // Description:
+  // Thickness (radius) of tubed trajectory ("thick fibers") geometry.
+  vtkSetMacro ( FiberTubeRadius , double );
+  vtkGetMacro ( FiberTubeRadius , double );
+
+  // Description:
+  // Resolution (number of sides) of tubed trajectory ("thick fibers") geometry.
+  vtkSetMacro ( FiberTubeNumberOfSides , int );
+  vtkGetMacro ( FiberTubeNumberOfSides , int );
+
+  //--------------------------------------------------------------------------
+  // Display Information: ColorMode for glyphs
+  //--------------------------------------------------------------------------
+  
+  // Description:
+  // Color mode for glyphs. The color modes are mutually exclusive.
+  vtkGetMacro ( ColorModeForFiberGlyphs, int );
+  vtkSetMacro ( ColorModeForFiberGlyphs, int );
+ 
+  // Description:
+  // Color by solid color (for example the whole fiber bundle red. blue, etc.)
+  void SetColorModeForFiberGlyphsToSolid ( ) {
+    this->SetColorModeForFiberGlyphs ( this->colorModeSolid );
+  };
+
+  // Description:
+  // Color according to the tensors using various scalar invariants.
+  void SetColorModeForFiberGlyphsToScalar ( ) {
+    this->SetColorModeForFiberGlyphs ( this->colorModeScalar );
+  };
+
+  // Description:
+  // Color according to the tensors using a function of scalar invariants along the tract.
+  // This enables coloring by average FA, for example.
+  void SetColorModeForFiberGlyphsToFunctionOfScalar ( ) {
+    this->SetColorModeForFiberGlyphs ( this->colorModeFunctionOfScalar );
+  };
+
+  // Description:
+  // Use to color by the active cell scalars.  This is intended to support
+  // external processing of fibers, for example to label each with the distance
+  // of that fiber from an fMRI activation.  Then by making that information
+  // the active cell scalar field, this will allow coloring by that information.
+  // TO DO: make sure this information can be saved with the tract, save name of
+  // active scalar field if needed.
+  void SetColorModeForFiberGlyphsToUseCellScalars ( ) {
+    this->SetColorModeForFiberGlyphs ( this->colorModeUseCellScalars );
+  };
+
+
+  //--------------------------------------------------------------------------
+  // Display Information: glyph properties
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Opacity of glyphs
+  vtkSetMacro ( FiberGlyphOpacity , double );
+  vtkGetMacro ( FiberGlyphOpacity , double );
+
+
+  //--------------------------------------------------------------------------
+  // MRML nodes that are observed
+  //--------------------------------------------------------------------------
+  
+  // Description:
+  // Get diffusion tensor display MRML object for fiber line.
+  vtkMRMLDiffusionTensorDisplayPropertiesNode* GetFiberLineDTDisplayPropertiesNode ( );
+
+  // Description:
+  // Set diffusion tensor display MRML object for fiber line.
+  void SetAndObserveFiberLineDTDisplayPropertiesNodeID ( const char *ID );
+
+  // Description:
+  // Get ID of diffusion tensor display MRML object for fiber line.
+  vtkGetStringMacro(FiberLineDTDisplayPropertiesNodeID);
+
+  // Description:
+  // Get diffusion tensor display MRML object for fiber tube.
+  vtkMRMLDiffusionTensorDisplayPropertiesNode* GetFiberTubeDTDisplayPropertiesNode ( );
+
+  // Description:
+  // Set diffusion tensor display MRML object for fiber tube.
+  void SetAndObserveFiberTubeDTDisplayPropertiesNodeID ( const char *ID );
+
+  // Description:
+  // Get ID of diffusion tensor display MRML object for fiber tube.
+  vtkGetStringMacro(FiberTubeDTDisplayPropertiesNodeID);
+
+  // Description:
+  // Get diffusion tensor display MRML object for fiber glyph.
+  vtkMRMLDiffusionTensorDisplayPropertiesNode* GetFiberGlyphDTDisplayPropertiesNode ( );
+
+  // Description:
+  // Set diffusion tensor display MRML object for fiber glyph.
+  void SetAndObserveFiberGlyphDTDisplayPropertiesNodeID ( const char *ID );
+
+  // Description:
+  // Get ID of diffusion tensor display MRML object for fiber glyph.
+  vtkGetStringMacro(FiberGlyphDTDisplayPropertiesNodeID);
+
+  // TO DO: Add updating of reference IDs
+
+ protected:
+  vtkMRMLFiberBundleDisplayNode ( );
+  ~vtkMRMLFiberBundleDisplayNode ( );
+  vtkMRMLFiberBundleDisplayNode ( const vtkMRMLFiberBundleDisplayNode& );
+  void operator= ( const vtkMRMLFiberBundleDisplayNode& );
+
+  // MRML nodes 
+  vtkMRMLDiffusionTensorDisplayPropertiesNode *FiberLineDTDisplayPropertiesNode;
+  vtkMRMLDiffusionTensorDisplayPropertiesNode *FiberTubeDTDisplayPropertiesNode;
+  vtkMRMLDiffusionTensorDisplayPropertiesNode *FiberGlyphDTDisplayPropertiesNode;
+
+  char *FiberLineDTDisplayPropertiesNodeID;
+  char *FiberTubeDTDisplayPropertiesNodeID;
+  char *FiberGlyphDTDisplayPropertiesNodeID;
+
+  vtkSetReferenceStringMacro(FiberLineDTDisplayPropertiesNodeID);
+  vtkSetReferenceStringMacro(FiberTubeDTDisplayPropertiesNodeID);
+  vtkSetReferenceStringMacro(FiberGlyphDTDisplayPropertiesNodeID);
+
+  // Numbers
+  double FiberLineOpacity;
+  double FiberTubeOpacity;
+  double FiberGlyphOpacity;
+
+  double FiberTubeRadius;
+  int FiberTubeNumberOfSides;
+  
+  // Enumerated
+  int ColorModeForFiberLines;
+  int ColorModeForFiberTubes;
+  int ColorModeForFiberGlyphs;
+
+  // Booleans
+  int FiberLineVisibility;
+  int FiberTubeVisibility;
+  int FiberGlyphVisibility;
+
+  int TwoDimensionalVisibility;
+
+
+  // Arrays
+  //double ScalarRange[2];
+
+};
+
+#endif
