4969c0264fbaaa0567747e09f77e9b8121beff93
nobyhata <nobyhata@3bd1e089-480b-0410-8dfb-8563597acbee>
2007-07-17 18:54:23 +0000

    ENH: Robot communication protocol is defined
    git-svn-id: http://svn.slicer.org/Slicer4@3858 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ branches/IGT/Modules/BrpNav/BRPTPRInterface.h
@@ -0,0 +1,26 @@
+// BRPTPRInterface.h version 1.0: July 17, 2007 2:42pm
+ 
+// Workflow values: "workphase" attribute
+#define BRPTPR_START_UP     "START_UP"
+#define BRPTPR_PLANNING     "PLANNING"
+#define BRPTPR_CALIBRATION  "CALIBRATION"
+#define BRPTPR_TARGETTING   "TARGETTING"
+#define BRPTPR_MANUAL       "MANUAL"
+#define BRPTPR_EMERGENCY    "EMERGENCY"
+ 
+// Command values: "command" attribute
+#define BRPTPR_ZFRAME       "ZFRAME"
+#define BRPTPR_TARGET       "TARGET"
+#define BRPTPR_POSITION     "POSITION"
+ 
+// Status values: "status" attribute
+#define BRPTPR_Idle         "Idle"
+#define BRPTPR_Initializing "Initializing"
+#define BRPTPR_Uncalibrated "Uncalibrated"
+#define BRPTPR_Ready        "Ready"
+#define BRPTPR_Moving       "Moving"
+#define BRPTPR_Manual       "Manual"
+#define BRPTPR_Error        "Error"
+#define BRPTPR_EStop        "EStop"
+
+
