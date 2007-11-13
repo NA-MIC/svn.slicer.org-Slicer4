a6b80d60f6209264acf994caf8087932a37a2cd4
freudling <freudling@3bd1e089-480b-0410-8dfb-8563597acbee>
2007-11-13 22:04:16 +0000

    ENH: Initial import before feature freeze
    git-svn-id: http://svn.slicer.org/Slicer4@4819 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ branches/andy_volume_rendering/vtkSlicerLabelmapTree.cxx
@@ -0,0 +1,125 @@
+#include "vtkSlicerLabelmapTree.h"
+#include "vtkObjectFactory.h"
+#include "vtkMRMLColorNode.h"
+#include "vtkKWTree.h"
+#include "vtkSlicerLabelmapElement.h"
+#include "vtkTimerLog.h"
+#include "vtkKWLabel.h"
+#include "vtkImageAccumulate.h"
+#include "vtkKWHistogram.h"
+#include "vtkMRMLScalarVolumeNode.h"
+#include "vtkPointData.h"
+#include "vtkCommand.h"
+#include "vtkLabelMapPiecewiseFunction.h"
+#include <sstream>
+#include <vector>
+vtkCxxRevisionMacro(vtkSlicerLabelmapTree, "$Revision: 0.1 $");
+vtkStandardNewMacro(vtkSlicerLabelmapTree);
+vtkSlicerLabelmapTree::vtkSlicerLabelmapTree(void)
+{
+    this->InChangeOpacityAll=0;
+    this->PiecewiseFunction=NULL;
+    this->Node=NULL;
+    this->StepSize=20;
+    //this->NumberOfSteps=6;
+    std::vector<vtkSlicerLabelmapElement*> Elements;
+}
+
+vtkSlicerLabelmapTree::~vtkSlicerLabelmapTree(void)
+{
+    for(unsigned int i=0;i<this->Elements.size();i++)
+    {
+        this->Elements[i]->RemoveObservers(vtkCommand::AnyEvent);
+        
+        this->Elements[i]->Delete();
+        this->Elements[i]=NULL;
+    }
+    this->Elements.clear();
+}
+void vtkSlicerLabelmapTree::CreateWidget(void)
+{
+    Superclass::CreateWidget();
+    vtkKWTree *tree=this->GetWidget();
+    tree->RedrawOnIdleOn();
+    tree->SelectionFillOn();
+    tree->SetDeltaY(30);
+}
+
+void vtkSlicerLabelmapTree::Init(vtkMRMLScalarVolumeNode *node,vtkLabelMapPiecewiseFunction *piecewiseFunction)
+{
+    this->Node=node;
+    this->PiecewiseFunction=piecewiseFunction;
+    vtkLookupTable *lookup=this->Node->GetVolumeDisplayNode()->GetColorNode()->GetLookupTable();
+    vtkTimerLog *timer=vtkTimerLog::New();
+
+    vtkKWHistogram *histo=vtkKWHistogram::New();
+    histo->BuildHistogram(node->GetImageData()->GetPointData()->GetScalars(),0);
+    timer->StartTimer();
+    int counter=0;
+    //detect the max size of color Name
+    int max=0;
+    for(int i=lookup->GetTableRange()[0];i<lookup->GetTableRange()[1];i++)
+    {
+        std::string colorName=this->Node->GetVolumeDisplayNode()->GetColorNode()->GetColorName(i);
+        int tmp=colorName.length();
+        if(tmp>max)
+        {
+            max=tmp;
+        }
+    }
+    for(int i=lookup->GetTableRange()[0];i<lookup->GetTableRange()[1];i++)
+    {
+        if(histo->GetOccurenceAtValue(i)>1)
+        {
+            std::stringstream streamA;
+            streamA<<i;
+            this->GetWidget()->AddNode("",streamA.str().c_str(),"");
+
+            vtkSlicerLabelmapElement *element=vtkSlicerLabelmapElement::New();
+            this->Elements.push_back(element);
+
+            element->SetParent(this->GetWidget());
+            element->Create();
+            double rgb[3];
+            lookup->GetColor(i,rgb);
+            //CalculateOpacities
+            int opacityLevel=piecewiseFunction->GetLabel(i)*this->StepSize;
+            if(opacityLevel>5)
+            {
+                opacityLevel=5;
+                    vtkWarningMacro("OpacityLevelIsOutOfRange");
+            }
+            element->Init(i,this->Node->GetVolumeDisplayNode()->GetColorNode()->GetColorName(i),rgb,opacityLevel,max);
+            this->Script("pack %s -side left -anchor c -expand y",element->GetWidgetName());
+            element->AddObserver(vtkCommand::AnyEvent,(vtkCommand *) this->BaseTreeCallbackCommand);
+            this->GetWidget()->SetNodeWindow(streamA.str().c_str(),element);
+            counter++;
+        }
+
+    }
+    timer->StopTimer();
+    vtkErrorMacro("Elapsed time:"<<timer->GetElapsedTime());
+    vtkErrorMacro("NumberOfElements:"<<counter);
+    histo->Delete();
+    timer->Delete();
+
+}
+void vtkSlicerLabelmapTree::ProcessBaseTreeEvents(vtkObject *caller, unsigned long eid, void *callData)
+{
+    int *callDataInt=(int*)callData;
+    this->PiecewiseFunction->EditLabel(callDataInt[0],callDataInt[1]/(double)this->StepSize);
+        if(this->InChangeOpacityAll==0)
+    {
+        this->InvokeEvent(vtkSlicerLabelmapTree::SingleLabelEdited);
+    }
+}
+void vtkSlicerLabelmapTree::ChangeAllOpacities(int stage)
+{
+    this->InChangeOpacityAll=1;
+    for(unsigned int i=0;i<this->Elements.size();i++)
+    {   
+        this->Elements[i]->ChangeOpacity(stage);
+
+    }
+    this->InChangeOpacityAll=0;
+}
