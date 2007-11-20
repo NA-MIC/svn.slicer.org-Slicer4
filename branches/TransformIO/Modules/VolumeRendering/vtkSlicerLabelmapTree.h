02c84af91eb68900d46ae2e987a5663ccabc9122
millerjv <millerjv@3bd1e089-480b-0410-8dfb-8563597acbee>
2007-11-20 20:55:59 +0000

    ENH: new branch for sending/returning transforms to registration modules
    git-svn-id: http://svn.slicer.org/Slicer4@4897 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ branches/TransformIO/Modules/VolumeRendering/vtkSlicerLabelmapTree.h
@@ -0,0 +1,71 @@
+#ifndef __vtkSlicerLabelmapTree_h
+#define __vtkSlicerLabelmapTree_h
+#include "vtkSlicerBaseTree.h"
+#include "vtkVolumeRenderingModule.h"
+#include <vector>
+#include <string>
+
+class vtkSlicerLabelmapElement;
+class vtkMRMLScalarVolumeNode;
+class vtkLabelMapPiecewiseFunction;
+class vtkMRMLVolumeRenderingNode;
+
+class VTK_VOLUMERENDERINGMODULE_EXPORT vtkSlicerLabelmapTree : public vtkSlicerBaseTree
+{
+public:
+    static vtkSlicerLabelmapTree *New();
+    vtkTypeRevisionMacro(vtkSlicerLabelmapTree,vtkSlicerBaseTree);
+    void Init(vtkMRMLScalarVolumeNode *scalarnode,vtkMRMLVolumeRenderingNode *vrnode);
+    void UpdateGuiElements(void);
+    virtual void CreateWidget(void);
+    void ChangeAllOpacities(int stage);
+    void UpdateVolumeRenderingNode(vtkMRMLVolumeRenderingNode *vrNode)
+      {
+          if(this->VolumeRenderingNode!=NULL)
+          {
+              this->VolumeRenderingNode=vrNode;
+          }
+          else
+          {
+              vtkErrorMacro("Init has to be used before Update is called");
+          }
+      }
+    //BTX
+    enum
+    {
+        SingleLabelEdited = 12000,
+    };
+    //ETX
+
+protected:
+    // Description:
+    // Use ::New() to get a new instance.
+    vtkSlicerLabelmapTree(void);
+
+    // Description:
+    // Use ->Delete() to delete object
+    ~vtkSlicerLabelmapTree(void);
+
+    //BTX
+    //Description:
+    //What was the RecentColorNodeID, (do we have to change colors and labels and range?!)
+    std::string RecentColorNodeID;
+    //ETX
+    int StepSize;
+    int InChangeOpacityAll;
+    vtkMRMLVolumeRenderingNode *VolumeRenderingNode;
+    vtkMRMLScalarVolumeNode *ScalarVolumeNode;
+    //BTX
+    std::vector<vtkSlicerLabelmapElement*> Elements;
+    //ETX
+
+
+    virtual void ProcessBaseTreeEvents(vtkObject *caller, unsigned long eid, void *callData);
+
+private:
+    // Description:
+    // Caution: Not implemented
+    vtkSlicerLabelmapTree(const vtkSlicerLabelmapTree&);//not implemented
+    void operator=(const vtkSlicerLabelmapTree&);//not implemented
+};
+#endif 
