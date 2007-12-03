c2bdf7e5adc1df6f9ca989f3b8e62cc56bf331a9
tokuda <tokuda@3bd1e089-480b-0410-8dfb-8563597acbee>
2007-12-03 17:11:13 +0000

    ENH: Start implementing generic data transfer for OpenTracker stream.
    git-svn-id: http://svn.slicer.org/Slicer4/branches/IGT@5024 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/IGT/vtkIGTOpenTrackerStream.cxx
+++ Libs/IGT/vtkIGTOpenTrackerStream.cxx
@@ -27,8 +27,8 @@ vtkIGTOpenTrackerStream::~vtkIGTOpenTrackerStream()
     {
         delete this->context;
     }
-
 }
+
 
 void vtkIGTOpenTrackerStream::Init(const char *configFile)
 {
@@ -43,7 +43,16 @@ void vtkIGTOpenTrackerStream::Init(const char *configFile)
     // sets the callback function
     // if we use NaviTrack (not opentracker), use this function
     // callbackMod->setCallback( "cb1", (OTCallbackFunction*)&callbackF ,this);    
-    callbackMod->setCallback( "cb1", (OTCallbackFunction*)&callbackF ,this);    
+    callbackMod->setCallback( "cb1", (OTCallbackFunction*)&callbackF ,this);
+
+    // Add additional callbacks
+    vtkIGTMessageAttributeSet::AttributeSetMap::iterator iter;
+    for (iter = AttributeSetMap.begin(); iter != AttributeSetMap.end(); iter ++)
+      {
+      const char* cbname                 = iter->first.c_str();
+      vtkIGTMessageAttributeSet* attrSet = iter->second;
+      callbackMod->setCallback(cbname, (OTCallbackFunction*)&GenericCallback, (void*)attrSet);
+      }
 
     context->start();
 }
@@ -83,7 +92,6 @@ void vtkIGTOpenTrackerStream::callbackF(const Node&, const Event &event, void *d
         VOT->LocatorMatrix->SetElement(j,0,position[j]);
     }
 
-
     for (j=0; j<3; j++) {
         VOT->LocatorMatrix->SetElement(j,1,norm[j]);
     }
@@ -103,6 +111,182 @@ void vtkIGTOpenTrackerStream::callbackF(const Node&, const Event &event, void *d
     VOT->LocatorMatrix->SetElement(3,3,1);
 }
 
+
+void vtkIGTOpenTrackerStream::GenericCallback(const Node &node, const Event &event, void *data)
+{
+
+  if (!data)
+    {
+    return;
+    }
+
+  vtkIGTMessageAttributeSet* attrSet = (vtkIGTMessageAttributeSet*)data;
+  
+  // GET attributes
+  //vtkIGTMessageAttributeSet::AttributeMapType& attrMap = attrSet->GetAttributeMap();
+  //vtkIGTMessageAttributeSet::AttributeMapType::iterator iter;
+  vtkIGTMessageAttributeSet::iterator iter;
+
+  for (iter = attrSet->begin(); iter != attrSet->end(); iter ++)
+    {
+    std::string key = iter->first;
+    vtkIGTMessageAttributeBase* attr = iter->second;
+
+    if (event.hasAttribute(key))
+      {
+
+      //========== Macro for switch(attr->GetTypeID()) {} ==========
+      #define CASE_GETATTRIB_TYPE(TYPE_ID, TYPE)       \
+        case TYPE_ID:                                         \
+          {                                                   \
+          TYPE data = (TYPE)event.getAttribute<TYPE>((TYPE*)NULL, key); \
+          attr->SetAttribute(&data);                          \
+          }                                                   \
+        break;
+      //============================================================
+      std::vector<float> fvec;
+      switch(attr->GetTypeID())
+        {
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_BOOL,           bool               );//(bool*)          false);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_CHAR,           char               );//(char*)          0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_SIGNED_CHAR,    signed char        );//(signed char*)   0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_UNSIGNED_CHAR,  unsigned char      );//(unsigned char*) 0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_INT,            int                );//(int*)           0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_LONG,           long               );//(long*)          0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_SHORT,          short              );//(short*)         0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_UNSIGNED_INT,   unsigned int       );//(unsigned int*)  0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_UNSIGNED_LONG,  unsigned long      );//(unsigned long*) 0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_UNSIGNED_SHORT, unsigned short     );//(unsigned short*)0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_DOUBLE,         double             );//(double*)        0.0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_LONG_DOUBLE,    long double        );//(long double*)   0.0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_FLOAT,          float              );//(float*)         0.0);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_STRING,         std::string        );//(char*)NULL);
+        CASE_GETATTRIB_TYPE(vtkIGTMessageAttributeSet::TYPE_VECTOR_FLOAT,   std::vector<float> );//fvec);
+        case vtkIGTMessageAttributeSet::TYPE_VTK_IMAGE_DATA:// vtkImageData
+          {
+          }
+          break;
+
+        /*
+        case vtkIGTMessageAttributeSet::TYPE_BOOL:          // bool
+          {
+          bool data = (bool)event.getAttribute(key, false);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_CHAR:          // char
+          {
+          char data = (char)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_SIGNED_CHAR:   // signed char
+          {
+          signed char data = (signed char)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_UNSIGNED_CHAR: // unsigned char
+          {
+          unsigned char data = (unsigned char)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_INT:           // int
+          {
+          int data = (int)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_LONG:          // long
+          {
+          long data = (long)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_SHORT:         // short
+          {
+          short data = (short)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_UNSIGNED_INT:  // unsigned int
+          {
+          unsigned int data = (unsigned int)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_UNSIGNED_LONG: // unsigned long
+          {
+          unsigned long data = (unsigned long)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_UNSIGNED_SHORT:// unsigned short
+          {
+          unsigned short data = (unsigned short)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_DOUBLE:        // double
+          {
+          double data = (double)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_LONG_DOUBLE:   // long double
+          {
+          long double data = (long double)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_FLOAT:         // float
+          {
+          float data = (float)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_STRING:        // std::string
+          {
+          std::string data = (std::string)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_VECTOR_FLOAT:  // std::vector<float>
+          {
+          std::vector<float> data = (std::vector<float>)event.getAttribute(key, 0);
+          attr->SetAttribute(&data);
+          }
+          break;
+        case vtkIGTMessageAttributeSet::TYPE_VTK_IMAGE_DATA:// vtkImageData
+          {
+          }
+          break;
+        */
+        default:
+          break;
+        }
+
+      }
+    }
+  
+  vtkIGTMessageAttributeSet::MessageHandlingFunction* handler = attrSet->GetHandlerFunction();
+  if (handler)
+    {
+    handler(attrSet);
+    }
+
+}
+
+
+void vtkIGTOpenTrackerStream::AddCallback(const char* cbname,
+                                          vtkIGTMessageAttributeSet::MessageHandlingFunction* func,
+                                          vtkIGTMessageAttributeSet* attrSet)
+{
+    attrSet->SetOpenTrackerStream(this);
+    attrSet->SetHandlerFunction(func);
+    this->AttributeSetMap[cbname] = attrSet;
+}
 
 
 void vtkIGTOpenTrackerStream::StopPulling()
--- Libs/IGT/vtkIGTOpenTrackerStream.h
+++ Libs/IGT/vtkIGTOpenTrackerStream.h
@@ -1,21 +1,22 @@
 // .NAME vtkIGTOpenTrackerStream - Central registry to provide control and I/O for
 //  trackers and imagers
 // .SECTION Description
-// vtkIGTOpenTrackerStream registers arbitary number of trackers and imagers, created MRML nodes in the MRML secene. Designed and Coded by Nobuhiko Hata and Haiying Liu, Jan 12, 2007 @ NA-MIC All Hands Meeting, Salt Lake City, UT
+// vtkIGTOpenTrackerStream registers arbitary number of trackers and imagers, created MRML nodes in the MRML secene.
+// Designed and Coded by Nobuhiko Hata and Haiying Liu, Jan 12, 2007 @ NA-MIC All Hands Meeting, Salt Lake City, UT
 
 #ifndef IGTOPENTRACKERSTREAM_H
 #define IGTOPENTRACKERSTREAM_H
 
-
 #include "vtkIGTWin32Header.h" 
 #include "vtkIGTDataStream.h"
 
+#include "vtkIGTMessageAttributeSet.h"
+
 #include "OpenTracker/OpenTracker.h"
 #include "OpenTracker/common/CallbackModule.h"
-
+#include <map>
 
 using namespace ot;
-
 
 class VTK_IGT_EXPORT vtkIGTOpenTrackerStream : public vtkIGTDataStream
 {
@@ -37,6 +38,16 @@ public:
 
     void Init(const char *configFile);
     static void callbackF(const Node&, const Event &event, void *data);
+    static void GenericCallback(const Node &node, const Event &event, void *data);
+ 
+    // Register callback functions which were called from GenericCallback().
+    // AddCallback() should be called before Init();
+    //BTX
+    void AddCallback(const char* cbname,
+                     vtkIGTMessageAttributeSet::MessageHandlingFunction* func,
+                     vtkIGTMessageAttributeSet* attrSet);
+    //ETX
+
     void StopPulling();
     void PullRealTime();    
 
@@ -46,7 +57,11 @@ private:
     Context *context;
 
     void CloseConnection();
-
+    //BTX
+    vtkIGTMessageAttributeSet::AttributeSetMap     AttributeSetMap;
+    friend class vtkIGTMessageAttributeSet;
+    //ETX
+    
 };
 
 #endif // IGTOPENTRACKERSTREAM_H
