8db84956f14dfa707252cb546c60655e67735b18
bensch <bensch@3bd1e089-480b-0410-8dfb-8563597acbee>
2007-12-12 16:52:05 +0000

    ENH: minor implementation of the Cuda Module Gui
    git-svn-id: http://svn.slicer.org/Slicer4/branches/cuda@5174 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/VolumeRenderingCuda/vtkVolumeRenderingCudaModuleGUI.cxx
+++ Modules/VolumeRenderingCuda/vtkVolumeRenderingCudaModuleGUI.cxx
@@ -1,32 +1,145 @@
-
 #include "vtkVolumeRenderingCudaModuleGUI.h"
+#include "vtkVolumeRenderingCudaModuleLogic.h"
+#include "vtkSlicerApplication.h"
+#include "vtkKWWidget.h"
+#include "vtkSlicerNodeSelectorWidget.h"
+#include "vtkSlicerModuleCollapsibleFrame.h"
+#include "vtkMRMLScene.h"
+
+vtkVolumeRenderingCudaModuleGUI::vtkVolumeRenderingCudaModuleGUI()
+{
+}
+vtkVolumeRenderingCudaModuleGUI::~vtkVolumeRenderingCudaModuleGUI()
+{
+}
 
 vtkVolumeRenderingCudaModuleGUI* vtkVolumeRenderingCudaModuleGUI::New()
 {
-    ///HACK
+    vtkObject* ret = vtkObjectFactory::CreateInstance("vtkVolumeRenderingCudaModuleGUI");
+    if (ret)
+        return (vtkVolumeRenderingCudaModuleGUI*)ret;
+    // If the Factory was unable to create the object, we do it ourselfes.
     return new vtkVolumeRenderingCudaModuleGUI();
 }
 
 
 void vtkVolumeRenderingCudaModuleGUI::BuildGUI ( )
 {
+    vtkSlicerApplication *app = (vtkSlicerApplication *)this->GetApplication();
+    this->GetUIPanel()->AddPage("VolumeRenderingCuda","VolumeRenderingCuda",NULL);
+
+    // Define your help text and build the help frame here.
+    const char *help = "VolumeRenderingCuda. 3D Segmentation This module is currently a prototype and will be under active development throughout 3DSlicer's Beta release.";
+    const char *about = "This work is supported by NA-MIC, NAC, BIRN, NCIGT, and the Slicer Community. See http://www.slicer.org for details.";
+    vtkKWWidget *page = this->UIPanel->GetPageWidget ( "VolumeRenderingCuda" );
+    this->BuildHelpAndAboutFrame ( page, help, about );
+    //
+    //Load and save
+    //
+    vtkSlicerModuleCollapsibleFrame *loadSaveDataFrame = vtkSlicerModuleCollapsibleFrame::New ( );
+    loadSaveDataFrame->SetParent (page);
+    loadSaveDataFrame->Create();
+    loadSaveDataFrame->ExpandFrame();
+    loadSaveDataFrame->SetLabelText("Load and Save");
+    app->Script ( "pack %s -side top -anchor nw -fill x -padx 2 -pady 2 -in %s",
+        loadSaveDataFrame->GetWidgetName(), this->UIPanel->GetPageWidget("VolumeRendering")->GetWidgetName());
+
+    ////Testing Pushbutton
+    //this->PB_Testing= vtkKWPushButton::New();
+    //this->PB_Testing->SetParent(loadSaveDataFrame->GetFrame());
+    //this->PB_Testing->Create();
+    //this->PB_Testing->SetText("Make All Models Invisible");
+    //app->Script("pack %s -side top -anchor e -padx 2 -pady 2",this->PB_Testing->GetWidgetName());
+
+
+    ////NodeSelector  for Node from MRML Scene
+    //this->NS_ImageData=vtkSlicerNodeSelectorWidget::New();
+    //this->NS_ImageData->SetParent(loadSaveDataFrame->GetFrame());
+    //this->NS_ImageData->Create();
+    //this->NS_ImageData->NoneEnabledOn();
+    //this->NS_ImageData->SetLabelText("Source Volume: ");
+    //this->NS_ImageData->SetNodeClass("vtkMRMLScalarVolumeNode","LabelMap","0","");
+    //app->Script("pack %s -side top -anchor e -padx 2 -pady 2",this->NS_ImageData->GetWidgetName());
+
+    ////NodeSelector for VolumeRenderingNode Preset
+    //this->NS_VolumeRenderingDataSlicer=vtkSlicerNodeSelectorVolumeRenderingWidget::New();
+    //this->NS_VolumeRenderingDataSlicer->SetParent(loadSaveDataFrame->GetFrame());
+    //this->NS_VolumeRenderingDataSlicer->Create();
+    //this->NS_VolumeRenderingDataSlicer->SetLabelText("Use Existing Visualization Parameterset: ");
+    //this->NS_VolumeRenderingDataSlicer->EnabledOff();//By default off
+    //this->NS_VolumeRenderingDataSlicer->NoneEnabledOn();
+    //this->NS_VolumeRenderingDataSlicer->SetNodeClass("vtkMRMLVolumeRenderingNode","","","");
+    //app->Script("pack %s -side top -anchor e -padx 2 -pady 2",this->NS_VolumeRenderingDataSlicer->GetWidgetName());
+
+    ////NodeSelector for VolumeRenderingNode Scene
+    //this->NS_VolumeRenderingDataScene=vtkSlicerNodeSelectorVolumeRenderingWidget::New();
+    //this->NS_VolumeRenderingDataScene->SetParent(loadSaveDataFrame->GetFrame());
+    //this->NS_VolumeRenderingDataScene->Create();
+    //this->NS_VolumeRenderingDataScene->NoneEnabledOn();
+    //this->NS_VolumeRenderingDataScene->SetLabelText("Current Visualization Parameterset: ");
+    //this->NS_VolumeRenderingDataScene->EnabledOff();//By default off
+    //this->NS_VolumeRenderingDataScene->SetNodeClass("vtkMRMLVolumeRenderingNode","","","");
+    //app->Script("pack %s -side top -anchor e -padx 2 -pady 2",this->NS_VolumeRenderingDataScene->GetWidgetName());
+    ////Missing: Load from file
+
+    ////Create New Volume Rendering Node
+    ////Entry With Label
+    //this->EWL_CreateNewVolumeRenderingNode=vtkKWEntryWithLabel::New();
+    //this->EWL_CreateNewVolumeRenderingNode->SetParent(loadSaveDataFrame->GetFrame());
+    //this->EWL_CreateNewVolumeRenderingNode->Create();
+    //this->EWL_CreateNewVolumeRenderingNode->SetLabelText("Name for Visualization Parameterset: ");
+    //this->EWL_CreateNewVolumeRenderingNode->EnabledOff();
+    //app->Script("pack %s -side top -anchor e -padx 2 -pady 2", this->EWL_CreateNewVolumeRenderingNode->GetWidgetName());
+
+
+    //this->PB_CreateNewVolumeRenderingNode=vtkKWPushButton::New();
+    //this->PB_CreateNewVolumeRenderingNode->SetParent(loadSaveDataFrame->GetFrame());
+    //this->PB_CreateNewVolumeRenderingNode->Create();
+    //this->PB_CreateNewVolumeRenderingNode->SetText("Create Visualization Parameterset");
+    //app->Script("pack %s -side top -anchor e -padx 2 -pady 2",this->PB_CreateNewVolumeRenderingNode->GetWidgetName());
+
+    ////Details frame
+    //this->detailsFrame = vtkSlicerModuleCollapsibleFrame::New ( );
+    //this->detailsFrame->SetParent (this->UIPanel->GetPageWidget("VolumeRendering"));
+    //this->detailsFrame->Create();
+    //this->detailsFrame->ExpandFrame();
+    //this->detailsFrame->SetLabelText("Details");
+    //app->Script ( "pack %s -side top -anchor nw -fill x -padx 2 -pady 2 -in %s",
+    //    this->detailsFrame->GetWidgetName(), this->UIPanel->GetPageWidget("VolumeRendering")->GetWidgetName());
+
+
+    ////set subnodes
+    ////Delete frames
+    //if ( this->GetApplicationGUI() &&  this->GetApplicationGUI()->GetMRMLScene())
+    //{
+    //    this->GetApplicationGUI()->GetMRMLScene()->AddObserver( vtkMRMLScene::SceneCloseEvent, this->MRMLCallbackCommand );
+    //}
+    //loadSaveDataFrame->Delete();
+    //this->Built=true;
 }
 
 void vtkVolumeRenderingCudaModuleGUI::TearDownGUI ( )
 {
+    this->Exit();
+    if ( this->Built )
+    {
+        this->RemoveGUIObservers();
+    }
 }
 
 void vtkVolumeRenderingCudaModuleGUI::CreateModuleEventBindings ( )
 {
+    vtkDebugMacro("VolumeRenderingCudaModule: CreateModuleEventBindings: No ModuleEventBindings yet");
 }
 void vtkVolumeRenderingCudaModuleGUI::ReleaseModuleEventBindings ( )
 {
+    vtkDebugMacro("VolumeRenderingCudaModule: ReleaseModuleEventBindings: No ModuleEventBindings to remove yet");
 }
 
 void vtkVolumeRenderingCudaModuleGUI::AddGUIObservers ( )
 {
 }
-  
+
 void vtkVolumeRenderingCudaModuleGUI::RemoveGUIObservers ( )
 {
 }
@@ -38,15 +151,15 @@ void vtkVolumeRenderingCudaModuleGUI::RemoveLogicObservers ( )
 }
 
 void vtkVolumeRenderingCudaModuleGUI::ProcessGUIEvents ( vtkObject *caller, unsigned long event,
-                                  void *callData )
+                                                        void *callData )
 {
 }
-  
+
 void vtkVolumeRenderingCudaModuleGUI::ProcessMRMLEvents ( vtkObject *caller, unsigned long event,
-                                   void *callData)
+                                                         void *callData)
 {
 }
-  
+
 
 void vtkVolumeRenderingCudaModuleGUI::SetViewerWidget(vtkSlicerViewerWidget *viewerWidget)
 {
@@ -55,23 +168,24 @@ void vtkVolumeRenderingCudaModuleGUI::SetInteractorStyle(vtkSlicerViewerInteract
 {
 }
 
-  
+
 void vtkVolumeRenderingCudaModuleGUI::Enter ( )
 {
+    vtkDebugMacro("Enter Volume Rendering Cuda Module");
 }
 void vtkVolumeRenderingCudaModuleGUI::Exit ( )
 {
-}
-  
-  
-void vtkVolumeRenderingCudaModuleGUI::PrintSelf(ostream& os, vtkIndent indent)
-{
-}
-  
-vtkVolumeRenderingCudaModuleGUI::vtkVolumeRenderingCudaModuleGUI()
-{
-}
-vtkVolumeRenderingCudaModuleGUI::~vtkVolumeRenderingCudaModuleGUI()
-{
+    vtkDebugMacro("Exit: removeObservers for VolumeRenderingModule");
+    this->ReleaseModuleEventBindings();
 }
 
+
+void vtkVolumeRenderingCudaModuleGUI::PrintSelf(ostream& os, vtkIndent indent)
+{
+    os<<indent<<"vtkVolumeRenderingCudaModuleGUI"<<endl;
+    os<<indent<<"vtkVolumeRenderingCudaModuleLogic"<<endl;
+    if(this->GetLogic())
+    {
+        this->GetLogic()->PrintSelf(os,indent.GetNextIndent());
+    }
+}
