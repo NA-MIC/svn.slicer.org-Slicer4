fee2c89de9b761442d752871583e663c62ac7f24
bensch <bensch@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-01-18 22:15:53 +0000

    ENH: just some teplatization to show the use of templates in VTK style
    git-svn-id: http://svn.slicer.org/Slicer4/branches/cuda@5545 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/VolumeRenderingCuda/CUDA_renderAlgo.cu
+++ Modules/VolumeRenderingCuda/CUDA_renderAlgo.cu
@@ -29,7 +29,8 @@ __constant__ float c_renderAlgo_disp[3];
 
 // Do hybrid rendering (combination between ray tracing volume rendering and MIP rendering). Here, rendering parameter was set to (1.0-transparencyLevel) MIP and (transparencyLevel) ray tracing volume rendering. Transparency level 1 means fully oblique, and transparency level 0 means fully transparent.
 
-__global__ void CUDAkernel_renderAlgo_doHybridRender(unsigned char* d_sourceData, 
+template <typename T>
+__global__ void CUDAkernel_renderAlgo_doHybridRender(T* d_sourceData, 
                                                      unsigned char minThreshold, 
                                                      unsigned char maxThreshold, 
                                                      int sliceDistance, 
@@ -242,7 +243,8 @@ __global__ void CUDAkernel_renderAlgo_doHybridRender(unsigned char* d_sourceData
 }
 
 void CUDArenderAlgo_doRender(uchar4* outputData,
-                             unsigned char* renderData,
+                             void* renderData,
+                             int inputDataType,
                              float* rotationMatrix, 
                              float* color, float* minmax, float* lightVec, 
                              int sizeX, int sizeY, int sizeZ, 
@@ -298,7 +300,8 @@ void CUDArenderAlgo_doRender(uchar4* outputData,
 
   float transparencyLevel = 1.0;
 
-  CUDAkernel_renderAlgo_doHybridRender<<< grid, threads >>>(renderData, minThreshold, maxThreshold, sliceDistance, transparencyLevel, outputData);
+  if (inputDataType == 1)
+  CUDAkernel_renderAlgo_doHybridRender<<< grid, threads >>>((unsigned char*)renderData, minThreshold, maxThreshold, sliceDistance, transparencyLevel, outputData);
   
   CUT_CHECK_ERROR("Kernel execution failed");
 
