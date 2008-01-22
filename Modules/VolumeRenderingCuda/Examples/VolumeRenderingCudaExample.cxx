2e2d2d13f9bf9eac8b43935488f2234d35cd6aa8
bensch <bensch@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-01-22 20:31:28 +0000

    ENH: Better Example
    git-svn-id: http://svn.slicer.org/Slicer4/branches/cuda@5570 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/VolumeRenderingCuda/Examples/VolumeRenderingCudaExample.cxx
+++ Modules/VolumeRenderingCuda/Examples/VolumeRenderingCudaExample.cxx
@@ -1,8 +1,15 @@
-
+#include "vtkActor.h"
+#include "vtkKWApplication.h"
+#include "vtkKWFrameWithLabel.h"
+#include "vtkKWRenderWidget.h"
+#include "vtkKWWindow.h"
 #include "vtkRenderWindow.h"
-#include "vtkRenderer.h"
-#include "vtkRenderWindowInteractor.h"
-#include "vtkCamera.h"
+#include "vtkKWSimpleAnimationWidget.h"
+
+
+#include <vtksys/SystemTools.hxx>
+#include <vtksys/CommandLineArguments.hxx>
+
 
 #include "vtkVolumeCudaMapper.h"
 #include "vtkImageReader.h"
@@ -10,22 +17,67 @@
 #include "vtkVolume.h"
 #include "vtkVolumeProperty.h"
 
-#include "vtkActor.h"
-#include "vtkCamera.h"
-#include "vtkCellArray.h"
-#include "vtkFloatArray.h"
-#include "vtkPointData.h"
-#include "vtkPoints.h"
-#include "vtkPolyData.h"
-#include "vtkPolyDataMapper.h"
-#include "vtkRenderWindow.h"
-#include "vtkRenderWindowInteractor.h"
-#include "vtkRenderer.h"
 
-
-
-int main(int argc, char** argv)
+int my_main(int argc, char *argv[])
 {
+    // Initialize Tcl
+
+    Tcl_Interp *interp = vtkKWApplication::InitializeTcl(argc, argv, &cerr);
+    if (!interp)
+    {
+        cerr << "Error: InitializeTcl failed" << endl ;
+        return 1;
+    }
+
+    // Process some command-line arguments
+    // The --test option here is used to run this example as a non-interactive 
+    // test for software quality purposes. You can ignore it.
+
+    int option_test = 0;
+    vtksys::CommandLineArguments args;
+    args.Initialize(argc, argv);
+    args.AddArgument(
+        "--test", vtksys::CommandLineArguments::NO_ARGUMENT, &option_test, "");
+    args.Parse();
+
+    // Create the application
+    // If --test was provided, ignore all registry settings, and exit silently
+    // Restore the settings that have been saved to the registry, like
+    // the geometry of the user interface so far.
+
+    vtkKWApplication *app = vtkKWApplication::New();
+    app->SetName("KWPolygonalObjectViewerExample");
+    if (option_test)
+    {
+        app->SetRegistryLevel(0);
+        app->PromptBeforeExitOff();
+    }
+    app->RestoreApplicationSettingsFromRegistry();
+
+    // Set a help link. Can be a remote link (URL), or a local file
+
+    app->SetHelpDialogStartingPage("http://www.kwwidgets.org");
+
+    // Add a window
+    // Set 'SupportHelp' to automatically add a menu entry for the help link
+
+    vtkKWWindow *win = vtkKWWindow::New();
+    win->SupportHelpOn();
+    app->AddWindow(win);
+    win->Create();
+    win->SecondaryPanelVisibilityOff();
+
+    // Add a render widget, attach it to the view frame, and pack
+
+    vtkKWRenderWidget *rw = vtkKWRenderWidget::New();
+    rw->SetParent(win->GetViewFrame());
+    rw->Create();
+
+    app->Script("pack %s -expand y -fill both -anchor c -expand y", 
+        rw->GetWidgetName());
+
+    // Create the mapper and actor
+
 
     vtkVolume* volume = vtkVolume::New();
     vtkVolumeCudaMapper* volumeMapper = vtkVolumeCudaMapper::New();
@@ -57,40 +109,80 @@ int main(int argc, char** argv)
     volume->SetProperty(prop);
     prop->Delete();
 
+    rw->AddViewProp(volume);
 
 
+    // Add the actor to the scene
+    rw->ResetCamera();
 
-    vtkRenderer *renderer = vtkRenderer::New();
-    vtkRenderWindow *renWin = vtkRenderWindow::New();
-    renWin->AddRenderer(renderer);
+    // Create a material property editor
 
-    renderer->AddVolume(volume);
-    vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
-    iren->SetRenderWindow(renWin);
+    // Create a simple animation widget
 
-    vtkCamera *camera = vtkCamera::New();
-    camera->SetPosition(1,1,1);
-    camera->SetFocalPoint(0,0,0);
+    vtkKWFrameWithLabel *animation_frame = vtkKWFrameWithLabel::New();
+    animation_frame->SetParent(win->GetMainPanelFrame());
+    animation_frame->Create();
+    animation_frame->SetLabelText("Movie Creator");
 
-    renderer->SetActiveCamera(camera);
-    renderer->ResetCamera();
-    renderer->SetBackground(1,1,1);
+    app->Script("pack %s -side top -anchor nw -expand n -fill x -pady 2",
+        animation_frame->GetWidgetName());
 
-    renWin->SetSize(300,300);
+    vtkKWSimpleAnimationWidget *animation_widget = 
+        vtkKWSimpleAnimationWidget::New();
+    animation_widget->SetParent(animation_frame->GetFrame());
+    animation_widget->Create();
+    animation_widget->SetRenderWidget(rw);
+    animation_widget->SetAnimationTypeToCamera();
 
-    // interact with data
-    renWin->Render();
-    iren->Start();
+    app->Script("pack %s -side top -anchor nw -expand n -fill x",
+        animation_widget->GetWidgetName());
 
-    // Clean up
+    // Start the application
+    // If --test was provided, do not enter the event loop and run this example
+    // as a non-interactive test for software quality purposes.
+
+    int ret = 0;
+    win->Display();
+    if (!option_test)
+    {
+        app->Start(argc, argv);
+        ret = app->GetExitStatus();
+    }
+    win->Close();
+
+    // Deallocate and exit
+
+
     volume->Delete();
     volumeMapper->Delete();
-
-    renderer->Delete();
-    renWin->Delete();
-    iren->Delete();
-
     for (unsigned int i = 0; i < 5; i++)
         reader[i]->Delete();
-    return 0;
+
+    animation_frame->Delete();
+    animation_widget->Delete();
+    rw->Delete();
+    win->Delete();
+    app->Delete();
+
+    return ret;
 }
+
+/*#if defined(_WIN32) && !defined(__CYGWIN__)
+#include <windows.h>
+int __stdcall WinMain(HINSTANCE, HINSTANCE, LPSTR lpCmdLine, int)
+{
+int argc;
+char **argv;
+vtksys::SystemTools::ConvertWindowsCommandLineToUnixArguments(
+lpCmdLine, &argc, &argv);
+int ret = my_main(argc, argv);
+for (int i = 0; i < argc; i++) { delete [] argv[i]; }
+delete [] argv;
+return ret;
+}
+#else */
+int main(int argc, char *argv[])
+{
+    return my_main(argc, argv);
+}
+//#endif
