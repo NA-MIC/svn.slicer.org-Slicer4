72c8da479e5bd631781e72d7bbce0aefd2990d2c
tokuda <tokuda@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-01-24 21:02:49 +0000

    ENH: OpenIGTLink protocol has been partially implemented.
    git-svn-id: http://svn.slicer.org/Slicer4@5589 3bd1e089-480b-0410-8dfb-8563597acbee

--- branches/IGT/Modules/OpenIGTLinkDaemon/vtkOpenIGTLinkTclHelper.h
+++ branches/IGT/Modules/OpenIGTLinkDaemon/vtkOpenIGTLinkTclHelper.h
@@ -6,12 +6,12 @@
   or http://www.slicer.org/copyright/copyright.txt for details.
 
   Program:   3D Slicer
-  Module:    $RCSfile: vtkTclHelper.h,v $
+  Module:    $RCSfile: vtkOpenIGTLinkTclHelper.h,v $
   Date:      $Date: 2006/01/06 17:58:01 $
   Version:   $Revision: 1.4 $
 
 =========================================================================auto=*/
-// .NAME vtkTclHelper - a bridge between tcl and vtk
+// .NAME vtkOpenIGTLinkTclHelper - a bridge between tcl and vtk
 // -- this class allows binary data to be read from a tcl channel 
 //    and put into a vtk data structure (vtkImageData for now)
 
@@ -21,60 +21,76 @@
 // vtkTkRenderWidget 
 
 
-#ifndef __vtkTclHelper_h
-#define __vtkTclHelper_h
+#ifndef __vtkOpenIGTLinkTclHelper_h
+#define __vtkOpenIGTLinkTclHelper_h
 
 #include "vtkTcl.h"
 #include "vtkObject.h"
+#include "vtkMRMLScene.h"
 #include "vtkImageData.h"
 #include "vtkMRMLVolumeNode.h"
 #include "vtkMatrix4x4.h"
-#include <vtkOpenIGTLinkDaemonWin32Header.h>
+#include "vtkOpenIGTLinkDaemonWin32Header.h"
+#include "vtkSlicerApplicationLogic.h"
 
+//typedef stract {
+//  char deviceName[20];
+//  long long crc;       // 64 bit CRC
+//  long long bodySize;  // 64 bit size of the body
+//} OpenIGTLinkHeader;
 
-class VTK_SLICERDAEMON_EXPORT vtkTclHelper : public vtkObject
+class VTK_OPENIGTLINKDAEMON_EXPORT vtkOpenIGTLinkTclHelper : public vtkObject
 {
+
 public:
     void PrintSelf(ostream& os, vtkIndent indent);
-    vtkTypeRevisionMacro(vtkTclHelper, vtkObject);
 
-    static vtkTclHelper *New();
-
-    void SetInterpFromCommand(unsigned long tag);
+    vtkTypeRevisionMacro(vtkOpenIGTLinkTclHelper, vtkObject);
 
     vtkSetObjectMacro(ImageData, vtkImageData);
     vtkGetObjectMacro(ImageData, vtkImageData);
-    vtkSetObjectMacro(VolumeNode, vtkMRMLVolumeNode);
-    vtkGetObjectMacro(VolumeNode, vtkMRMLVolumeNode);
-    vtkSetObjectMacro(MeasurementFrame, vtkMatrix4x4);
-    vtkGetObjectMacro(MeasurementFrame, vtkMatrix4x4);
     vtkSetObjectMacro(Matrix, vtkMatrix4x4);
     vtkGetObjectMacro(Matrix, vtkMatrix4x4);
+    //vtkGetObjectMacro(Scene, vtkMRMLScene);
+    //vtkSetObjectMacro(Scene, vtkMRMLScene);
+    void SetScene(vtkMRMLScene* s)
+    {
+      this->Scene = s;
+    }
+    
+    void SetAppLogic(vtkSlicerApplicationLogic* al)
+    {
+      this->AppLogic = al;
+    }
+      
+    static vtkOpenIGTLinkTclHelper *New();
 
-    void GetRASToVTKMatrix(vtkMatrix4x4 *RASToVTK);
+    void SetInterpFromCommand(unsigned long tag);
+
     void SendImageDataScalars(char *sockname);
-    void SendImageDataTensors(char *sockname);
-    void SendImageDataTensors_UndoSlicerTransform(char *sockname);
-    void ReceiveImageDataScalars(char *sockname);
-    void ReceiveImageDataTensors(char *sockname);
-    void ReceiveImageDataTensors_DoSlicerTransform(char *sockname);
-    const char *Execute (char *Command);
 
+    void OnReceiveOpenIGTLinkMessage(char *sockname);
     void ReceiveMatrix(char *sockname);
     void SendMessage(char *sockname);
+
+    void ReceiveImage(Tcl_Channel channel, char* deviceName, long long size, long long crc, int newNode);
+    void ReceiveTracker(Tcl_Channel channel, char* deviceName, long long size, long long crc, int newNode);
+
+    const char *Execute (char *Command);
 
     void PerformVTKSocketHandshake(char *sockname);
 
 protected:
-    vtkTclHelper();
-    ~vtkTclHelper(); 
-    
+    vtkOpenIGTLinkTclHelper();
+    ~vtkOpenIGTLinkTclHelper(); 
+
+    vtkSlicerApplicationLogic* AppLogic;
+    vtkMRMLScene *Scene;
     vtkMRMLVolumeNode *VolumeNode;
     vtkImageData *ImageData;    
     vtkMatrix4x4 *MeasurementFrame;    
     vtkMatrix4x4 *Matrix;    
     Tcl_Interp *Interp;           /* Tcl interpreter */
 };
-
 
 #endif
