a7d0e9d356da4b0ab9ac2a583654aba540a7b992
bensch <bensch@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-01-25 22:14:58 +0000

    ENH: Written a simple test. fails, this is BAD
    git-svn-id: http://svn.slicer.org/Slicer4/branches/cuda@5613 3bd1e089-480b-0410-8dfb-8563597acbee

--- Libs/CudaSupport/Testing/MemoryCopyTest.cxx
+++ Libs/CudaSupport/Testing/MemoryCopyTest.cxx
@@ -1,7 +1,27 @@
 
 
+#include "vtkCudaDeviceMemory.h"
+#include "vtkCudaLocalMemory.h"
+
 int main(int argc, char** argv)
 {
+  vtkCudaLocalMemory* local = vtkCudaLocalMemory::New();
+  vtkCudaDeviceMemory* dev = vtkCudaDeviceMemory::New();
+  
+  vtkCudaLocalMemory* dest = vtkCudaLocalMemory::New();
+
+  local->Allocate<int>(1000);
+  dev->Allocate<int>(1000);
+  dest->Allocate<int>(1000);
+  
+  local->MemSet(5);
+  
+  local->CopyTo(dev);
+  dev->CopyTo(dest);
+  
+  for (unsigned int i = 0; i < local->GetSize(); i++)
+    if (local->GetMemPointerAs<int>()[i] != dest->GetMemPointerAs<int>()[i])
+      return -1;
 
   return 0;  
 }
