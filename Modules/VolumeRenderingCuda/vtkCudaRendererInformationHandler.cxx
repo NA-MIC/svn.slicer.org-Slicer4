5589e39392c70046a395e2ad524ed785dcdb187d
bensch <bensch@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-02-07 13:19:17 +0000

    ENH: Added new Classes that shall handle the cuda comunication
    git-svn-id: http://svn.slicer.org/Slicer4/branches/cuda@5743 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/VolumeRenderingCuda/vtkCudaRendererInformationHandler.cxx
@@ -0,0 +1,16 @@
+#include "vtkCudaRendererInformationHandler.h"
+
+#include "vtkObjectFactory.h"
+
+vtkCxxRevisionMacro(vtkCudaRendererInformationHandler, "$Revision: 1.0 $");
+vtkStandardNewMacro(vtkCudaRendererInformationHandler);
+
+vtkCudaRendererInformationHandler::vtkCudaRendererInformationHandler()
+{
+
+}
+
+vtkCudaRendererInformationHandler::~vtkCudaRendererInformationHandler()
+{
+
+}
