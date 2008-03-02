123efd2a8c1323d8794902ff0817be3d3ab384fe
tgl <tgl@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-03-02 16:29:02 +0000

    BUG: typo in Description output, mismatched tag
    git-svn-id: http://svn.slicer.org/Slicer4@6034 3bd1e089-480b-0410-8dfb-8563597acbee

--- branches/tgl_loadable_modules/Modules/GradientAnisotropicDiffusionFilter/GradientAnisotropicDiffusionFilterModuleLib.cxx
+++ branches/tgl_loadable_modules/Modules/GradientAnisotropicDiffusionFilter/GradientAnisotropicDiffusionFilterModuleLib.cxx
@@ -7,7 +7,7 @@ static char GradientAnisotropicDiffusionFilterDescription[] =
 "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
 "<loadable>\n"
 "  <name>Gradient Anisotropic Module</name>\n"
-"  <shortname>GradientAnisotropicFilter<shortname>\n"
+"  <shortname>GradientAnisotropicFilter</shortname>\n"
 "  <guiname>GradientAnisotropicFilter</guiname>\n"
 "  <tclinitname>Gradientanisotropicdiffusionfilter_Init</tclinitname>\n"
 "  <message>Initializing Gradient Anisotropic Module...</message>\n"
