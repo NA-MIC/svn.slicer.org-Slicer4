1760ecc39e8176384c32327be0dbea57e9e219ed
tokuda <tokuda@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-03-10 04:29:49 +0000

    BUG: XYZ -> RAS in Prostate Module GUI
    git-svn-id: http://svn.slicer.org/Slicer4@6148 3bd1e089-480b-0410-8dfb-8563597acbee

--- branches/IGT/Modules/ProstateNav/Wizard/vtkProstateNavScanControlStep.cxx
+++ branches/IGT/Modules/ProstateNav/Wizard/vtkProstateNavScanControlStep.cxx
@@ -81,7 +81,7 @@ void vtkProstateNavScanControlStep::ShowUserInterface()
     this->MultiColumnList->GetWidget()->MovableColumnsOff();
 
     const char* labels[] =
-      { "Name", "X", "Y", "Z", "OrW", "OrX", "OrY", "OrZ" };
+      { "Name", "R", "A", "S", "OrW", "OrR", "OrA", "OrS" };
     const int widths[] = 
       { 8, 6, 6, 6, 6, 6, 6, 6 };
 
