ee064c44980978b3cc5e638e0e2679dd769147bd
tokuda <tokuda@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-03-19 20:41:46 +0000

    ENH: Added OpenIGTLink Module (with GUI).
    git-svn-id: http://svn.slicer.org/Slicer4/branches/IGT2@6251 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/OpenIGTLink/vtkIGTLConnector.h
@@ -0,0 +1,136 @@
+/*==========================================================================
+
+Portions (c) Copyright 2008 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+See Doc/copyright/copyright.txt
+or http://www.slicer.org/copyright/copyright.txt for details.
+
+Program:   3D Slicer
+Module:    $HeadURL: $
+Date:      $Date: $
+Version:   $Revision: $
+
+==========================================================================*/
+
+#ifndef __vtkIGTLConnector_h
+#define __vtkIGTLConnector_h
+
+#include <string>
+
+#include "vtkObject.h"
+#include "vtkOpenIGTLinkWin32Header.h" 
+
+//class vtkSocketCommunicator;
+class vtkMultiThreader;
+class vtkClientSocket;
+class vtkServerSocket;
+
+class VTK_OPENIGTLINK_EXPORT vtkIGTLConnector : public vtkObject
+{
+ public:  
+  //----------------------------------------------------------------
+  // Constants Definitions
+  //----------------------------------------------------------------
+
+  //BTX
+  enum {
+    TYPE_NOT_DEFINED,
+    TYPE_SERVER,
+    TYPE_CLIENT,
+    NUM_TYPE
+  };
+  
+  enum {
+    STATE_OFF,
+    STATE_WAIT_CONNECTION,
+    STATE_CONNECTED,
+    NUM_STATE
+  };
+  //ETX
+  
+ public:
+  
+  static vtkIGTLConnector *New();
+  vtkTypeRevisionMacro(vtkIGTLConnector,vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  vtkGetMacro( ServerPort, int );
+  vtkSetMacro( ServerPort, int );
+  vtkGetMacro( Type, int );
+  vtkSetMacro( Type, int );
+  vtkGetMacro( State, int );
+  vtkSetMacro( State, int );
+
+  //BTX
+  const char* GetServerHostname() { return this->ServerHostname.c_str(); }
+  //ETX
+
+  //----------------------------------------------------------------
+  // Constructor and Destructor
+  //----------------------------------------------------------------
+
+  vtkIGTLConnector();
+  virtual ~vtkIGTLConnector();
+
+  //----------------------------------------------------------------
+  // Connector configuration
+  //----------------------------------------------------------------
+
+  int SetTypeServer(int port);
+  int SetTypeClient(char* hostname, int port);
+  //BTX
+  int SetTypeClient(std::string hostname, int port);
+  //ETX
+
+  //BTX
+  const char* GetName() { return this->Name.c_str(); }
+  void SetName (const char* str) { this->Name = str; }
+  void SetName (std::string str) { this->Name = str; }
+  //ETX
+
+  //----------------------------------------------------------------
+  // Thread Control
+  //----------------------------------------------------------------
+
+  int Start();
+  int Stop();
+  static void* ThreadFunction(void* ptr);
+
+  //----------------------------------------------------------------
+  // OpenIGTLink Message handlers
+  //----------------------------------------------------------------
+  vtkClientSocket* WaitForConnection();
+  int ReceiveController(vtkClientSocket* socket);
+  int ReceiveImage(vtkClientSocket* socket, const char* deviceName,
+                   long long bodySize, long long crc);
+  int ReceiveTransform(vtkClientSocket* socket, const char* deviceName,
+                       long long bodySize, long long crc);
+
+ private:
+  //----------------------------------------------------------------
+  // Connector configuration 
+  //----------------------------------------------------------------
+  //BTX
+  std::string Name;
+  //ETX
+  int Type;
+  int State;
+
+  //----------------------------------------------------------------
+  // Thread and Socket
+  //----------------------------------------------------------------
+
+  vtkMultiThreader* Thread;
+  vtkServerSocket*  ServerSocket;
+  int               ThreadID;
+  int               ServerPort;
+  int               ServerStopFlag;
+  //BTX
+  std::string       ServerHostname;
+  //ETX
+
+};
+
+#endif // __vtkIGTLConnector_h
+
+
---
+++ Modules/OpenIGTLink/vtkOpenIGTLinkLogic.cxx
@@ -0,0 +1,266 @@
+/*==========================================================================
+
+Portions (c) Copyright 2008 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+See Doc/copyright/copyright.txt
+or http://www.slicer.org/copyright/copyright.txt for details.
+
+Program:   3D Slicer
+Module:    $HeadURL: $
+Date:      $Date: $
+Version:   $Revision: $
+
+==========================================================================*/
+
+#include "vtkObjectFactory.h"
+#include "vtkCallbackCommand.h"
+
+#include "vtkOpenIGTLinkLogic.h"
+
+#include "vtkMRMLModelDisplayNode.h"
+#include "vtkMRMLScalarVolumeNode.h"
+#include "vtkMRMLLinearTransformNode.h"
+#include "vtkSlicerApplication.h"
+#include "vtkSlicerApplicationGUI.h"
+#include "vtkSlicerColorLogic.h"
+
+#include "vtkMultiThreader.h"
+
+#include "vtkIGTLConnector.h"
+
+
+vtkCxxRevisionMacro(vtkOpenIGTLinkLogic, "$Revision: 1.9.12.1 $");
+vtkStandardNewMacro(vtkOpenIGTLinkLogic);
+
+//---------------------------------------------------------------------------
+vtkOpenIGTLinkLogic::vtkOpenIGTLinkLogic()
+{
+
+    std::cerr << "vtkOpenIGTLinkLogic::vtkOpenIGTLinkLogic() called " << std::endl;
+    vtkIGTLConnector* connector = vtkIGTLConnector::New();
+    connector->SetTypeServer(18944);
+    connector->Start();
+
+    this->SliceDriver0 = vtkOpenIGTLinkLogic::SLICE_DRIVER_USER;
+    this->SliceDriver1 = vtkOpenIGTLinkLogic::SLICE_DRIVER_USER;
+    this->SliceDriver2 = vtkOpenIGTLinkLogic::SLICE_DRIVER_USER;
+
+
+    // If the following code doesn't work, slice nodes should be obtained from application GUI
+    this->SliceNode0 = NULL;
+    this->SliceNode1 = NULL;
+    this->SliceNode2 = NULL;
+
+    /*
+    this->SliceNode0 = this->GetApplication()->GetApplicationGUI()->GetMainSliceLogic0()->GetSliceNode();
+    this->SliceNode1 = this->GetApplication()->GetApplicationGUI()->GetMainSliceLogic1()->GetSliceNode();
+    this->SliceNode2 = this->GetApplication()->GetApplicationGUI()->GetMainSliceLogic2()->GetSliceNode();
+    */
+    /*
+    this->Logic0 = appGUI->GetMainSliceGUI0()->GetLogic();
+    this->Logic1 = appGUI->GetMainSliceGUI1()->GetLogic();
+    this->Logic2 = appGUI->GetMainSliceGUI2()->GetLogic();
+    */
+                                         
+    this->NeedRealtimeImageUpdate0 = 0;
+    this->NeedRealtimeImageUpdate1 = 0;
+    this->NeedRealtimeImageUpdate2 = 0;
+
+    this->ImagingControl = 0;
+
+#ifdef USE_NAVITRACK
+    this->OpenTrackerStream   = vtkOpenIGTLinkDataStream::New();
+    this->RealtimeVolumeNode = NULL;
+#endif
+
+    // Timer Handling
+
+    this->DataCallbackCommand = vtkCallbackCommand::New();
+    this->DataCallbackCommand->SetClientData( reinterpret_cast<void *> (this) );
+    this->DataCallbackCommand->SetCallback(vtkOpenIGTLinkLogic::DataCallback);
+
+#ifdef USE_NAVITRACK
+    this->OpenTrackerStream->AddObserver(vtkCommand::ModifiedEvent, this->DataCallbackCommand);
+#endif 
+
+}
+
+
+//---------------------------------------------------------------------------
+vtkOpenIGTLinkLogic::~vtkOpenIGTLinkLogic()
+{
+
+    if (this->DataCallbackCommand)
+    {
+      this->DataCallbackCommand->Delete();
+    }
+
+#ifdef USE_NAVITRACK
+    if (this->OpenTrackerStream)
+    {
+      this->OpenTrackerStream->RemoveObservers( vtkCommand::ModifiedEvent, this->DataCallbackCommand );
+      this->OpenTrackerStream->Delete();
+    }
+#endif
+
+}
+
+
+//---------------------------------------------------------------------------
+void vtkOpenIGTLinkLogic::PrintSelf(ostream& os, vtkIndent indent)
+{
+    this->vtkObject::PrintSelf(os, indent);
+
+    os << indent << "vtkOpenIGTLinkLogic:             " << this->GetClassName() << "\n";
+
+}
+
+
+//---------------------------------------------------------------------------
+void vtkOpenIGTLinkLogic::DataCallback(vtkObject *caller, 
+                                       unsigned long eid, void *clientData, void *callData)
+{
+    vtkOpenIGTLinkLogic *self = reinterpret_cast<vtkOpenIGTLinkLogic *>(clientData);
+    vtkDebugWithObjectMacro(self, "In vtkOpenIGTLinkLogic DataCallback");
+    self->UpdateAll();
+}
+
+
+//---------------------------------------------------------------------------
+void vtkOpenIGTLinkLogic::UpdateAll()
+{
+
+}
+
+//---------------------------------------------------------------------------
+void vtkOpenIGTLinkLogic::AddConnector()
+{
+  vtkIGTLConnector* connector = vtkIGTLConnector::New();
+  connector->SetName("connector");
+  this->ConnectorList.push_back(connector);
+  this->SelectedConnectorIndex = this->ConnectorList.size();
+}
+
+//---------------------------------------------------------------------------
+void vtkOpenIGTLinkLogic::DeleteConnector()
+{
+  this->ConnectorList[this->SelectedConnectorIndex]->Delete();
+  this->ConnectorList.erase(this->ConnectorList.begin() + this->SelectedConnectorIndex);
+  if (this->SelectedConnectorIndex > this->ConnectorList.size())
+    {
+      this->SelectedConnectorIndex = this->ConnectorList.size();
+    }
+}
+
+//---------------------------------------------------------------------------
+int vtkOpenIGTLinkLogic::GetNumberOfConnectors()
+{
+  return this->ConnectorList.size();
+}
+
+vtkIGTLConnector* vtkOpenIGTLinkLogic::GetConnector(int id)
+{
+  if (id >= 0 && id < GetNumberOfConnectors())
+    {
+      return this->ConnectorList[id];
+    }
+  else
+    {
+      return NULL;
+    }
+}
+
+
+//---------------------------------------------------------------------------
+vtkMRMLVolumeNode* vtkOpenIGTLinkLogic::AddVolumeNode(const char* volumeNodeName)
+{
+
+    std::cerr << "AddVolumeNode(): called." << std::endl;
+
+    vtkMRMLVolumeNode *volumeNode = NULL;
+
+    if (volumeNode == NULL)  // if real-time volume node has not been created
+    {
+
+        //vtkMRMLVolumeDisplayNode *displayNode = NULL;
+        vtkMRMLScalarVolumeDisplayNode *displayNode = NULL;
+        vtkMRMLScalarVolumeNode *scalarNode = vtkMRMLScalarVolumeNode::New();
+        vtkImageData* image = vtkImageData::New();
+
+        float fov = 300.0;
+        image->SetDimensions(256, 256, 1);
+        image->SetExtent(0, 255, 0, 255, 0, 0 );
+        image->SetSpacing( fov/256, fov/256, 10 );
+        image->SetOrigin( -fov/2, -fov/2, -0.0 );
+        image->SetScalarTypeToShort();
+        image->AllocateScalars();
+        
+        short* dest = (short*) image->GetScalarPointer();
+        if (dest)
+        {
+          memset(dest, 0x00, 256*256*sizeof(short));
+          image->Update();
+        }
+        
+        /*
+        vtkSlicerSliceLayerLogic *reslice = vtkSlicerSliceLayerLogic::New();
+        reslice->SetUseReslice(0);
+        */
+        scalarNode->SetAndObserveImageData(image);
+
+        
+        /* Based on the code in vtkSlicerVolumeLogic::AddHeaderVolume() */
+        //displayNode = vtkMRMLVolumeDisplayNode::New();
+        displayNode = vtkMRMLScalarVolumeDisplayNode::New();
+        scalarNode->SetLabelMap(0);
+        volumeNode = scalarNode;
+        
+        if (volumeNode != NULL)
+        {
+            volumeNode->SetName(volumeNodeName);
+            this->GetMRMLScene()->SaveStateForUndo();
+            
+            vtkDebugMacro("Setting scene info");
+            volumeNode->SetScene(this->GetMRMLScene());
+            displayNode->SetScene(this->GetMRMLScene());
+            
+            
+            double range[2];
+            vtkDebugMacro("Set basic display info");
+            volumeNode->GetImageData()->GetScalarRange(range);
+            range[0] = 0.0;
+            range[1] = 256.0;
+            displayNode->SetLowerThreshold(range[0]);
+            displayNode->SetUpperThreshold(range[1]);
+            displayNode->SetWindow(range[1] - range[0]);
+            displayNode->SetLevel(0.5 * (range[1] - range[0]) );
+            
+            vtkDebugMacro("Adding node..");
+            this->GetMRMLScene()->AddNode(displayNode);
+            
+            //displayNode->SetDefaultColorMap();
+            vtkSlicerColorLogic *colorLogic = vtkSlicerColorLogic::New();
+            displayNode->SetAndObserveColorNodeID(colorLogic->GetDefaultVolumeColorNodeID());
+            //colorLogic->Delete();
+            
+            volumeNode->SetAndObserveDisplayNodeID(displayNode->GetID());
+            
+            vtkDebugMacro("Name vol node "<<volumeNode->GetClassName());
+            vtkDebugMacro("Display node "<<displayNode->GetClassName());
+            
+            this->GetMRMLScene()->AddNode(volumeNode);
+            vtkDebugMacro("Node added to scene");
+        }
+
+        //scalarNode->Delete();
+        /*
+        if (displayNode)
+        {
+            displayNode->Delete();
+        }
+        */
+
+    }
+    return volumeNode;
+}
+
