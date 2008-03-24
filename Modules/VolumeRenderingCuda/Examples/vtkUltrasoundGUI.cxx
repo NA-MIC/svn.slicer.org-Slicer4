4f0e54fc4a25d67baf3a4c505292533260ef9d2d
bensch <bensch@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-03-24 16:01:46 +0000

    ENH: Added new files
    git-svn-id: http://svn.slicer.org/Slicer4/branches/igt_cuda_merge@6274 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/VolumeRenderingCuda/Examples/vtkUltrasoundGUI.cxx
@@ -0,0 +1,17 @@
+
+#include "vtkUltrasoundGUI.h"
+#include "vtkObjectFactory.h"
+
+
+vtkCxxRevisionMacro(vtkUltrasoundGUI, "$ Revision 1.0$");
+vtkStandardNewMacro(vtkUltrasoundGUI);
+
+vtkUltrasoundGUI::vtkUltrasoundGUI()
+{
+ 
+}
+
+vtkUltrasoundGUI::~vtkUltrasoundGUI()
+{
+
+}
