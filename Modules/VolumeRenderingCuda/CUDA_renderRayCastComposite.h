5c3cef38e3e4eb66c5603c6bce1041721cdf7657
bensch <bensch@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-03-26 18:12:51 +0000

    ENH: correcting ZBuffer.
    git-svn-id: http://svn.slicer.org/Slicer4/branches/igt_cuda_merge@6298 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/VolumeRenderingCuda/CUDA_renderRayCastComposite.h
+++ Modules/VolumeRenderingCuda/CUDA_renderRayCastComposite.h
@@ -5,14 +5,14 @@
 
 template <typename T>
 __device__ void CUDAkernel_RayCastCompositeAlgorithm(const int3& index,
-                                            int outindex,
-                                            const float* minmax /*[6] */,
-                                            const float3* rayMap,
-                                            const cudaVolumeInformation& volInfo,
-                                            const cudaRendererInformation& renInfo,
-                                            float3* outputVal,
-                                            float* zBuffer,
-                                            float* remainingOpacity)
+                                                     int outindex,
+                                                     const float* minmax /*[6] */,
+                                                     const float3* rayMap,
+                                                     const cudaVolumeInformation& volInfo,
+                                                     const cudaRendererInformation& renInfo,
+                                                     float3* outputVal,
+                                                     float* zBuffer,
+                                                     float* remainingOpacity)
 {
     //ray tracing start from here
     float depth = 0.0;  //current step distance from camera
@@ -22,13 +22,13 @@ __device__ void CUDAkernel_RayCastCompositeAlgorithm(const int3& index,
     T tempValue;        //!< A Temporary color value
     int tempIndex;      //!< Temporaty index in the 3D data
     float alpha;        //!< Alpha value of current voxel
-    //float initialZBuffer = zBuffer[index.z]; //!< initial zBuffer from input
+    float initialZBuffer  =  zBuffer[index.z]; //!< initial zBuffer from input
 
     float A = renInfo.ClippingRange.y / (renInfo.ClippingRange.y - renInfo.ClippingRange.x);
     float B = renInfo.ClippingRange.y * renInfo.ClippingRange.x / (renInfo.ClippingRange.x - renInfo.ClippingRange.y);
 
     //perform ray tracing until integration of alpha value reach threshold 
-    while(depth < 1.0) {
+    while(depth < initialZBuffer) {
         distFromCam = B / ( depth - A);
 
         //calculate current position in ray tracing
@@ -43,59 +43,45 @@ __device__ void CUDAkernel_RayCastCompositeAlgorithm(const int3& index,
             tempPos.z >= minmax[4] && tempPos.z < minmax[5] )
         {
             //check whether current position is in front of z buffer wall
-            if(depth < 1.0 )//initialZBuffer)
-            { 
 
-                tempValue=((T*)volInfo.SourceData)[(int)(__float2int_rn(tempPos.z)*volInfo.VolumeSize.x*volInfo.VolumeSize.y + 
-                 __float2int_rn(tempPos.y)*volInfo.VolumeSize.x +
-                 __float2int_rn(tempPos.x))];
-                /*interpolation start here*/
-                //float posX = tempPos.x - __float2int_rd(tempPos.x);
-                //float posY = tempPos.y - __float2int_rd(tempPos.y);
-                //float posZ = tempPos.z - __float2int_rd(tempPos.z);
+            tempValue=((T*)volInfo.SourceData)[(int)(__float2int_rn(tempPos.z)*volInfo.VolumeSize.x*volInfo.VolumeSize.y + 
+                __float2int_rn(tempPos.y)*volInfo.VolumeSize.x +
+                __float2int_rn(tempPos.x))];
+            /*interpolation start here*/
+            //float posX = tempPos.x - __float2int_rd(tempPos.x);
+            //float posY = tempPos.y - __float2int_rd(tempPos.y);
+            //float posZ = tempPos.z - __float2int_rd(tempPos.z);
 
-                //int base = __float2int_rd((tempPos.z)) * volInfo.VolumeSize.x*volInfo.VolumeSize.y + __float2int_rd((tempPos.y)) * volInfo.VolumeSize.x + __float2int_rd((tempPos.x));
+            //int base = __float2int_rd((tempPos.z)) * volInfo.VolumeSize.x*volInfo.VolumeSize.y + __float2int_rd((tempPos.y)) * volInfo.VolumeSize.x + __float2int_rd((tempPos.x));
 
-                //tempValue=interpolate(posX, posY, posZ,
-                //    ((T*)volInfo.SourceData)[base],
-                //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x*volInfo.VolumeSize.y)],
-                //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x)],
-                //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x*volInfo.VolumeSize.y + volInfo.VolumeSize.x)],
-                //    ((T*)volInfo.SourceData)[(int)(base + 1)],
-                //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x*volInfo.VolumeSize.y + 1)],
-                //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x + 1)],
-                //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x*volInfo.VolumeSize.y + volInfo.VolumeSize.x + 1)]);
-                /*interpolation end here*/
+            //tempValue=interpolate(posX, posY, posZ,
+            //    ((T*)volInfo.SourceData)[base],
+            //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x*volInfo.VolumeSize.y)],
+            //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x)],
+            //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x*volInfo.VolumeSize.y + volInfo.VolumeSize.x)],
+            //    ((T*)volInfo.SourceData)[(int)(base + 1)],
+            //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x*volInfo.VolumeSize.y + 1)],
+            //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x + 1)],
+            //    ((T*)volInfo.SourceData)[(int)(base + volInfo.VolumeSize.x*volInfo.VolumeSize.y + volInfo.VolumeSize.x + 1)]);
+            /*interpolation end here*/
 
-                tempIndex = __float2int_rn((volInfo.FunctionSize-1) * (float)(tempValue-volInfo.FunctionRange[0]) /
-                    (float)(volInfo.FunctionRange[1]-volInfo.FunctionRange[0]));
-                alpha = volInfo.AlphaTransferFunction[tempIndex];
-                if(alpha >= 0){
+            tempIndex = __float2int_rn((volInfo.FunctionSize-1) * (float)(tempValue-volInfo.FunctionRange[0]) /
+                (float)(volInfo.FunctionRange[1]-volInfo.FunctionRange[0]));
+            alpha = volInfo.AlphaTransferFunction[tempIndex];
+            if(alpha >= 0){
 
-                    if(remainingOpacity[index.z] > 0.02)  // check if remaining opacity has reached threshold(0.02)
-                    {
-                        outputVal[index.z].x += remainingOpacity[index.z] * alpha * volInfo.ColorTransferFunction[tempIndex*3];
-                        outputVal[index.z].y += remainingOpacity[index.z] * alpha * volInfo.ColorTransferFunction[tempIndex*3+1];
-                        outputVal[index.z].z += remainingOpacity[index.z] * alpha * volInfo.ColorTransferFunction[tempIndex*3+2];
-                        remainingOpacity[index.z] *= (1.0 - alpha);
-                    }
-                    else // buffer filled to the max value
-                    { 
-                        zBuffer[index.z] = depth;
-                        break;
-                    }
-                }
-
-            } 
-            else 
-            { // current position is behind z buffer wall
-                if(index.x < renInfo.Resolution.x && index.y < renInfo.Resolution.y)
+                if(remainingOpacity[index.z] > 0.02)  // check if remaining opacity has reached threshold(0.02)
                 {
-                    outputVal[index.z].x += remainingOpacity[index.z] * renInfo.OutputImage[outindex].x;
-                    outputVal[index.z].y += remainingOpacity[index.z] * renInfo.OutputImage[outindex].y;
-                    outputVal[index.z].z += remainingOpacity[index.z] * renInfo.OutputImage[outindex].z;
+                    outputVal[index.z].x += remainingOpacity[index.z] * alpha * volInfo.ColorTransferFunction[tempIndex*3];
+                    outputVal[index.z].y += remainingOpacity[index.z] * alpha * volInfo.ColorTransferFunction[tempIndex*3+1];
+                    outputVal[index.z].z += remainingOpacity[index.z] * alpha * volInfo.ColorTransferFunction[tempIndex*3+2];
+                    remainingOpacity[index.z] *= (1.0 - alpha);
                 }
-                break;
+                else // buffer filled to the max value
+                { 
+                    zBuffer[index.z] = depth;
+                    break;
+                }
             }
         }
         depth += 1.0/256.0 * volInfo.SampleDistance;
