7838c7df193b39ccdd5926a56a35646403c969cc
tokuda <tokuda@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-04-04 21:29:14 +0000

    STYLE: Indent changed to VTK style
    git-svn-id: http://svn.slicer.org/Slicer4/branches/IGT@6406 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/OpenIGTLink/vtkIGTLConnector.h
+++ Modules/OpenIGTLink/vtkIGTLConnector.h
@@ -1,14 +1,14 @@
 /*==========================================================================
 
-Portions (c) Copyright 2008 Brigham and Women's Hospital (BWH) All Rights Reserved.
+  Portions (c) Copyright 2008 Brigham and Women's Hospital (BWH) All Rights Reserved.
 
-See Doc/copyright/copyright.txt
-or http://www.slicer.org/copyright/copyright.txt for details.
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
 
-Program:   3D Slicer
-Module:    $HeadURL: $
-Date:      $Date: $
-Version:   $Revision: $
+  Program:   3D Slicer
+  Module:    $HeadURL: $
+  Date:      $Date: $
+  Version:   $Revision: $
 
 ==========================================================================*/
 
