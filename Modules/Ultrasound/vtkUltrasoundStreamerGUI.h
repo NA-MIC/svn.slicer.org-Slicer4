dc9d1bbc676633b8d36fe68c387b0512b8a5fe05
bensch <bensch@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-04-11 16:07:13 +0000

    ENH: Compiles again after a lot of changes
    git-svn-id: http://svn.slicer.org/Slicer4/branches/igt_cuda_merge@6455 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ Modules/Ultrasound/vtkUltrasoundStreamerGUI.h
@@ -0,0 +1,50 @@
+#ifndef __ULTRASOUND_ULTRASOUND_STREAMER_GUI_H_
+#define __ULTRASOUND_ULTRASOUND_STREAMER_GUI_H_
+
+#include "vtkKWFrame.h"
+#include "vtkUltrasoundModule.h"
+
+class vtkKWCheckButton;
+class vtkKWScaleWithLabel;
+
+#include "vtkImageData.h"
+class vtkUltrasoundStreamSource;
+class vtkCallbackCommand;
+
+class VTK_ULTRASOUNDMODULE_EXPORT vtkUltrasoundStreamerGUI : public vtkKWFrame
+{
+public:
+    vtkTypeRevisionMacro(vtkUltrasoundStreamerGUI, vtkKWFrame);
+    static vtkUltrasoundStreamerGUI* New();
+
+    bool IsEnabled() const;
+    int GetRefreshRate() const;
+
+    vtkGetObjectMacro(StreamSource, vtkUltrasoundStreamSource);
+    vtkGetObjectMacro(ImageData, vtkImageData);
+    vtkSetObjectMacro(ImageData, vtkImageData);
+
+    void ProcessGUIEvents ( vtkObject *caller, unsigned long event, void *callData );
+    void UpdateInput();
+
+    vtkCallbackCommand* GUICallbackCommand;
+
+protected:
+    vtkUltrasoundStreamerGUI();
+    ~vtkUltrasoundStreamerGUI();
+
+    virtual void CreateWidget();
+    virtual void AddGUIObservers();
+
+private:
+    vtkUltrasoundStreamerGUI(const vtkUltrasoundStreamerGUI&); // Not Defined
+    vtkUltrasoundStreamerGUI& operator=(const vtkUltrasoundStreamerGUI&); // Not Defined
+
+    vtkUltrasoundStreamSource*  StreamSource;
+    vtkKWCheckButton*           cb_Enabled;
+    vtkKWScaleWithLabel*        sc_RefreshRate;
+
+    vtkImageData*               ImageData;
+};
+
+#endif /* __ULTRASOUND_ULTRASOUND_STREAMER_GUI_H_ */
