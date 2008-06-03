1cd723319bb73bfc93090a19d877e4c9a033fdb9
clisle <clisle@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-06-03 20:17:56 +0000

    ENH: testing v2 of Meshing integration
    git-svn-id: http://svn.slicer.org/Slicer4/branches/testing@6964 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ MeshingIntegration3/Modules/Editor/RemoveIslandsEffect.tcl
@@ -0,0 +1,139 @@
+
+package require Itcl
+
+#########################################################
+#
+if {0} { ;# comment
+
+  RemoveIslandsEffect an editor effect
+
+
+# TODO : 
+
+}
+#
+#########################################################
+
+#
+#########################################################
+# ------------------------------------------------------------------
+#                             RemoveIslandsEffect
+# ------------------------------------------------------------------
+#
+# The class definition - define if needed (not when re-sourcing)
+#
+if { [itcl::find class RemoveIslandsEffect] == "" } {
+
+  itcl::class RemoveIslandsEffect {
+
+    inherit EffectSWidget
+
+    constructor {sliceGUI} {EffectSWidget::constructor $sliceGUI} {}
+    destructor {}
+
+    # methods
+    method processEvent {{caller ""} {event ""}} {}
+    method preview {} {}
+    method apply {} {}
+    method buildOptions {} {}
+    method tearDownOptions {} {}
+  }
+}
+
+# ------------------------------------------------------------------
+#                        CONSTRUCTOR/DESTRUCTOR
+# ------------------------------------------------------------------
+itcl::body RemoveIslandsEffect::constructor {sliceGUI} {
+  # rely on superclass constructor
+}
+
+itcl::body RemoveIslandsEffect::destructor {} {
+}
+
+# ------------------------------------------------------------------
+#                             METHODS
+# ------------------------------------------------------------------
+
+itcl::body RemoveIslandsEffect::processEvent { {caller ""} {event ""} } {
+
+  if { [$this preProcessEvent $caller $event] } {
+    # superclass processed the event, so we don't
+    return
+  }
+
+  set event [$sliceGUI GetCurrentGUIEvent] 
+  set _currentPosition [$this xyToRAS [$_interactor GetEventPosition]]
+
+  switch $event {
+    "LeftButtonPressEvent" {
+      $this apply
+      $sliceGUI SetGUICommandAbortFlag 1
+    }
+    "EnterEvent" {
+      $o(cursorActor) VisibilityOn
+    }
+    "LeaveEvent" {
+      $o(cursorActor) VisibilityOff
+    }
+  }
+
+  $this positionCursor
+  [$sliceGUI GetSliceViewer] RequestRender
+}
+
+itcl::body RemoveIslandsEffect::apply {} {
+
+  foreach {x y} [$_interactor GetEventPosition] {}
+  $this queryLayers $x $y
+
+  if { [$this getInputLabel] == "" || [$this getInputLabel] == "" } {
+    $this flashCursor 3
+    return
+  }
+
+  set conn [vtkImageConnectivity New]
+  $conn SetFunctionToRemoveIslands
+  $conn SetSeed $_layers(label,i) $_layers(label,j) $_layers(label,k) 
+  $conn SetInput [$this getInputLabel]
+  $conn SetOutput [$this getOutputLabel]
+  $this setProgressFilter $conn "Remove Islands"
+  [$this getOutputLabel] Update
+  $conn Delete
+
+  $this postApply
+}
+
+  
+itcl::body RemoveIslandsEffect::buildOptions {} {
+
+  #
+  # a cancel button
+  #
+  set o(cancel) [vtkNew vtkKWPushButton]
+  $o(cancel) SetParent [$this getOptionsFrame]
+  $o(cancel) Create
+  $o(cancel) SetText "Cancel"
+  $o(cancel) SetBalloonHelpString "Cancel threshold without applying to label map."
+  pack [$o(cancel) GetWidgetName] \
+    -side right -anchor e -padx 2 -pady 2 
+
+  #
+  # event observers - TODO: if there were a way to make these more specific, I would...
+  #
+  set tag [$o(cancel) AddObserver AnyEvent "after idle ::EffectSWidget::RemoveAll"]
+  lappend _observerRecords "$o(cancel) $tag"
+
+  if { [$this getInputLabel] == "" } {
+    $this errorDialog "Need to have a label layer to use island effect"
+    after idle ::EffectSWidget::RemoveAll
+  }
+}
+
+itcl::body RemoveIslandsEffect::tearDownOptions { } {
+  foreach w "cancel" {
+    if { [info exists o($w)] } {
+      $o($w) SetParent ""
+      pack forget [$o($w) GetWidgetName] 
+    }
+  }
+}
---
+++ MeshingIntegration3/Modules/VolumeRendering/vtkVolumeRenderingLogic.cxx
@@ -0,0 +1,45 @@
+#include "vtkVolumeRenderingLogic.h"
+#include "vtkObjectFactory.h"
+#include "vtkObject.h"
+
+#include "vtkMRMLVolumeRenderingNode.h"
+
+bool vtkVolumeRenderingLogic::First = true;
+
+vtkVolumeRenderingLogic::vtkVolumeRenderingLogic(void)
+{
+}
+
+vtkVolumeRenderingLogic::~vtkVolumeRenderingLogic(void)
+{
+}
+vtkVolumeRenderingLogic* vtkVolumeRenderingLogic::New()
+{
+ // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkVolumeRenderingLogic");
+  if(ret)
+    {
+      return (vtkVolumeRenderingLogic*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkVolumeRenderingLogic;
+}
+void vtkVolumeRenderingLogic::PrintSelf(std::ostream &os, vtkIndent indent)
+{
+    os<<indent<<"Print logic"<<endl;
+}
+
+
+void vtkVolumeRenderingLogic::SetMRMLScene(vtkMRMLScene *scene)
+{
+  vtkSlicerModuleLogic::SetMRMLScene(scene);
+
+  if (this->First)
+    {
+      // :NOTE: 20050513 tgl: Guard this so it is only registered once.
+      vtkMRMLVolumeRenderingNode *vrNode=vtkMRMLVolumeRenderingNode::New();
+      scene->RegisterNodeClass(vrNode);
+      vrNode->Delete();
+      this->First = false;
+    }
+}
