2344d0cd49dfb785177a17a2907e607bb44659ff
demian <demian@3bd1e089-480b-0410-8dfb-8563597acbee>
2008-06-26 23:20:06 +0000

    ENH: Branch for simple generalization of previous volume glyphing architecture, which is very diffusion oriented
    git-svn-id: http://svn.slicer.org/Slicer4@7212 3bd1e089-480b-0410-8dfb-8563597acbee

---
+++ branches/demianGlyphVolume_simple/Applications/CLI/RegisterImagesModule/itkInitialImageToImageRegistrationMethod.txx
@@ -0,0 +1,262 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: MomentRegistrator.txx,v $
+  Language:  C++
+  Date:      $Date: 2007/03/29 17:52:55 $
+  Version:   $Revision: 1.6 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef __InitialImageToImageRegistrationMethod_txx
+#define __InitialImageToImageRegistrationMethod_txx
+
+#include "itkInitialImageToImageRegistrationMethod.h"
+
+#include "itkImageMomentsCalculator.h"
+
+namespace itk
+{
+
+template< class TImage >
+InitialImageToImageRegistrationMethod< TImage >
+::InitialImageToImageRegistrationMethod( void )
+{
+  this->SetTransform( TransformType::New() );
+  this->GetTypedTransform()->SetIdentity();
+
+  this->m_NumberOfMoments = 0 ;
+
+  this->m_ComputeCenterOfRotationOnly = false;
+}
+
+template< class TImage >
+InitialImageToImageRegistrationMethod< TImage >
+::~InitialImageToImageRegistrationMethod( void )
+{
+}
+
+/** Only the GenerateData() method should be overloaded. The Update() method
+ * must not be overloaded */
+template< class TImage >
+void
+InitialImageToImageRegistrationMethod< TImage >
+::GenerateData( void )
+{
+  Superclass::GenerateData();
+
+  typedef ImageMomentsCalculator< TImage > MomentsCalculatorType;
+
+  typename MomentsCalculatorType::AffineTransformType::Pointer newTransform;
+  newTransform = MomentsCalculatorType::AffineTransformType::New();
+  newTransform->SetIdentity();
+
+  if( this->m_ComputeCenterOfRotationOnly )
+    {
+    typename TImage::SizeType    size;
+
+    //  Moving image info
+    typename TImage::IndexType       movingCenterIndex;
+    Point<double, ImageDimension>    movingCenterPoint;
+
+    size = this->GetMovingImage()->GetLargestPossibleRegion().GetSize();
+
+    for(int i=0; i<ImageDimension; i++)
+      {
+      movingCenterIndex[i] = size[i]/2;
+      }
+    this->GetMovingImage()->TransformIndexToPhysicalPoint(movingCenterIndex,
+                                                          movingCenterPoint);
+
+    newTransform->SetCenter(movingCenterPoint);
+    }
+  else if( this->m_NumberOfMoments == 0)
+    {
+    typename TImage::SizeType    size;
+
+    //  Fixed image info
+    typename TImage::IndexType        fixedCenterIndex;
+    Point<double, ImageDimension>     fixedCenterPoint;
+
+    size = this->GetFixedImage()->GetLargestPossibleRegion().GetSize();
+
+    for(int i=0; i<ImageDimension; i++)
+      {
+      fixedCenterIndex[i] = size[i]/2;
+      }
+    this->GetFixedImage()->TransformIndexToPhysicalPoint(fixedCenterIndex,
+                                                         fixedCenterPoint);
+
+    //  Moving image info
+    typename TImage::IndexType       movingCenterIndex;
+    Point<double, ImageDimension>    movingCenterPoint;
+
+    size = this->GetMovingImage()->GetLargestPossibleRegion().GetSize();
+
+    for(int i=0; i<ImageDimension; i++)
+      {
+      movingCenterIndex[i] = size[i]/2;
+      }
+    this->GetMovingImage()->TransformIndexToPhysicalPoint(movingCenterIndex,
+                                                          movingCenterPoint);
+
+    //  Compute alignment
+    typename TransformType::OffsetType   offset;
+    offset = movingCenterPoint - fixedCenterPoint;
+
+    newTransform->SetCenter(movingCenterPoint);
+    newTransform->SetOffset(offset);
+    }
+  else 
+    {
+    typedef ImageMomentsCalculator< TImage > MomentsCalculatorType;
+
+    typename MomentsCalculatorType::Pointer momCalc;
+    momCalc = MomentsCalculatorType::New();
+
+    momCalc->SetImage( this->GetFixedImage() );
+    if( this->GetUseFixedImageMaskObject() )
+      {
+      if( this->GetFixedImageMaskObject() )
+        {
+        momCalc->SetSpatialObjectMask( this->GetFixedImageMaskObject() );
+        }
+      }
+    // HELP: ImageMomentsCalculator isn't multi-threaded :(
+    //momCalc->SetNumberOfThreads( this->GetRegistrationNumberOfThreads() );
+    try
+      {
+      momCalc->Compute();
+      }
+    catch( ... )
+      {
+      std::cout << "Exception thrown when computing moments of fixed image." << std::endl;
+      std::cout << "Initialization returning identity." << std::endl;
+      newTransform->SetIdentity();
+      this->SetTransform(newTransform);
+      return;
+      }
+
+    typename MomentsCalculatorType::AffineTransformType::Pointer fixedImageAxesTransform;
+
+    fixedImageAxesTransform = momCalc->GetPhysicalAxesToPrincipalAxesTransform();
+
+    typename TransformType::InputPointType fixedImageCenterOfMass;
+
+    for(unsigned int i=0; i<this->ImageDimension; i++)
+      {
+      fixedImageCenterOfMass[i] = momCalc->GetCenterOfGravity()[i];
+      }
+
+    momCalc->SetImage( this->GetMovingImage() );
+
+    if( this->GetUseMovingImageMaskObject() )
+      {
+      if( this->GetMovingImageMaskObject() )
+        {
+        momCalc->SetSpatialObjectMask( this->GetMovingImageMaskObject() );
+        }
+      }
+
+    try
+      {
+      momCalc->Compute();
+      }
+    catch( ... )
+      {
+      std::cout << "Exception thrown when computing moments of moving image." << std::endl;
+      std::cout << "Initialization returning identity." << std::endl;
+      newTransform->SetIdentity();
+      this->SetTransform(newTransform);
+      return;
+      }
+
+    typename MomentsCalculatorType::AffineTransformType::Pointer 
+          movingImageAxesTransform;
+    movingImageAxesTransform = 
+          momCalc->GetPrincipalAxesToPhysicalAxesTransform();
+
+    typename TransformType::InputPointType movingImageCenterOfMass;
+    for(unsigned int i=0; i<this->ImageDimension; i++)
+      {
+      movingImageCenterOfMass[i] = momCalc->GetCenterOfGravity()[i];
+      }
+
+    typename TransformType::OffsetType offset;
+    offset = movingImageCenterOfMass - fixedImageCenterOfMass;
+    
+    if( this->m_NumberOfMoments == 1 ) // Centers of mass
+      {
+      newTransform->SetCenter(movingImageCenterOfMass);
+      newTransform->SetOffset(offset);
+      }
+    else  // m_NumberOfMoments == 2 // Principle axes
+      {
+      newTransform->SetCenter(fixedImageCenterOfMass);
+      newTransform->SetMatrix(fixedImageAxesTransform->GetMatrix());
+      newTransform->SetOffset(fixedImageAxesTransform->GetOffset());
+      newTransform->Compose(movingImageAxesTransform, true);
+      }
+    }
+
+  this->SetTransform(newTransform);
+}
+
+template< class TImage >
+typename InitialImageToImageRegistrationMethod< TImage >::TransformType *
+InitialImageToImageRegistrationMethod< TImage >
+::GetTypedTransform( void )
+{
+  return static_cast< TransformType  * >( Superclass::GetTransform() );
+}
+
+template< class TImage >
+const typename InitialImageToImageRegistrationMethod< TImage >::TransformType *
+InitialImageToImageRegistrationMethod< TImage >
+::GetTypedTransform( void ) const
+{
+  return static_cast< const TransformType  * >( this->Superclass::GetTransform() );
+}
+
+
+template< class TImage >
+typename InitialImageToImageRegistrationMethod< TImage >::TransformPointer
+InitialImageToImageRegistrationMethod< TImage >
+::GetAffineTransform( void ) const
+{   
+  typename TransformType::Pointer trans = TransformType::New();
+
+  const TransformType * typededTransform = this->GetTypedTransform();
+
+  trans->SetIdentity();
+  trans->SetCenter( typededTransform->GetCenter() );
+  trans->SetMatrix( typededTransform->GetMatrix() );
+  trans->SetOffset( typededTransform->GetOffset() );
+
+  return trans;
+}   
+
+
+template< class TImage >
+void
+InitialImageToImageRegistrationMethod< TImage >
+::PrintSelf( std::ostream & os, Indent indent ) const
+{
+  Superclass::PrintSelf( os, indent );
+
+  os << indent << "Number of moments = " << this->m_NumberOfMoments << std::endl;
+
+  os << indent << "Compute Center Of Rotation Only = " << this->m_ComputeCenterOfRotationOnly << std::endl;
+}
+
+};
+
+#endif 
---
+++ branches/demianGlyphVolume_simple/Libs/MRML/vtkMRMLVolumeArchetypeStorageNode.cxx
@@ -0,0 +1,411 @@
+/*=auto=========================================================================
+
+Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+See Doc/copyright/copyright.txt
+or http://www.slicer.org/copyright/copyright.txt for details.
+
+Program:   3D Slicer
+Module:    $RCSfile: vtkMRMLVolumeArchetypeStorageNode.cxx,v $
+Date:      $Date: 2006/03/17 15:10:10 $
+Version:   $Revision: 1.6 $
+
+=========================================================================auto=*/
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "vtkMRMLConfigure.h" // MRML_USE*
+
+#include "vtkObjectFactory.h"
+#include "vtkCallbackCommand.h"
+#include "vtkImageChangeInformation.h"
+#include "vtkMRMLVolumeArchetypeStorageNode.h"
+#include "vtkMRMLVolumeNode.h"
+#include "vtkMRMLScalarVolumeNode.h"
+
+#ifdef MRML_USE_vtkTeem
+#include "vtkMRMLVectorVolumeNode.h"
+#endif
+
+#include "vtkMatrix4x4.h"
+#include "vtkImageData.h"
+#include "vtkDataArray.h"
+#include "vtkPointData.h"
+#include "vtkITKArchetypeImageSeriesReader.h"
+#include "vtkITKArchetypeImageSeriesScalarReader.h"
+#include "vtkITKArchetypeImageSeriesVectorReaderFile.h"
+#include "vtkITKArchetypeImageSeriesVectorReaderSeries.h"
+#include "vtkITKImageWriter.h"
+
+//------------------------------------------------------------------------------
+vtkMRMLVolumeArchetypeStorageNode* vtkMRMLVolumeArchetypeStorageNode::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLVolumeArchetypeStorageNode");
+  if(ret)
+    {
+    return (vtkMRMLVolumeArchetypeStorageNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLVolumeArchetypeStorageNode;
+}
+
+//----------------------------------------------------------------------------
+
+vtkMRMLNode* vtkMRMLVolumeArchetypeStorageNode::CreateNodeInstance()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMRMLVolumeArchetypeStorageNode");
+  if(ret)
+    {
+    return (vtkMRMLVolumeArchetypeStorageNode*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLVolumeArchetypeStorageNode;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLVolumeArchetypeStorageNode::vtkMRMLVolumeArchetypeStorageNode()
+{
+  this->CenterImage = 0;
+  this->SingleFile  = 0;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLVolumeArchetypeStorageNode::~vtkMRMLVolumeArchetypeStorageNode()
+{
+}
+
+void vtkMRMLVolumeArchetypeStorageNode::WriteXML(ostream& of, int nIndent)
+{
+  Superclass::WriteXML(of, nIndent);
+  vtkIndent indent(nIndent);
+  {
+  std::stringstream ss;
+  ss << this->CenterImage;
+  of << indent << " centerImage=\"" << ss.str() << "\"";
+  }
+  {
+  std::stringstream ss;
+  ss << this->SingleFile;
+  of << indent << " singleFile=\"" << ss.str() << "\"";
+  }
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLVolumeArchetypeStorageNode::ReadXMLAttributes(const char** atts)
+{
+
+  Superclass::ReadXMLAttributes(atts);
+
+  const char* attName;
+  const char* attValue;
+  while (*atts != NULL) 
+    {
+    attName = *(atts++);
+    attValue = *(atts++);
+    if (!strcmp(attName, "centerImage")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> this->CenterImage;
+      }
+    if (!strcmp(attName, "singleFile")) 
+      {
+      std::stringstream ss;
+      ss << attValue;
+      ss >> this->SingleFile;
+      }
+    }
+}
+
+//----------------------------------------------------------------------------
+// Copy the node's attributes to this object.
+// Does NOT copy: ID, FilePrefix, Name, StorageID
+void vtkMRMLVolumeArchetypeStorageNode::Copy(vtkMRMLNode *anode)
+{
+  Superclass::Copy(anode);
+  vtkMRMLVolumeArchetypeStorageNode *node = (vtkMRMLVolumeArchetypeStorageNode *) anode;
+
+  this->SetCenterImage(node->CenterImage);
+  this->SetSingleFile(node->SingleFile);
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLVolumeArchetypeStorageNode::PrintSelf(ostream& os, vtkIndent indent)
+{  
+  vtkMRMLStorageNode::PrintSelf(os,indent);
+  os << indent << "CenterImage:   " << this->CenterImage << "\n";
+  os << indent << "SingleFile:   " << this->SingleFile << "\n";
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLVolumeArchetypeStorageNode::ProcessParentNode(vtkMRMLNode *parentNode)
+{
+  this->ReadData(parentNode);
+}
+
+//----------------------------------------------------------------------------
+int vtkMRMLVolumeArchetypeStorageNode::ReadData(vtkMRMLNode *refNode)
+{
+
+  // do not read if if we are not in the scene (for example inside snapshot)
+  if ( !this->GetAddToScene() || !refNode->GetAddToScene() )
+    {
+    return 1;
+    }
+   
+  // test whether refNode is a valid node to hold a volume
+  if ( !( refNode->IsA("vtkMRMLScalarVolumeNode") || refNode->IsA("vtkMRMLVectorVolumeNode" ) ) )
+    {
+    //vtkErrorMacro("Reference node is not a vtkMRMLVolumeNode");
+    return 0;         
+    }
+  if (this->GetFileName() == NULL && this->GetURI() == NULL) 
+    {
+    vtkErrorMacro("ReadData: both filename and uri are null.");
+    return 0;
+    }
+  
+  Superclass::StageReadData(refNode);
+  if ( this->GetReadState() != this->TransferDone )
+    {
+    // remote file download hasn't finished
+    vtkWarningMacro("ReadData: read state is pending, remote download hasn't finished yet");
+    return 0;
+    }
+  else
+    {
+    vtkDebugMacro("ReadData: read state is ready, URI = " << (this->GetURI() == NULL ? "null" : this->GetURI()) << ", filename = " << (this->GetFileName() == NULL ? "null" : this->GetFileName()));
+    }
+  
+  std::string fullName = this->GetFullNameFromFileName();
+
+  if (fullName == std::string("")) 
+    {
+    vtkErrorMacro("ReadData: File name not specified");
+    return 0;
+    }
+  
+  vtkMRMLVolumeNode *volNode;
+  vtkITKArchetypeImageSeriesReader* reader;
+  
+  if ( refNode->IsA("vtkMRMLScalarVolumeNode") ) 
+    {
+    volNode = dynamic_cast <vtkMRMLScalarVolumeNode *> (refNode);
+    reader = vtkITKArchetypeImageSeriesScalarReader::New();  
+    reader->SetSingleFile( this->GetSingleFile() );
+    }
+#ifdef MRML_USE_vtkTeem
+  else if ( refNode->IsA("vtkMRMLVectorVolumeNode") ) 
+    {
+    volNode = dynamic_cast <vtkMRMLVectorVolumeNode *> (refNode);
+    // 
+    // decide if we want to use a vector file reader (e.g. for multi-component nrrd)
+    // or a vector series reader (e.g. for a sequence of rgb image files)
+    // - note these are different classes because they are too big to compile
+    //   as a single class on some systems
+    //
+    vtkITKArchetypeImageSeriesVectorReaderFile *readerFile = vtkITKArchetypeImageSeriesVectorReaderFile::New();
+    vtkITKArchetypeImageSeriesVectorReaderSeries *readerSeries = vtkITKArchetypeImageSeriesVectorReaderSeries::New();
+
+    readerFile->SetSingleFile( this->GetSingleFile() );
+    readerSeries->SetSingleFile( this->GetSingleFile() );
+
+    readerFile->SetArchetype(fullName.c_str());
+    readerSeries->SetArchetype(fullName.c_str());
+    try 
+      {
+      readerSeries->UpdateInformation();
+      }
+    catch ( ... )
+      {
+      readerFile->Delete();
+      readerSeries->Delete();
+      return 0;
+      }
+    if ( readerSeries->GetNumberOfFileNames() == 1 )
+      {
+      reader = readerFile;
+      reader->UpdateInformation();
+      readerSeries->Delete();
+      }
+    else
+      {
+      reader = readerSeries;
+      readerFile->Delete();
+      }
+    if (reader->GetNumberOfComponents() < 3)
+      {
+      reader->Delete();
+      return 0;
+      }
+    }
+#endif
+
+  reader->AddObserver( vtkCommand::ProgressEvent,  this->MRMLCallbackCommand);
+
+  if (volNode->GetImageData()) 
+    {
+    volNode->SetAndObserveImageData (NULL);
+    }
+
+
+  reader->SetArchetype(fullName.c_str());
+  reader->SetOutputScalarTypeToNative();
+  reader->SetDesiredCoordinateOrientationToNative();
+  if (this->CenterImage) 
+    {
+    reader->SetUseNativeOriginOff();
+    }
+  else
+    {
+    reader->SetUseNativeOriginOn();
+    }
+
+  int result = 1;
+  try
+    {
+    reader->Update();
+    }
+    catch (...)
+    {
+    vtkErrorMacro("vtkMRMLVolumeArchetypeStorageNode: Cannot read file: " << fullName.c_str() );
+    reader->RemoveObservers( vtkCommand::ProgressEvent,  this->MRMLCallbackCommand);
+    reader->Delete();
+    return 0;
+    }
+  if (reader->GetOutput() == NULL 
+      || reader->GetOutput()->GetPointData()->GetScalars()->GetNumberOfTuples() == 0) 
+    {
+    vtkErrorMacro("vtkMRMLVolumeArchetypeStorageNode: Cannot read file: " << fullName.c_str() );
+    reader->Delete();
+    return 0;
+    }
+  // set volume attributes
+  volNode->SetAndObserveStorageNodeID(this->GetID());
+  volNode->SetMetaDataDictionary( reader->GetMetaDataDictionary() );
+  //TODO update scene to send Modified event
+ 
+  vtkImageChangeInformation *ici = vtkImageChangeInformation::New();
+  ici->SetInput (reader->GetOutput());
+  ici->SetOutputSpacing( 1, 1, 1 );
+  ici->SetOutputOrigin( 0, 0, 0 );
+  ici->Update();
+
+  if (ici->GetOutput() == NULL)
+    {
+    vtkErrorMacro("vtkMRMLVolumeArchetypeStorageNode: Cannot read file: " << fullName.c_str() );
+    reader->RemoveObservers( vtkCommand::ProgressEvent,  this->MRMLCallbackCommand);
+    reader->Delete();
+    ici->Delete();
+    return 0;
+    }
+  else
+    {
+    volNode->SetAndObserveImageData (ici->GetOutput());
+    volNode->ModifiedSinceReadOff();
+    }
+
+  vtkMatrix4x4* mat = reader->GetRasToIjkMatrix();
+  if ( mat == NULL )
+    {
+    vtkErrorMacro ("Reader returned NULL RasToIjkMatrix");
+    }
+  volNode->SetRASToIJKMatrix(mat);
+
+  reader->RemoveObservers( vtkCommand::ProgressEvent,  this->MRMLCallbackCommand);
+  reader->Delete();
+  ici->Delete();
+
+  this->SetReadStateIdle();
+  
+  return result;
+}
+
+//----------------------------------------------------------------------------
+int vtkMRMLVolumeArchetypeStorageNode::WriteData(vtkMRMLNode *refNode)
+{
+  // test whether refNode is a valid node to hold a volume
+  if (!refNode->IsA("vtkMRMLScalarVolumeNode") ) 
+    {
+    vtkErrorMacro("Reference node is not a vtkMRMLVolumeNode");
+    return 0;
+    }
+  
+  vtkMRMLVolumeNode *volNode;
+  
+  if ( refNode->IsA("vtkMRMLScalarVolumeNode") ) 
+    {
+    volNode = vtkMRMLScalarVolumeNode::SafeDownCast(refNode);
+    }
+  
+  if (volNode->GetImageData() == NULL) 
+    {
+    vtkErrorMacro("cannot write ImageData, it's NULL");
+    return 0;
+    }
+  
+  std::string fullName = this->GetFullNameFromFileName();  
+  if (fullName == std::string("")) 
+    {
+    vtkErrorMacro("WriteData: File name not specified");
+    return 0;
+    }
+  vtkITKImageWriter *writer = vtkITKImageWriter::New();
+  writer->SetFileName(fullName.c_str());
+  
+  writer->SetInput( volNode->GetImageData() );
+  writer->SetUseCompression(this->GetUseCompression());
+
+  // set volume attributes
+  vtkMatrix4x4* mat = vtkMatrix4x4::New();
+  volNode->GetRASToIJKMatrix(mat);
+  writer->SetRasToIJKMatrix(mat);
+
+  int result = 1;
+  try
+    {
+    writer->Write();
+    }
+    catch (...)
+    {
+    result = 0;
+    }
+  mat->Delete();
+  writer->Delete();    
+
+  Superclass::StageWriteData(refNode);
+  
+  return result;
+
+}
+
+//----------------------------------------------------------------------------
+int vtkMRMLVolumeArchetypeStorageNode::SupportedFileType(const char *fileName)
+{
+  // check to see which file name we need to check
+  std::string name;
+  if (fileName)
+    {
+    name = std::string(fileName);
+    }
+  else if (this->FileName != NULL)
+    {
+    name = std::string(this->FileName);
+    }
+  else if (this->URI != NULL)
+    {
+    name = std::string(this->URI);
+    }
+  else
+    {
+    vtkWarningMacro("SupportedFileType: no file name to check");
+    return 0;
+    }
+
+  // for now, return 1
+  return 1;
+}
