4b288a1afd58233283a6df1a879c92a210e3aea7
pieper <pieper@3bd1e089-480b-0410-8dfb-8563597acbee>
2009-04-22 23:00:01 +0000

    COMP: fix the version numbers
    git-svn-id: http://svn.slicer.org/Slicer4/branches/Slicer-3-4@9283 3bd1e089-480b-0410-8dfb-8563597acbee

--- Scripts/getbuildtest.tcl
+++ Scripts/getbuildtest.tcl
@@ -389,7 +389,7 @@ if { $::GETBUILDTEST(version-patch) == "" } {
 }
 
 # set the binary filename root
-set ::GETBUILDTEST(binary-filename) "Slicer3-3.4-alpha-$::GETBUILDTEST(version-patch)-$::env(BUILD)"
+set ::GETBUILDTEST(binary-filename) "Slicer3-3.4-RC-$::GETBUILDTEST(version-patch)-$::env(BUILD)"
 if {$::GETBUILDTEST(verbose)} {
     puts "CPack will use $::::GETBUILDTEST(binary-filename)"
 }
