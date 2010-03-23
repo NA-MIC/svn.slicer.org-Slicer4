b01c2f20cc9cc674eddae26dce6bd1b3065eee44
hayes <hayes@3bd1e089-480b-0410-8dfb-8563597acbee>
2010-03-23 17:44:29 +0000

    ENH: Changed version number to 3.4.3
    git-svn-id: http://svn.slicer.org/Slicer4/branches/Slicer-3-4@12435 3bd1e089-480b-0410-8dfb-8563597acbee

--- Scripts/getbuildtest.tcl
+++ Scripts/getbuildtest.tcl
@@ -393,7 +393,7 @@ if { $::GETBUILDTEST(version-patch) == "" } {
 }
 
 # set the binary filename root
-set ::GETBUILDTEST(binary-filename) "Slicer3-3.4.2-$::GETBUILDTEST(version-patch)-$::env(BUILD)"
+set ::GETBUILDTEST(binary-filename) "Slicer3-3.4.3-$::GETBUILDTEST(version-patch)-$::env(BUILD)"
 if {$::GETBUILDTEST(verbose)} {
     puts "CPack will use $::::GETBUILDTEST(binary-filename)"
 }
