NA-MIC/svn.slicer.org-Slicer4

commit 9ee2fff39fa6c33ea9a7dde8059f1a6838189d1f

BUG: disconnect the reader's pipeline so that the data can be a 'clean source' for the rest of the rendering pipeline
git-svn-id: http://svn.slicer.org/Slicer4/trunk@18464 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/vtkMRMLVolumeArchetypeStorageNode.cxx
@@ -372,6 +372,7 @@ int vtkMRMLVolumeArchetypeStorageNode::ReadData(vtkMRMLNode *refNode)
   else
     {
     volNode->SetAndObserveImageData (ici->GetOutput());
+    ici->GetOutput()->SetSource(0);
     volNode->ModifiedSinceReadOff();
     }
 

commit 098bfa3af3ea9eb0867ce72fd2c1087c13d7f61c

BUG: Correctly detach vtkImageData from pipeline
Setting the source to 0 from a vtkImageData doesn't "detach" correctly the image data from the pipeline.
As a side effect, vtkImageData::GetScalarType couldn't retrieve its scalar info and was returning the wrong scalar type.
Bug introduced by r18464
Closes #1524

git-svn-id: http://svn.slicer.org/Slicer4/trunk@18609 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/vtkMRMLVolumeArchetypeStorageNode.cxx
@@ -371,8 +371,10 @@ int vtkMRMLVolumeArchetypeStorageNode::ReadData(vtkMRMLNode *refNode)
     }
   else
     {
-    volNode->SetAndObserveImageData (ici->GetOutput());
-    ici->GetOutput()->SetSource(0);
+    vtkSmartPointer<vtkImageData> iciOutputCopy =
+      vtkSmartPointer<vtkImageData>::New();
+    iciOutputCopy->ShallowCopy(ici->GetOutput());
+    volNode->SetAndObserveImageData(iciOutputCopy.GetPointer());
     volNode->ModifiedSinceReadOff();
     }
 
@@ -386,7 +388,7 @@ int vtkMRMLVolumeArchetypeStorageNode::ReadData(vtkMRMLNode *refNode)
   reader->RemoveObservers( vtkCommand::ProgressEvent,  this->MRMLCallbackCommand);
 
   this->SetReadStateIdle();
-  
+
   return result;
 }
 

QTModules/Volumes/Testing/Cxx/CMakeLists.txt
@@ -9,6 +9,7 @@ set(CMAKE_TESTDRIVER_BEFORE_TESTMAIN "DEBUG_LEAKS_ENABLE_EXIT_ERROR();" )
 create_test_sourcelist(Tests ${KIT}CxxTests.cxx
   ${KIT_TEST_NAMES_CXX}
   qSlicerVolumesModuleWidgetTest1.cxx
+  vtkSlicerVolumesLogicTest1.cxx
   EXTRA_INCLUDE TestingMacros.h
   )
 list(REMOVE_ITEM Tests ${KIT_TEST_NAMES_CXX})
@@ -29,3 +30,4 @@ endmacro()
 set(VOLUME_FILE "${MRML_SOURCE_DIR}/Testing/TestData/fixed.nrrd")
 
 SIMPLE_TEST( qSlicerVolumesModuleWidgetTest1 ${VOLUME_FILE} )
+SIMPLE_TEST( vtkSlicerVolumesTest1 ${VOLUME_FILE} )

QTModules/Volumes/Testing/Cxx/vtkSlicerVolumesLogicTest1.cxx
@@ -1,0 +1,97 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) Kitware Inc.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Julien Finet, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+// Volumes logic
+#include "vtkSlicerVolumesLogic.h"
+
+// MRML includes
+#include <vtkMRMLScalarVolumeNode.h>
+#include <vtkMRMLScene.h>
+
+// VTK includes
+#include <vtkDataSetAttributes.h>
+#include <vtkImageData.h>
+#include <vtkNew.h>
+
+//-----------------------------------------------------------------------------
+bool isImageDataValid(vtkImageData* imageData)
+{
+  if (!imageData)
+    {
+    return false;
+    }
+  imageData->GetProducerPort();
+  vtkInformation* info = imageData->GetPipelineInformation();
+  if (!info)
+    {
+    return false;
+    }
+  vtkInformation *scalarInfo = vtkDataObject::GetActiveFieldInformation(info,
+    vtkDataObject::FIELD_ASSOCIATION_POINTS, vtkDataSetAttributes::SCALARS);
+  if (!scalarInfo)
+    {
+    return false;
+    }
+  return true;
+}
+
+//-----------------------------------------------------------------------------
+int vtkSlicerVolumesLogicTest1( int argc, char * argv[] )
+{
+  vtkNew<vtkMRMLScene> scene;
+  vtkNew<vtkSlicerVolumesLogic> logic;
+
+  if (argc < 2)
+    {
+    std::cerr << "Usage: qSlicerVolumesModuleWidgetTest1 volumeName [-I]" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  logic->SetMRMLScene(scene.GetPointer());
+
+  vtkMRMLScalarVolumeNode* scalarVolume =
+    logic->AddArchetypeScalarVolume(argv[1], "scalarVolume");
+
+  if (!scalarVolume ||
+      !isImageDataValid(scalarVolume->GetImageData()))
+    {
+    std::cerr << "Failed to load scalar image." << std::endl;
+    if (scalarVolume && scalarVolume->GetImageData())
+      {
+      scalarVolume->GetImageData()->Print(std::cerr);
+      }
+    return EXIT_FAILURE;
+    }
+
+  vtkMRMLVolumeNode* volume =
+    logic->AddArchetypeVolume(argv[1], "volume", 0);
+
+  if (!volume ||
+      !isImageDataValid(volume->GetImageData()))
+    {
+    std::cerr << "Failed to load scalar image." << std::endl;
+    if (volume && volume->GetImageData())
+      {
+      volume->GetImageData()->Print(std::cerr);
+      }
+    return EXIT_FAILURE;
+    }
+  return EXIT_SUCCESS;
+}