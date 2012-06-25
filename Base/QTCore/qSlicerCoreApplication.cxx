NA-MIC/svn.slicer.org-Slicer4

commit 33add28c5a5c32e12c9c41ea0b892885df44322a

BUG: Ensure generic loadable module test compiled externally can be executed
There are two cases:
 1) Test compiled within Slicer build tree: located along with SlicerApp-real
 2) Test compiled outside of Slicer build tree

In the second case, when qSlicer(Core)Application is instantiated within
a test, the applicationDirPath resolved to the test executable itself.
The method "QCoreApplication::applicationDirPath" being used, it was NOT
returning the SlicerApp-real path.
This was causing problem and the SlicerBin could NOT be computed adequately:

1: ASSERT: "qSlicerUtils::pathEndsWith(this->SlicerBin, Slicer_BIN_DIR)" in file /Users/millerjv/Projects/Slicer4-git/Slicer4/Base/QTCore/qSlicerCoreApplication.cxx, line 414


The implemented solution attempts to compute the SlicerBin only if SLICER_HOME
hasn't been set. The launcher taking care of setting this environment
variable, test instantiating a qSlicer(Core)Application will work
as expected.

See http://www.na-mic.org/Bug/view.php?id=2248

Fixes #2248

git-svn-id: http://svn.slicer.org/Slicer4/trunk@20456 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTCore/qSlicerCoreApplication.cxx
@@ -148,7 +148,6 @@ void qSlicerCoreApplicationPrivate::init()
     {
     qDebug() << "qSlicerCoreApplication must be given the True argc/argv";
     }
-  this->discoverSlicerBinDirectory();
 
   this->SlicerHome = this->discoverSlicerHomeDirectory();
   this->setEnvironmentVariable("SLICER_HOME", this->SlicerHome);
@@ -296,8 +295,6 @@ QSettings* qSlicerCoreApplicationPrivate::instantiateSettings(const QString& suf
 //-----------------------------------------------------------------------------
 QString qSlicerCoreApplicationPrivate::discoverSlicerHomeDirectory()
 {
-  Q_Q(qSlicerCoreApplication);
-
   // Since some standalone executable (i.e EMSegmentCommandLine) can create
   // an instance of qSlicer(Core)Application so that the environment and the
   // python manager are properly initialized. This executable will have
@@ -307,10 +304,11 @@ QString qSlicerCoreApplicationPrivate::discoverSlicerHomeDirectory()
   QString slicerHome = this->Environment.value("SLICER_HOME");
   if (!slicerHome.isEmpty())
     {
-#ifndef Q_OS_MAC
+#ifdef Q_OS_WIN32
+    Q_Q(qSlicerCoreApplication);
     if (this->IntDir.isEmpty())
       {
-      // Additionally, in an attempt to compute InDir, let's note that its value
+      // Additionally, in an attempt to compute IntDir, let's note that its value
       // is first set with the help of "pathWithoutIntDir" in the method
       // "discoverSlicerBinDirectory" happening before this call.
       // When first called, the parameters to "pathWithoutIntDir" are "applicationDirPath" and "Slicer_BIN_DIR".
@@ -323,13 +321,17 @@ QString qSlicerCoreApplicationPrivate::discoverSlicerHomeDirectory()
     //qDebug() << "qSlicerCoreApplication: SLICER_HOME externally set to" << slicerHome;
     return slicerHome;
     }
-  QDir slicerBinDir(this->SlicerBin);
-  bool cdUpRes = slicerBinDir.cdUp();
-  if (!cdUpRes)
+  else
     {
-    qDebug() << "Warning, can't cdUp in " << slicerBinDir;
+    this->discoverSlicerBinDirectory();
+    QDir slicerBinDir(this->SlicerBin);
+    bool cdUpRes = slicerBinDir.cdUp();
+    if (!cdUpRes)
+      {
+      qDebug() << "Warning, can't cdUp in " << slicerBinDir;
+      }
+    return slicerBinDir.canonicalPath();
     }
-  return slicerBinDir.canonicalPath();
 }
 
 //-----------------------------------------------------------------------------
@@ -418,8 +420,8 @@ void qSlicerCoreApplicationPrivate::discoverSlicerBinDirectory()
     slicerBinAsDir.cd(Slicer_BIN_DIR);
     }
   this->SlicerBin = slicerBinAsDir.path();
-  Q_ASSERT(qSlicerUtils::pathEndsWith(this->SlicerBin, Slicer_BIN_DIR));
 #endif
+  Q_ASSERT(qSlicerUtils::pathEndsWith(this->SlicerBin, Slicer_BIN_DIR));
 }
 
 //-----------------------------------------------------------------------------

commit 5c2c17353aa2f525ab646755bfffae6507f311a8

BUG: Ensure intDir is properly computed
This fix a regression introduced by r20456

See http://www.na-mic.org/Bug/view.php?id=2248

Fixes #2248

git-svn-id: http://svn.slicer.org/Slicer4/trunk@20479 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTCore/qSlicerCoreApplication.cxx
@@ -293,6 +293,12 @@ QSettings* qSlicerCoreApplicationPrivate::instantiateSettings(const QString& suf
 }
 
 //-----------------------------------------------------------------------------
+bool qSlicerCoreApplicationPrivate::isInstalled(const QString& slicerHome)const
+{
+  return !QFile::exists(slicerHome + "/CMakeCache.txt");
+}
+
+//-----------------------------------------------------------------------------
 QString qSlicerCoreApplicationPrivate::discoverSlicerHomeDirectory()
 {
   // Since some standalone executable (i.e EMSegmentCommandLine) can create
@@ -302,36 +308,29 @@ QString qSlicerCoreApplicationPrivate::discoverSlicerHomeDirectory()
   // executable will be considered and initialization code expecting SLICER_HOME
   // to be properly set will fail.
   QString slicerHome = this->Environment.value("SLICER_HOME");
-  if (!slicerHome.isEmpty())
+  if (slicerHome.isEmpty())
     {
+    QString slicerBin = this->discoverSlicerBinDirectory();
+    QDir slicerBinDir(slicerBin);
+    bool cdUpRes = slicerBinDir.cdUp();
+    Q_ASSERT(cdUpRes);
+    slicerHome = slicerBinDir.canonicalPath();
+    }
+
 #ifdef Q_OS_WIN32
-    Q_Q(qSlicerCoreApplication);
+  Q_Q(qSlicerCoreApplication);
+  if (this->IntDir.isEmpty())
+    {
+    qSlicerUtils::pathWithoutIntDir(q->applicationDirPath(), Slicer_BIN_DIR, this->IntDir);
     if (this->IntDir.isEmpty())
       {
-      // Additionally, in an attempt to compute IntDir, let's note that its value
-      // is first set with the help of "pathWithoutIntDir" in the method
-      // "discoverSlicerBinDirectory" happening before this call.
-      // When first called, the parameters to "pathWithoutIntDir" are "applicationDirPath" and "Slicer_BIN_DIR".
-      // If set to an empty value, we should then consider the case of command line module,
-      // by trying with "Slicer_CLIMODULES_BIN_DIR".
       qSlicerUtils::pathWithoutIntDir(q->applicationDirPath(), Slicer_CLIMODULES_BIN_DIR, this->IntDir);
       }
+    }
+  Q_ASSERT(this->isInstalled(slicerHome) ? this->IntDir.isEmpty() : !this->IntDir.isEmpty());
 #endif
 
-    //qDebug() << "qSlicerCoreApplication: SLICER_HOME externally set to" << slicerHome;
-    return slicerHome;
-    }
-  else
-    {
-    this->discoverSlicerBinDirectory();
-    QDir slicerBinDir(this->SlicerBin);
-    bool cdUpRes = slicerBinDir.cdUp();
-    if (!cdUpRes)
-      {
-      qDebug() << "Warning, can't cdUp in " << slicerBinDir;
-      }
-    return slicerBinDir.canonicalPath();
-    }
+  return slicerHome;
 }
 
 //-----------------------------------------------------------------------------
@@ -389,18 +388,19 @@ void qSlicerCoreApplicationPrivate::updateEnvironmentVariable(const QString& key
 }
 
 //-----------------------------------------------------------------------------
-void qSlicerCoreApplicationPrivate::discoverSlicerBinDirectory()
+QString qSlicerCoreApplicationPrivate::discoverSlicerBinDirectory()
 {
   Q_Q(qSlicerCoreApplication);
+  QString slicerBin;
   // Note: On Linux, QCoreApplication::applicationDirPath() will attempt
   //       to get the path using the "/proc" filesystem.
   if (!QFile::exists(q->applicationDirPath()))
     {
     qCritical() << "Cannot find Slicer executable" << q->applicationDirPath();
-    return ;
+    return slicerBin;
     }
 #ifndef Q_OS_MAC
-  this->SlicerBin =
+  slicerBin =
       qSlicerUtils::pathWithoutIntDir(q->applicationDirPath(), Slicer_BIN_DIR, this->IntDir);
 #else
   // There are two cases to consider, the application could be started from:
@@ -419,9 +419,10 @@ void qSlicerCoreApplicationPrivate::discoverSlicerBinDirectory()
     slicerBinAsDir.cdUp(); // Move from /path/to/build-dir/bin/Foo.app          to /path/to/build-dir/bin
     slicerBinAsDir.cd(Slicer_BIN_DIR);
     }
-  this->SlicerBin = slicerBinAsDir.path();
+  slicerBin = slicerBinAsDir.path();
 #endif
-  Q_ASSERT(qSlicerUtils::pathEndsWith(this->SlicerBin, Slicer_BIN_DIR));
+  Q_ASSERT(qSlicerUtils::pathEndsWith(slicerBin, Slicer_BIN_DIR));
+  return slicerBin;
 }
 
 //-----------------------------------------------------------------------------
@@ -889,7 +890,7 @@ CTK_GET_CPP(qSlicerCoreApplication, QString, intDir, IntDir);
 bool qSlicerCoreApplication::isInstalled()const
 {
   Q_D(const qSlicerCoreApplication);
-  return !QFile::exists(d->SlicerHome + "/CMakeCache.txt");
+  return d->isInstalled(d->SlicerHome);
 }
 
 //-----------------------------------------------------------------------------

Base/QTCore/qSlicerCoreApplication_p.h
@@ -56,6 +56,9 @@ public:
   /// Instanciate settings object
   QSettings* instantiateSettings(const QString& suffix, bool useTmp);
 
+  /// Return true is this instance of Slicer is running from an installed directory
+  bool isInstalled(const QString& slicerHome)const;
+
   /// Given the program name, should return Slicer Home Directory
   QString discoverSlicerHomeDirectory();
 
@@ -74,9 +77,10 @@ public:
   void updateEnvironmentVariable(
     const QString& key, const QString& value, QChar separator, bool prepend = false);
 
-  /// If it successfully obtains 'applicationDirPath()', sets the variable 'SlicerBin'
+  /// If it successfully obtains 'applicationDirPath()', returns Slicer binary directory
+  /// \note SlicerBin doesn't contain Debug/Release/... (see IntDir)
   /// \sa QCoreApplication::applicationDirPath
-  void discoverSlicerBinDirectory();
+  QString discoverSlicerBinDirectory();
 
   /// Set 'ITKFactoriesDir' variable using 'ITK_AUTOLOAD_PATH' environment variable
   QString discoverITKFactoriesDirectory();
@@ -110,8 +114,6 @@ public:
   /// Data manager
   vtkSmartPointer<vtkDataIOManagerLogic>      DataIOManagerLogic;
 
-  /// SlicerBin doesn't contain Debug/Release/... (see IntDir)
-  QString                                     SlicerBin;
   QString                                     SlicerHome;
   QString                                     ITKFactoriesDir;
   /// On windows platform, after the method 'discoverSlicerBinDirectory' has been called,