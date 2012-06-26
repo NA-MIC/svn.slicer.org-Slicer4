NA-MIC/svn.slicer.org-Slicer4

commit 18baaf16779fe1880026ba4f32c9eb4eb5f556ee

COMP: The compiler options must be propogated
Compiler options selected in the main Slicer4
Superbuild harness needs to be propogated to
each of the subsequent External_project builds.

This was tested by setting
export CC=/dev/null
export CXX=/dev/null
cmake -DCMAKE_C_COMPILER:PATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:PATH=/usr/bin/g++
and then compiling all of Slicer.

Propogate the necessary compiler flags to each
external package consistently.  Additionally,
only propogate the flags that are explicitly needed
for that external project.  In many cases, only the
C compiler options need to be propogated.

Slicer compiler options needed by Extensions as well.
The extension need to have the same compiler used
as the primary Slicer build.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@20464 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_LibArchive.cmake
@@ -6,6 +6,11 @@ if(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED)
 endif()
 set(${CMAKE_CURRENT_LIST_FILENAME}_FILE_INCLUDED 1)
 
+# Sanity checks
+if(DEFINED LibArchive_DIR AND NOT EXISTS ${LibArchive_DIR})
+  message(FATAL_ERROR "LibArchive_DIR variable is defined but corresponds to non-existing directory")
+endif()
+
 # Set dependency list
 set(LibArchive_DEPENDENCIES "")
 if(WIN32)
@@ -15,6 +20,7 @@ endif()
 # Include dependent projects if any
 SlicerMacroCheckExternalProjectDependency(LibArchive)
 set(proj LibArchive)
+
 if(NOT DEFINED LibArchive_DIR)
   #message(STATUS "${__indent}Adding project ${proj}")
   #
@@ -32,19 +38,6 @@ if(NOT DEFINED LibArchive_DIR)
       -DZLIB_LIBRARY:FILEPATH=${zlib_DIR}/lib/zlib.lib
       -DZLIB_ROOT:PATH=${zlib_DIR}
       )
-  else()
-    if(${CMAKE_VERSION} VERSION_GREATER "2.8.5")
-      # Note that CMAKE_DISABLE_FIND_PACKAGE_* is only supported in CMake >= 2.8.6
-      list(APPEND ADDITIONAL_CMAKE_ARGS
-        -DCMAKE_DISABLE_FIND_PACKAGE_OpenSSL:BOOL=TRUE
-        )
-      message(STATUS "${proj} - Configured *WITHOUT* OpenSSL support.")
-    else()
-      message(STATUS "${proj} - Configured *WITH* OpenSSL support.")
-    endif()
-    list(APPEND ADDITIONAL_CMAKE_ARGS
-      -DCRYPTO_LIBRARY:FILEPATH=
-      )
   endif()
 
   # Set CMake OSX variable to pass down the external project
@@ -56,18 +49,25 @@ if(NOT DEFINED LibArchive_DIR)
       -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
   endif()
 
+  if(NOT DEFINED git_protocol)
+    set(git_protocol "git")
+  endif()
+
   ExternalProject_Add(${proj}
     GIT_REPOSITORY "${git_protocol}://github.com/libarchive/libarchive.git"
     GIT_TAG "v3.0.4"
-    SOURCE_DIR LibArchive
-    BINARY_DIR LibArchive-build
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BINARY_DIR ${proj}-build
+    UPDATE_COMMAND ""
     INSTALL_DIR LibArchive-install
     CMAKE_GENERATOR ${gen}
     CMAKE_ARGS
+    # Not used -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+    # Not used -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+      -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+      -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
       ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
       -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-      # -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags} # Not used
-      -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
       -DBUILD_SHARED_LIBS:BOOL=ON
       -DENABLE_ACL:BOOL=OFF
       -DENABLE_CPIO:BOOL=OFF

CMake/SlicerConfig.cmake.in
@@ -206,7 +206,7 @@ macro(slicer_config_set_ep var value)
   endif()
 endmacro()
 
-# For sake of simplification, the macro 'slicer_config_set_ep' is not invoked confitionally, if
+# For sake of simplification, the macro 'slicer_config_set_ep' is not invoked conditionally, if
 # the configured 'value' parameter is an empty string, the macro 'slicer_config_set_ep' is a no-op.
 
 slicer_config_set_ep(
@@ -286,6 +286,49 @@ slicer_config_set_ep(
   VTK_DIR
   "@VTK_DIR_CONFIG@"
   CACHE PATH "Path to VTK build or install directory")
+
+# --------------------------------------------------------------------------
+# Consistent Compiler Selections
+# --------------------------------------------------------------------------
+# Compilation Commands
+set(Slicer_CMAKE_CXX_COMPILER "@Slicer_CMAKE_CXX_COMPILER_CONFIG@")
+set(Slicer_CMAKE_C_COMPILER   "@Slicer_CMAKE_C_COMPILER_CONFIG@")
+set(Slicer_CMAKE_CXX_FLAGS    "@Slicer_CMAKE_CXX_FLAGS_CONFIG@")
+set(Slicer_CMAKE_C_FLAGS      "@Slicer_CMAKE_C_FLAGS_CONFIG@")
+
+# With the help of SuperBuild, Slicer has been configured with specific compiler
+#
+# Considering that, by default, Slicer and most of its dependent external projects are
+# built with a particular compiler, it's important to make sure that the project including
+# SlicerConfig.cmake (this file) is built against the same compliant compiler
+#
+# A typical use of this macro is illustrated below:
+#    slicer_config_set_compiler_ep("CMAKE_C_COMPILER", "@Slicer_CMAKE_C_COMPILER@")
+#    slicer_config_set_compiler_ep("CMAKE_CXX_COMPILER", "@Slicer_CMAKE_CXX_COMPILER@")
+#
+macro(slicer_config_set_compiler_ep var value)
+  if(NOT "${value}" STREQUAL "")
+    if(DEFINED ${var})
+      get_filename_component(var_realpath "${${var}}" REALPATH)
+      get_filename_component(value_realpath ${value} REALPATH)
+      if(NOT ${var_realpath} STREQUAL ${value_realpath})
+        message(FATAL_ERROR "Variable ${var} defined prior calling 'find_package(Slicer)' does NOT "
+                            "match value used to configure Slicer. It probably means that a different "
+                            "${var} has been used to configure this project and Slicer.\n"
+                            "${var}=${${var}}\n"
+                            "Slicer_${var}=${value}")
+      endif()
+    endif()
+    set(${var} "${value}" ${ARGN})
+  endif()
+endmacro()
+slicer_config_set_compiler_ep( CMAKE_C_COMPILER   "@Slicer_CMAKE_C_COMPILER_CONFIG@"
+   CACHE PATH "Path to C compiler used in Slicer build" FORCE )
+slicer_config_set_compiler_ep( CMAKE_CXX_COMPILER "@Slicer_CMAKE_CXX_COMPILER_CONFIG@"
+   CACHE PATH "Path to CXX compiler used in Slicer build" FORCE )
+
+set(CMAKE_CXX_FLAGS    "@Slicer_CMAKE_CXX_FLAGS_CONFIG@")
+set(CMAKE_C_FLAGS      "@Slicer_CMAKE_C_FLAGS_CONFIG@")
 
 # List all required external project
 set(Slicer_EXTERNAL_PROJECTS @Slicer_EXTERNAL_PROJECTS_CONFIG@)

CMake/SlicerGenerateSlicerConfig.cmake
@@ -20,6 +20,12 @@
 
 set(Slicer_USE_FILE_CONFIG ${Slicer_BINARY_DIR}/UseSlicer.cmake)
 
+# Compilation settings
+set(Slicer_CMAKE_CXX_COMPILER_CONFIG ${CMAKE_CXX_COMPILER})
+set(Slicer_CMAKE_CXX_FLAGS_CONFIG    ${ep_common_cxx_flags})
+set(Slicer_CMAKE_C_COMPILER_CONFIG   ${CMAKE_C_COMPILER})
+set(Slicer_CMAKE_C_FLAGS_CONFIG      ${ep_common_c_flags})
+
 # Launcher command
 set(Slicer_LAUNCHER_EXECUTABLE_CONFIG ${Slicer_LAUNCHER_EXECUTABLE})
 set(Slicer_LAUNCH_COMMAND_CONFIG ${Slicer_LAUNCH_COMMAND})

SuperBuild.cmake
@@ -45,13 +45,6 @@ set(ep_base        "${CMAKE_BINARY_DIR}")
 set(ep_common_c_flags "${CMAKE_C_FLAGS_INIT} ${ADDITIONAL_C_FLAGS}")
 set(ep_common_cxx_flags "${CMAKE_CXX_FLAGS_INIT} ${ADDITIONAL_CXX_FLAGS}")
 
-set(ep_common_compiler_args
-  -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
-  -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
-  -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
-  -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
-  )
-
 # Compute -G arg for configuring external projects with the same CMake generator:
 if(CMAKE_EXTRA_GENERATOR)
   set(gen "${CMAKE_EXTRA_GENERATOR} - ${CMAKE_GENERATOR}")
@@ -308,7 +301,12 @@ ExternalProject_Add(${proj}
   SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
   BINARY_DIR Slicer-build
   CMAKE_GENERATOR ${gen}
+  UPDATE_COMMAND ""
   CMAKE_ARGS
+    -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+    -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+    -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+    -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
     ${ep_superbuild_boolean_args}
     ${ep_superbuild_extra_args}
     ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}

SuperBuild/External_BatchMake.cmake
@@ -35,7 +35,10 @@ ExternalProject_Add(${proj}
   UPDATE_COMMAND ""
   CMAKE_GENERATOR ${gen}
   CMAKE_ARGS
-    ${ep_common_compiler_args}
+    -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+    -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+    -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+    -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
     ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     -DBUILD_TESTING:BOOL=OFF

SuperBuild/External_CLAPACK.cmake
@@ -54,7 +54,10 @@ ExternalProject_Add(${proj}
   URL_MD5 ${CLAPACK_MD5}
   CMAKE_GENERATOR ${gen}
   CMAKE_ARGS
-    ${ep_common_compiler_args}
+    -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+    -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+    -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+    -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
     ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     -DBUILD_TESTING:BOOL=OFF

SuperBuild/External_CTK.cmake
@@ -63,7 +63,10 @@ if(NOT DEFINED CTK_DIR)
     UPDATE_COMMAND ""
     CMAKE_GENERATOR ${gen}
     CMAKE_ARGS
-      ${ep_common_compiler_args}
+      -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+      -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+      -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+      -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
       ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
       -DADDITIONAL_C_FLAGS:STRING=${ADDITIONAL_C_FLAGS}
       -DADDITIONAL_CXX_FLAGS:STRING=${ADDITIONAL_CXX_FLAGS}

SuperBuild/External_ITKv3.cmake
@@ -47,7 +47,10 @@ if(NOT DEFINED ITK_DIR)
     BINARY_DIR ${proj}-build
     CMAKE_GENERATOR ${gen}
     CMAKE_ARGS
-      ${ep_common_compiler_args}
+      -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+      -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+      -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+      -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
       ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
       -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
       -DBUILD_EXAMPLES:BOOL=OFF

commit 5947b0c7e56a4e03dae9bee21a201d41d5343b54

COMP: LibArchive is built without OpenSSL on all platform. Fix regression introduced by r20464
git-svn-id: http://svn.slicer.org/Slicer4/trunk@20491 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_LibArchive.cmake
@@ -24,21 +24,24 @@ set(proj LibArchive)
 if(NOT DEFINED LibArchive_DIR)
   #message(STATUS "${__indent}Adding project ${proj}")
   #
-  # NOTE: - a stable, recent release (3.0.4) of LibArchive is now checked out from git 
-  #         for all platforms.  For notes on cross-platform issues with earlier versions 
+  # NOTE: - a stable, recent release (3.0.4) of LibArchive is now checked out from git
+  #         for all platforms.  For notes on cross-platform issues with earlier versions
   #         of LibArchive, see the repository for earlier revisions of this file.
   #
   set(ADDITIONAL_CMAKE_ARGS)
   if(WIN32)
     # CMake arguments specific to LibArchive >= 2.8.4
     list(APPEND ADDITIONAL_CMAKE_ARGS
-      -DBUILD_TESTING:BOOL=OFF
-      -DENABLE_OPENSSL:BOOL=OFF
       -DZLIB_INCLUDE_DIR:PATH=${zlib_DIR}/include
       -DZLIB_LIBRARY:FILEPATH=${zlib_DIR}/lib/zlib.lib
       -DZLIB_ROOT:PATH=${zlib_DIR}
       )
   endif()
+  # CMake arguments specific to LibArchive >= 2.8.4
+  list(APPEND ADDITIONAL_CMAKE_ARGS
+    -DBUILD_TESTING:BOOL=OFF
+    -DENABLE_OPENSSL:BOOL=OFF
+    )
 
   # Set CMake OSX variable to pass down the external project
   set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)