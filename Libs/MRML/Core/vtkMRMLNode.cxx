NA-MIC/svn.slicer.org-Slicer4

commit e5dd6b6b6d49496e58b2c72b9b61b166fe12a6af

BUG: 3436. Fixed node references to use predefined events. Made reference code similar between Diaplayable and Transformable references. This is also continuation of work on bug 3388.
git-svn-id: http://svn.slicer.org/Slicer4/trunk@22603 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkMRMLNode.cxx
@@ -1042,7 +1042,14 @@ void vtkMRMLNode::UpdateNthNodeReference(vtkMRMLNodeReference *reference, int n)
   vtkMRMLNode* node = this->GetScene() ?
     this->GetScene()->GetNodeByID(reference->GetReferencedNodeID()) : 0;
 
-  this->SetAndObserveNthNodeReference(reference->GetReferenceRole(), n, node, reference->Events);
+  vtkIntArray *events = reference->Events;
+  const char* referenceRole = reference->GetReferenceRole();
+  if (events == 0 && this->NodeReferenceEvents[referenceRole] && this->NodeReferenceEvents[referenceRole]->GetNumberOfTuples() > 0)
+    {
+    events = NodeReferenceEvents[referenceRole];
+    }
+
+  this->SetAndObserveNthNodeReference(reference->GetReferenceRole(), n, node, events);
   reference->ReferencedNode = node;
   reference->ReferencingNode = this;
 }
@@ -1260,6 +1267,7 @@ void vtkMRMLNode::UpdateNthNodeReference(const char* referenceRole, int n)
   if (newReferencedNodeID.empty())
     {
     /// Need to unobserve
+    (*referencedNodesIt)->SetReferencedNodeID(0);
     this->SetAndObserveNthNodeReference(referenceRole, n, 0, (*referencedNodesIt)->Events);
     vtkMRMLNodeReference *tmp = (*referencedNodesIt);
     referencedNodes.erase(referencedNodesIt);

Libs/MRML/Core/Testing/vtkMRMLTransformableNodeOnNodeReferenceAddTest.cxx
@@ -98,7 +98,7 @@ int vtkMRMLTransformableNodeOnNodeReferenceAddTest(int , char * [] )
               << callback->GetErrorString().c_str() << " "
               << "Number of ModifiedEvent: " << callback->GetNumberOfModified() << " "
               << "Number of TransformModifiedEvent: "
-              << callback->GetNumberOfEvents(vtkMRMLTransformNode::TransformModifiedEvent)
+              << callback->GetNumberOfEvents(vtkMRMLTransformableNode::TransformModifiedEvent)
               << std::endl;
     return EXIT_FAILURE;
     }

Libs/MRML/Core/vtkMRMLDisplayableNode.h
@@ -249,7 +249,10 @@ public:
   virtual void OnNodeReferenceModified(vtkMRMLNodeReference *reference)
   {
     Superclass::OnNodeReferenceModified(reference);
-    this->InvokeEvent(vtkMRMLDisplayableNode::DisplayModifiedEvent, reference->ReferencedNode);
+    if (std::string(reference->GetReferenceRole()) == this->DisplayNodeReferenceRole)
+    {
+      this->InvokeEvent(vtkMRMLDisplayableNode::DisplayModifiedEvent, reference->ReferencedNode);
+    }
   }
 
   ///
@@ -257,7 +260,10 @@ public:
   virtual void OnNodeReferenceRemoved(vtkMRMLNodeReference *reference)
   {
     Superclass::OnNodeReferenceRemoved(reference);
-    //this->InvokeEvent(vtkMRMLDisplayableNode::DisplayModifiedEvent, reference->ReferencedNode);
+    if (std::string(reference->GetReferenceRole()) == this->DisplayNodeReferenceRole)
+    {
+      this->InvokeEvent(vtkMRMLDisplayableNode::DisplayModifiedEvent, reference->ReferencedNode);
+    }
   }
 
 

Libs/MRML/Core/vtkMRMLTransformableNode.cxx
@@ -123,21 +123,6 @@ void vtkMRMLTransformableNode::SetAndObserveTransformNodeID(const char *transfor
   this->SetAndObserveNodeReferenceID(this->GetTransformNodeReferenceRole(), transformNodeID);
 }
 
-//----------------------------------------------------------------------------
-void vtkMRMLTransformableNode::OnNodeReferenceAdded(vtkMRMLNodeReference *reference)
-{
-  Superclass::OnNodeReferenceAdded(reference);
-
-  // When a scene is loaded, vtkMRMLScene::UpdateNodeReferences doesn't handle
-  // event registration. Except for vtkCommand::ModifiedEvent, any other events
-  // need to be registered via OnNodeReferenceAdded API.
-  if (std::string(reference->GetReferenceRole()) == this->GetTransformNodeReferenceRole()
-     && reference->ReferencedNode && reference->ReferencingNode)
-    {
-    vtkEventBroker::GetInstance()->AddObservation(reference->ReferencedNode,
-      vtkMRMLTransformableNode::TransformModifiedEvent, reference->ReferencingNode, this->MRMLCallbackCommand);
-    }
-}
 
 //---------------------------------------------------------------------------
 void vtkMRMLTransformableNode::ProcessMRMLEvents ( vtkObject *caller,

Libs/MRML/Core/vtkMRMLTransformableNode.h
@@ -105,8 +105,39 @@ protected:
   virtual const char* GetTransformNodeReferenceRole();
   virtual const char* GetTransformNodeReferenceMRMLAttributeName();
 
+  ///
   /// Called when a node reference ID is added (list size increased).
-  virtual void OnNodeReferenceAdded(vtkMRMLNodeReference *reference);
+  virtual void OnNodeReferenceAdded(vtkMRMLNodeReference *reference)
+  {
+    Superclass::OnNodeReferenceAdded(reference);
+    if (std::string(reference->GetReferenceRole()) == this->TransformNodeReferenceRole)
+      {
+      this->InvokeEvent(vtkMRMLTransformableNode::TransformModifiedEvent, reference->ReferencedNode);
+      }
+  }
+
+  ///
+  /// Called when a node reference ID is modified.
+  virtual void OnNodeReferenceModified(vtkMRMLNodeReference *reference)
+  {
+    Superclass::OnNodeReferenceModified(reference);
+    if (std::string(reference->GetReferenceRole()) == this->TransformNodeReferenceRole)
+    {
+      this->InvokeEvent(vtkMRMLTransformableNode::TransformModifiedEvent, reference->ReferencedNode);
+    }
+  }
+
+  ///
+  /// Called after a node reference ID is removed (list size decreased).
+  virtual void OnNodeReferenceRemoved(vtkMRMLNodeReference *reference)
+  {
+    Superclass::OnNodeReferenceRemoved(reference);
+    if (std::string(reference->GetReferenceRole()) == this->TransformNodeReferenceRole)
+    {
+      this->InvokeEvent(vtkMRMLTransformableNode::TransformModifiedEvent, reference->ReferencedNode);
+    }
+  }
+
 
 private:
   char* TransformNodeIDInternal;

commit 117ffc83bc770abc33af2ed368ac2e38f561b11b

BUG: fixed problems and test failures introduced by r22603.
git-svn-id: http://svn.slicer.org/Slicer4/trunk@22604 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkMRMLNode.cxx
@@ -1441,7 +1441,15 @@ int vtkMRMLNode::GetNumberOfNodeReferences(const char* referenceRole)
   int n=0;
   if (referenceRole)
     {
-    n = static_cast<int>(this->NodeReferences[std::string(referenceRole)].size());
+    std::vector< vtkMRMLNodeReference *> &references = this->NodeReferences[std::string(referenceRole)];
+    std::vector< vtkMRMLNodeReference *>::iterator it;
+    for (it = references.begin(); it != references.end(); it++)
+      {
+      if ((*it)->GetReferencedNodeID() != 0)
+        {
+        n++;
+        }
+      }
     }
   return n;
 }

Libs/MRML/DisplayableManager/vtkMRMLModelDisplayableManager.cxx
@@ -442,13 +442,17 @@ void vtkMRMLModelDisplayableManager::ProcessMRMLNodesEvents(vtkObject *caller,
                                                            unsigned long event,
                                                            void *callData)
 {
+  if (this->GetMRMLScene() == 0)
+    {
+    return;
+    }
   if ( this->GetInteractor() &&
      this->GetInteractor()->GetRenderWindow() &&
      this->GetInteractor()->GetRenderWindow()->CheckInRenderStatus())
-  {
+    {
     vtkDebugMacro("skipping ProcessMRMLNodesEvents during render");
     return;
-  }
+    }
 
   bool isUpdating = this->GetMRMLScene()->IsBatchProcessing();
   if (vtkMRMLDisplayableNode::SafeDownCast(caller))

Libs/MRML/Core/Testing/vtkMRMLDisplayableNodeTest1.cxx
@@ -588,16 +588,16 @@ bool TestDisplayModifiedEvent()
 
   displayableNode->SetAndObserveDisplayNodeID(0);
 
-  if (spy->GetTotalNumberOfEvents() != 2 ||
+  if (spy->GetTotalNumberOfEvents() != 3 ||
       spy->GetNumberOfEvents(vtkCommand::ModifiedEvent) != 1 ||
-      spy->GetNumberOfEvents(vtkMRMLNode::ReferenceRemovedEvent) != 1) //||
-      //spy->GetNumberOfEvents(vtkMRMLDisplayableNode::DisplayModifiedEvent) != 1)
+      spy->GetNumberOfEvents(vtkMRMLNode::ReferenceRemovedEvent) != 1 ||
+      spy->GetNumberOfEvents(vtkMRMLDisplayableNode::DisplayModifiedEvent) != 1)
     {
     std::cout << __LINE__ << ": SetAndObserveDisplayNodeID failed:" << std::endl
               << spy->GetTotalNumberOfEvents() << " "
               << spy->GetNumberOfEvents(vtkCommand::ModifiedEvent) << " "
-              << spy->GetNumberOfEvents(vtkMRMLNode::ReferenceRemovedEvent) << " "<< std::endl;
-              //<< spy->GetNumberOfEvents(vtkMRMLDisplayableNode::DisplayModifiedEvent) << std::endl;
+              << spy->GetNumberOfEvents(vtkMRMLNode::ReferenceRemovedEvent) << " "<< std::endl
+              << spy->GetNumberOfEvents(vtkMRMLDisplayableNode::DisplayModifiedEvent) << std::endl;
     return false;
     }
   spy->ResetNumberOfEvents();