NA-MIC/svn.slicer.org-Slicer4

commit f98eda15171deaa948a692c54440c8c6db3853db

BUG: Made file extension comparisons case-insensitive. Fixes #3511
Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

From: Andras Lasso <lasso@queensu.ca>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@22753 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/vtkTeem/vtkNRRDReader.cxx
@@ -44,6 +44,7 @@
 #include "vtkLongArray.h"
 #include "vtkDoubleArray.h"
 #include "vtkFloatArray.h"
+#include <vtksys/SystemTools.hxx>
 
 #include "teem/ten.h"
 
@@ -167,22 +168,8 @@ int vtkNRRDReader::CanReadFile(const char* filename)
   //  return true;
   //  }
 
-  bool extensionFound = false;
-  std::string::size_type nrrdPos = fname.rfind(".nrrd");
-  if ((nrrdPos != std::string::npos)
-      && (nrrdPos == fname.length() - 5))
-    {
-    extensionFound = true;
-    }
-
-  std::string::size_type nhdrPos = fname.rfind(".nhdr");
-  if ((nhdrPos != std::string::npos)
-      && (nhdrPos == fname.length() - 5))
-    {
-    extensionFound = true;
-    }
-
-  if( !extensionFound )
+  std::string extension = vtksys::SystemTools::LowerCase( vtksys::SystemTools::GetFilenameLastExtension(fname) );
+  if (extension != ".nrrd" || extension != ".nhdr")
     {
     vtkDebugMacro(<<"The filename extension is not recognized");
     return false;

Base/QTCLI/vtkSlicerCLIModuleLogic.cxx
@@ -34,6 +34,7 @@
 #include <vtkMultiThreader.h>
 #include <vtkNew.h>
 #include <vtkStringArray.h>
+#include <vtksys/SystemTools.hxx>
 
 // ITKSYS includes
 #include <itksys/Process.h>
@@ -1022,13 +1023,11 @@ void vtkSlicerCLIModuleLogic::ApplyTask(void *clientdata)
       {
       // Transform nodes will use either a storage node OR a miniscene
 
-      std::string::size_type loc = (*id2fn0).second.find_last_of(".");
-      if (loc != std::string::npos)
+      std::string extension = vtksys::SystemTools::LowerCase( vtksys::SystemTools::GetFilenameLastExtension((*id2fn0).second) );
+      if (!extension.empty())
         {
         // if we start passing pointers to MRML transforms, then we'll
         // need an additional check/case
-        std::string extension = (*id2fn0).second.substr(loc);
-
         if (extension == ".mrml")
           {
           // not using a storage node.  using a mini-scene to transfer
@@ -1096,13 +1095,11 @@ void vtkSlicerCLIModuleLogic::ApplyTask(void *clientdata)
 
     if (tnd || mhnd)
       {
-      std::string::size_type loc = (*id2fn0).second.find_last_of(".");
-      if (loc != std::string::npos)
+      std::string extension = vtksys::SystemTools::LowerCase( vtksys::SystemTools::GetFilenameLastExtension((*id2fn0).second) );
+      if (!extension.empty())
         {
         // if we start passing pointers to MRML transforms, then we'll
         // need an additional check/case
-        std::string extension = (*id2fn0).second.substr(loc);
-
         if (extension == ".mrml")
           {
           // put this transform node in the miniscene

Base/QTGUI/qSlicerSaveDataDialog.cxx
@@ -352,8 +352,8 @@ void qSlicerSaveDataDialogPrivate::populateScene()
   foreach(const QString& nameFilter,
           coreIOManager->fileWriterExtensions(this->MRMLScene))
     {
-    QString extension = QString(vtkDataFileFormatHelper::GetFileExtensionFromFormatString(
-      nameFilter.toLatin1()));
+    QString extension = QString::fromStdString(
+      vtkDataFileFormatHelper::GetFileExtensionFromFormatString(nameFilter.toLatin1()));
     sceneComboBoxWidget->addItem(nameFilter, extension);
     if (extension == currentExtension)
       {
@@ -621,8 +621,8 @@ QWidget* qSlicerSaveDataDialogPrivate::createFileFormatsWidget(vtkMRMLStorableNo
   QString currentExtension = QString(".") + fileInfo.suffix();
   foreach(QString nameFilter, coreIOManager->fileWriterExtensions(node))
     {
-    QString extension = QString(vtkDataFileFormatHelper::GetFileExtensionFromFormatString(
-      nameFilter.toLatin1()));
+    QString extension = QString::fromStdString(
+      vtkDataFileFormatHelper::GetFileExtensionFromFormatString(nameFilter.toLatin1()));
     fileFormats->addItem(nameFilter, extension);
     if (extension == currentExtension)
       {
@@ -698,8 +698,8 @@ QString qSlicerSaveDataDialogPrivate::extractKnownExtension(const QString& fileN
   foreach(const QString& nameFilter,
           coreIOManager->fileWriterExtensions(object))
     {
-    QString extension = QString(vtkDataFileFormatHelper::GetFileExtensionFromFormatString(
-      nameFilter.toLatin1()));
+    QString extension = QString::fromStdString(
+      vtkDataFileFormatHelper::GetFileExtensionFromFormatString(nameFilter.toLatin1()));
     if (!extension.isEmpty() && fileName.endsWith(extension))
       {
       return extension;
@@ -1168,8 +1168,8 @@ void qSlicerSaveDataDialogPrivate::formatChanged()
   Q_ASSERT(fileNameItem);
 
   // Set the new selected extension to the file name
-  QString extension = vtkDataFileFormatHelper::GetFileExtensionFromFormatString(
-    formatComboBox->currentText().toLatin1());
+  QString extension = QString::fromStdString(vtkDataFileFormatHelper::GetFileExtensionFromFormatString(
+    formatComboBox->currentText().toLatin1()));
   if (extension == "*")
     {
     extension = QString();

Libs/MGHImageIO/itkMGHImageIO.cxx
@@ -6,6 +6,7 @@
 #include "itkMetaDataObject.h"
 #include "itksys/SystemTools.hxx"
 
+#include <itksys/SystemTools.hxx>
 
 static const int MRI_UCHAR = 0;
 static const int MRI_INT   = 1;
@@ -101,7 +102,7 @@ MGHImageIO::CanReadFile(const char* FileNameToRead)
     }
 
   // check if the correct extension is given by the user
-  const std::string extension = itksys::SystemTools::GetFilenameExtension(filename.c_str());
+  std::string extension = itksys::SystemTools::LowerCase( itksys::SystemTools::GetFilenameLastExtension(filename) );
   if( extension == __MGH_EXT || this->IsCompressedFilename(filename) )
     {
     return true;
@@ -469,7 +470,7 @@ MGHImageIO::CanWriteFile(const char* name)
     return false;
     }
 
-  const std::string extension = itksys::SystemTools::GetFilenameExtension(filename.c_str());
+  std::string extension = itksys::SystemTools::LowerCase( itksys::SystemTools::GetFilenameLastExtension(filename) );
   if( extension != __MGH_EXT && !this->IsCompressedFilename(filename))
     {
     return false;
@@ -524,7 +525,7 @@ MGHImageIO::Write(const void* buffer)
 bool
 MGHImageIO::IsCompressedFilename(const std::string fname)
 {
-  const std::string extension = itksys::SystemTools::GetFilenameExtension(fname.c_str());
+  std::string extension = itksys::SystemTools::LowerCase( itksys::SystemTools::GetFilenameLastExtension(fname) );
   return extension == __MGZ_EXT || extension == __GZ_EXT || extension == __MGHGZ_EXT;
 }
 

Libs/MRML/Core/vtkDataFileFormatHelper.cxx
@@ -3,6 +3,7 @@
 
 #include <vtkObjectFactory.h>
 #include <vtkStringArray.h>
+#include <vtksys/SystemTools.hxx>
 
 #include <set>
 
@@ -190,7 +191,7 @@ void vtkDataFileFormatHelper::PopulateITKSupportedWriteFileTypes()
 }
 
 //----------------------------------------------------------------------------
-const char* vtkDataFileFormatHelper::GetFileExtensionFromFormatString(
+std::string vtkDataFileFormatHelper::GetFileExtensionFromFormatString(
   const char* format)
 {
   std::string fileformat(format);
@@ -211,14 +212,12 @@ const char* vtkDataFileFormatHelper::GetFileExtensionFromFormatString(
       {
       fileext = fileext.substr(0, pos1);
       }
-    static char buffer[20];
-    sprintf(buffer, "%s", fileext.c_str());
-    return buffer;
-    //return fileext.c_str();
+    std::string lowercaseExtension=vtksys::SystemTools::LowerCase(fileext);
+    return lowercaseExtension;
     }
   else
     {
-    return NULL;
+    return "";
     }
 }
 
@@ -227,10 +226,10 @@ const char* vtkDataFileFormatHelper::GetClassNameFromFormatString(
   const char* format)
 {
   std::string fileformat(format);
-  const char* fileExt = vtkDataFileFormatHelper::GetFileExtensionFromFormatString(
+  std::string fileExt = vtkDataFileFormatHelper::GetFileExtensionFromFormatString(
     fileformat.c_str());
   // if no extension found
-  if(!fileExt || !(*fileExt))
+  if(fileExt.empty())
     {
     return NULL;
     }
@@ -245,14 +244,12 @@ const char* vtkDataFileFormatHelper::GetClassNameFromFormatString(
     for(int idx=0; idx<itkFileExtensions->GetNumberOfTuples(); idx++)
       {
       const char* extFormat = this->GetITKSupportedExtensionGenericNameByIndex(idx);
-      std::string strExt = itkFileExtensions->GetValue(idx);
+      std::string strExt = vtksys::SystemTools::LowerCase( itkFileExtensions->GetValue(idx) );
       
-      if((!strExt.empty() && strcmp(strExt.c_str(), fileExt)==0)
+      if((!strExt.empty() && strExt.compare(fileExt)==0)
         && (extFormat && strcmp(extFormat, genericName.c_str())==0))
         {
-        std::string strClassName(
-          this->GetITKSupportedExtensionClassNameByIndex(idx));
-        return strClassName.c_str();
+        return this->GetITKSupportedExtensionClassNameByIndex(idx);
         }
       }
 

Libs/MRML/Core/vtkDataFileFormatHelper.h
@@ -25,7 +25,7 @@ class VTK_MRML_EXPORT vtkDataFileFormatHelper : public vtkObject
   vtkTypeRevisionMacro(vtkDataFileFormatHelper, vtkObject);
   void PrintSelf(ostream& os, vtkIndent indent);
 
-  static const char* GetFileExtensionFromFormatString(
+  static std::string GetFileExtensionFromFormatString(
    const char* fileformat);
   const char* GetClassNameFromFormatString(
    const char* fileformat);

Libs/MRML/Core/vtkMRMLFreeSurferModelOverlayStorageNode.cxx
@@ -37,6 +37,7 @@ Version:   $Revision: 1.2 $
 #include <vtkPointData.h>
 #include <vtkPolyData.h>
 #include <vtkStringArray.h>
+#include <vtksys/SystemTools.hxx>
 
 // ITKSys includes
 
@@ -97,15 +98,9 @@ int vtkMRMLFreeSurferModelOverlayStorageNode::ReadDataInternal(vtkMRMLNode *refN
   vtkDebugMacro("ReadData: reading " << fullName.c_str());
 
   // compute file prefix
-  std::string name(fullName);
-  std::string::size_type loc = name.find_last_of(".");
-  if( loc == std::string::npos )
-    {
-    vtkErrorMacro("ReadData: no file extension specified");
-    }
-  std::string extension = name.substr(loc);
-
+  std::string extension = vtkMRMLStorageNode::GetLowercaseExtensionFromFileName(fullName);
   vtkDebugMacro("ReadData: extension = " << extension.c_str());
+
   // don't delete the polydata if reading in a scalar overlay
 
   // reset this to 0 if have an error
@@ -130,27 +125,27 @@ int vtkMRMLFreeSurferModelOverlayStorageNode::ReadDataInternal(vtkMRMLNode *refN
         reader->SetFileName(fullName.c_str());
         // the array to read into
         vtkFloatArray *floatArray = vtkFloatArray::New();
-        std::string::size_type ptr = name.find_last_of(std::string("/"));
+        std::string::size_type ptr = fullName.find_last_of(std::string("/"));
         std::string scalarName;
         if (ptr != std::string::npos)
         {
             // find the dir name above
             std::string::size_type ptrNext = ptr;
-            std::string::size_type dirptr = name.find_last_of(std::string("/"), --ptrNext);
+            std::string::size_type dirptr = fullName.find_last_of(std::string("/"), --ptrNext);
             if (dirptr != std::string::npos)
             {
-                scalarName = name.substr(++dirptr);
+                scalarName = fullName.substr(++dirptr);
                 vtkDebugMacro("Using dir name in scalar name " << scalarName.c_str());
             }
             else
             {
-                scalarName = name.substr(++ptr);
+                scalarName = fullName.substr(++ptr);
                 vtkDebugMacro("Not using the dir name in the scalar name " << scalarName.c_str());
             }
         }
         else
         {
-            scalarName = name;
+            scalarName = fullName;
         }
         floatArray->SetName(scalarName.c_str());
         reader->SetOutput(floatArray);
@@ -206,15 +201,15 @@ int vtkMRMLFreeSurferModelOverlayStorageNode::ReadDataInternal(vtkMRMLNode *refN
 
         // the array to read into
         vtkFloatArray *floatArray = vtkFloatArray::New();
-        std::string::size_type ptr = name.find_last_of(std::string("/"));
+        std::string::size_type ptr = fullName.find_last_of(std::string("/"));
         std::string scalarName;
         if (ptr != std::string::npos)
           {
-          scalarName = name.substr(++ptr);
+          scalarName = fullName.substr(++ptr);
           }
         else
           {
-          scalarName = name;
+          scalarName = fullName;
           }
         floatArray->SetName(scalarName.c_str());
         reader->SetOutput(floatArray);
@@ -274,7 +269,8 @@ int vtkMRMLFreeSurferModelOverlayStorageNode::ReadDataInternal(vtkMRMLNode *refN
         }
       else
         {
-        vtkErrorMacro("Cannot read scalar overlay file '" << name.c_str() << "', as there are no points in the model " << modelNode->GetID() << " to associate it with.");
+        vtkErrorMacro("Cannot read scalar overlay file '" << fullName.c_str()
+                      << "', as there are no points in the model " << modelNode->GetID() << " to associate it with.");
         result = 1;
         }
       }
@@ -293,15 +289,15 @@ int vtkMRMLFreeSurferModelOverlayStorageNode::ReadDataInternal(vtkMRMLNode *refN
 
         // the array to read into
         vtkFloatArray *floatArray = vtkFloatArray::New();
-        std::string::size_type ptr = name.find_last_of(std::string("/"));
+        std::string::size_type ptr = fullName.find_last_of(std::string("/"));
         std::string scalarName;
         if (ptr != std::string::npos)
           {
-          scalarName = name.substr(++ptr);
+          scalarName = fullName.substr(++ptr);
           }
         else
           {
-          scalarName = name;
+          scalarName = fullName;
           }
         floatArray->SetName(scalarName.c_str());
         reader->SetOutput(floatArray);
@@ -538,51 +534,55 @@ int vtkMRMLFreeSurferModelOverlayStorageNode::ReadDataInternal(vtkMRMLNode *refN
           }
         catch (...)
           {
-          vtkErrorMacro("vtkMRMLFreeSurferModelOverlayStorageNode::ReadData Cannot read scalar overlay volume file " << fullName.c_str());
+          vtkErrorMacro("vtkMRMLFreeSurferModelOverlayStorageNode::ReadData Cannot read scalar overlay volume file "
+                        << fullName.c_str());
           reader->Delete();
           return 0;
           }
         vtkImageData *imageData = reader->GetOutput();
         if (imageData == NULL)
           {
-          vtkErrorMacro("vtkMRMLFreeSurferModelOverlayStorageNode::ReadData Unable to get image data out of scalar overlay volume file " << fullName.c_str());
+          vtkErrorMacro("vtkMRMLFreeSurferModelOverlayStorageNode::ReadData Unable to get image data out of scalar overlay volume file "
+                        << fullName.c_str());
           reader->Delete();
           return 0;
           }
         int numPoints = imageData->GetNumberOfPoints();
-        vtkDebugMacro("Testing volume file for scalar overlay, num vertices = " << numVertices << ", image data number of points = " << numPoints );
+        vtkDebugMacro("Testing volume file for scalar overlay, num vertices = "
+                      << numVertices << ", image data number of points = " << numPoints );
         // only valid if volume w*h*d == num vertices
         if (numPoints != numVertices)
           {
-          vtkErrorMacro("vtkMRMLFreeSurferModelOverlayStorageNode::ReadData : volume file data size " << numPoints << " not the same as model vertices " << numVertices);
+          vtkErrorMacro("vtkMRMLFreeSurferModelOverlayStorageNode::ReadData : volume file data size "
+                        << numPoints << " not the same as model vertices " << numVertices);
           reader->Delete();
           return 0;
           }
 
         // put it into an array
         vtkFloatArray *floatArray = vtkFloatArray::New();
-        std::string::size_type ptr = name.find_last_of(std::string("/"));
+        std::string::size_type ptr = fullName.find_last_of(std::string("/"));
         std::string scalarName;
         if (ptr != std::string::npos)
           {
           // find the dir name above
           std::string::size_type ptrNext = ptr;
-          std::string::size_type dirptr = name.find_last_of(std::string("/"), --ptrNext);
+          std::string::size_type dirptr = fullName.find_last_of(std::string("/"), --ptrNext);
           if (dirptr != std::string::npos)
             {
-            scalarName = name.substr(++dirptr);
+            scalarName = fullName.substr(++dirptr);
             vtkDebugMacro("Using dir name in scalar name " << scalarName.c_str());
             }
           else
             {
-            scalarName = name.substr(++ptr);
+            scalarName = fullName.substr(++ptr);
             vtkDebugMacro("Not using the dir name in the scalar name " << scalarName.c_str());
             }
 
           }
         else
           {
-          scalarName = name;
+          scalarName = fullName;
           }
         floatArray->SetName(scalarName.c_str());
 
@@ -624,7 +624,7 @@ int vtkMRMLFreeSurferModelOverlayStorageNode::ReadDataInternal(vtkMRMLNode *refN
       }
     else
       {
-      vtkErrorMacro("MRML FreeSurfer ModelStorage Node: Cannot read model file '" << name.c_str() << "' (extension = " << extension.c_str() << ")");
+      vtkErrorMacro("MRML FreeSurfer ModelStorage Node: Cannot read model file '" << fullName.c_str() << "' (extension = " << extension.c_str() << ")");
       return 0;
       }
     }
@@ -646,26 +646,27 @@ int vtkMRMLFreeSurferModelOverlayStorageNode::ReadDataInternal(vtkMRMLNode *refN
 std::string vtkMRMLFreeSurferModelOverlayStorageNode
 ::GetColorNodeIDFromExtension(const std::string& extension)
 {
+  std::string lowercaseExtension = vtksys::SystemTools::LowerCase(extension);
   int type = vtkMRMLFreeSurferProceduralColorNode::Heat;
-  if (extension == std::string(".thickness"))
+  if (lowercaseExtension == std::string(".thickness"))
     {
     type = vtkMRMLFreeSurferProceduralColorNode::GreenRed;
     }
-  else if (extension == std::string(".curv") ||
-           extension == std::string(".avg_curv") ||
-           extension == std::string(".sulc"))
+  else if (lowercaseExtension == std::string(".curv") ||
+           lowercaseExtension == std::string(".avg_curv") ||
+           lowercaseExtension == std::string(".sulc"))
     {
     type = vtkMRMLFreeSurferProceduralColorNode::GreenRed;
     }
-  else if (extension == std::string(".area"))
+  else if (lowercaseExtension == std::string(".area"))
     {
     type = vtkMRMLFreeSurferProceduralColorNode::RedGreen;
     }
-  else if (extension == std::string(".fs"))
+  else if (lowercaseExtension == std::string(".fs"))
     {
     type = vtkMRMLFreeSurferProceduralColorNode::BlueRed;
     }
-  else if (extension == std::string(".retinotopy"))
+  else if (lowercaseExtension == std::string(".retinotopy"))
     {
     vtkWarningMacro("Retinotopy color wheel not implemented yet, using default of Heat.");
     //type = type = vtkMRMLFreeSurferProceduralColorNode::ColorWheel();

Libs/MRML/Core/vtkMRMLFreeSurferModelStorageNode.cxx
@@ -118,14 +118,7 @@ int vtkMRMLFreeSurferModelStorageNode::ReadDataInternal(vtkMRMLNode *refNode)
   vtkDebugMacro("ReadDataInternal: reading " << fullName.c_str());
   
   // compute file prefix
-  std::string name(fullName);
-  std::string::size_type loc = name.find_last_of(".");
-  if( loc == std::string::npos ) 
-    {
-    vtkErrorMacro("ReadDataInternal: no file extension specified");
-    }
-  std::string extension = name.substr(loc);
-
+  std::string extension = vtkMRMLStorageNode::GetLowercaseExtensionFromFileName(fullName);
   vtkDebugMacro("ReadDataInternal: extension = " << extension.c_str());
   
   int result = 1;

commit 1512213c3865bba2f91e5ded2fe9fb77c8328e76

BUG: Fix reading of DWI files. Fixes #3545
This commit fixes regression introduced by commit r22753 fixing issue #3511
that was preventing "vtkNRRDReader::CanReadFile" from returning the correct
value.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@22801 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/vtkTeem/vtkNRRDReader.cxx
@@ -169,7 +169,7 @@ int vtkNRRDReader::CanReadFile(const char* filename)
   //  }
 
   std::string extension = vtksys::SystemTools::LowerCase( vtksys::SystemTools::GetFilenameLastExtension(fname) );
-  if (extension != ".nrrd" || extension != ".nhdr")
+  if (extension != ".nrrd" && extension != ".nhdr")
     {
     vtkDebugMacro(<<"The filename extension is not recognized");
     return false;