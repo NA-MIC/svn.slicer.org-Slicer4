NA-MIC/svn.slicer.org-Slicer4

commit cbdd6ca0552fc596cd32138c55fd3c09418caf2c

ENH: Add support for scripted module available as ".pyc" file
git-svn-id: http://svn.slicer.org/Slicer4/trunk@23061 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Logic/vtkSlicerScriptedLoadableModuleLogic.cxx
@@ -179,7 +179,11 @@ void vtkSlicerScriptedLoadableModuleLogic::PrintSelf(ostream& os, vtkIndent inde
 //---------------------------------------------------------------------------
 bool vtkSlicerScriptedLoadableModuleLogic::SetPythonSource(const std::string& pythonSource)
 {
-  assert(pythonSource.find(".py") != std::string::npos);
+  if(pythonSource.find(".py") == std::string::npos &&
+     pythonSource.find(".pyc") == std::string::npos)
+    {
+    return false;
+    }
 
   // Extract filename - It should match the associated python class
   std::string className = vtksys::SystemTools::GetFilenameWithoutExtension(pythonSource);
@@ -194,9 +198,22 @@ bool vtkSlicerScriptedLoadableModuleLogic::SetPythonSource(const std::string& py
   PyObject * classToInstantiate = PyDict_GetItemString(global_dict, className.c_str());
   if (!classToInstantiate)
     {
-    std::string pyRunStr = std::string("execfile('") + pythonSource + std::string("')");
-    PyObject * pyRes = PyRun_String(pyRunStr.c_str(),
-                                    Py_file_input, global_dict, global_dict);
+    PyObject * pyRes = 0;
+    if (newPythonSource.endsWith(".py"))
+      {
+      std::string pyRunStr = std::string("execfile('") + pythonSource + std::string("')");
+      pyRes = PyRun_String(pyRunStr.c_str(),
+                           Py_file_input, global_dict, global_dict);
+      }
+    else if (newPythonSource.endsWith(".pyc"))
+      {
+      std::string pyRunStr = std::string("with open('") + pythonSource +
+          std::string("', 'rb') as f:import imp;imp.load_module('__main__', f, '") + pythonSource +
+          std::string("', ('.pyc', 'rb', 2))");
+      pyRes = PyRun_String(
+            pyRunStr.c_str(),
+            Py_file_input, global_dict, global_dict);
+      }
     if (!pyRes)
       {
       vtkErrorMacro(<< "setPythonSource - Failed to execute file" << pythonSource << "!");

Libs/MRML/DisplayableManager/vtkMRMLScriptedDisplayableManager.cxx
@@ -276,7 +276,11 @@ void vtkMRMLScriptedDisplayableManager::OnMRMLDisplayableNodeModifiedEvent(vtkOb
 //---------------------------------------------------------------------------
 void vtkMRMLScriptedDisplayableManager::SetPythonSource(const std::string& pythonSource)
 {
-  assert(pythonSource.find(".py") != std::string::npos);
+  if(pythonSource.find(".py") == std::string::npos &&
+     pythonSource.find(".pyc") == std::string::npos)
+    {
+    return false;
+    }
 
   // Extract filename - It should match the associated python class
   std::string className = vtksys::SystemTools::GetFilenameWithoutExtension(pythonSource);
@@ -290,9 +294,22 @@ void vtkMRMLScriptedDisplayableManager::SetPythonSource(const std::string& pytho
   PyObject * classToInstantiate = PyDict_GetItemString(global_dict, className.c_str());
   if (!classToInstantiate)
     {
-    std::string pyRunStr = std::string("execfile('") + pythonSource + std::string("')");
-    PyObject * pyRes = PyRun_String(pyRunStr.c_str(),
-                                    Py_file_input, global_dict, global_dict);
+    PyObject * pyRes = 0;
+    if (newPythonSource.endsWith(".py"))
+      {
+      std::string pyRunStr = std::string("execfile('") + pythonSource + std::string("')");
+      pyRes = PyRun_String(pyRunStr.c_str(),
+                           Py_file_input, global_dict, global_dict);
+      }
+    else if (newPythonSource.endsWith(".pyc"))
+      {
+      std::string pyRunStr = std::string("with open('") + pythonSource +
+          std::string("', 'rb') as f:import imp;imp.load_module('__main__', f, '") + pythonSource +
+          std::string("', ('.pyc', 'rb', 2))");
+      pyRes = PyRun_String(
+            pyRunStr.c_str(),
+            Py_file_input, global_dict, global_dict);
+      }
     if (!pyRes)
       {
       vtkErrorMacro(<< "setPythonSource - Failed to execute file" << pythonSource << "!");

Base/QTCore/qSlicerScriptedFileWriter.cxx
@@ -128,7 +128,10 @@ bool qSlicerScriptedFileWriter::setPythonSource(const QString& newPythonSource, 
     return false;
     }
 
-  Q_ASSERT(newPythonSource.endsWith(".py"));
+  if(!newPythonSource.endsWith(".py") && !newPythonSource.endsWith(".pyc"))
+    {
+    return false;
+    }
 
   // Extract moduleName from the provided filename
   QString classNameToLoad = className;

Base/QTGUI/qSlicerScriptedFileDialog.cxx
@@ -134,7 +134,10 @@ bool qSlicerScriptedFileDialog::setPythonSource(const QString& newPythonSource, 
     return false;
     }
 
-  Q_ASSERT(newPythonSource.endsWith(".py"));
+  if(!newPythonSource.endsWith(".py") && !newPythonSource.endsWith(".pyc"))
+    {
+    return false;
+    }
 
   // Extract moduleName from the provided filename
   QString classNameToLoad = className;

Base/QTGUI/qSlicerScriptedLoadableModule.cxx
@@ -131,7 +131,6 @@ QString qSlicerScriptedLoadableModule::pythonSource()const
   Q_D(const qSlicerScriptedLoadableModule);
   return d->PythonSource;
 }
-
 //-----------------------------------------------------------------------------
 bool qSlicerScriptedLoadableModule::setPythonSource(const QString& newPythonSource)
 {
@@ -142,7 +141,10 @@ bool qSlicerScriptedLoadableModule::setPythonSource(const QString& newPythonSour
     return false;
     }
 
-  Q_ASSERT(newPythonSource.endsWith(".py"));
+  if (!newPythonSource.endsWith(".py") && !newPythonSource.endsWith(".pyc"))
+    {
+    return false;
+    }
 
   // Extract moduleName from the provided filename
   QString moduleName = QFileInfo(newPythonSource).baseName();
@@ -157,8 +159,18 @@ bool qSlicerScriptedLoadableModule::setPythonSource(const QString& newPythonSour
   if (!classToInstantiate)
     {
     PyDict_SetItemString(global_dict, "__name__", PyString_FromString(className.toLatin1()));
-    PyObject * pyRes = PyRun_String(QString("execfile('%1')").arg(newPythonSource).toLatin1(),
-                                    Py_file_input, global_dict, global_dict);
+    PyObject* pyRes = 0;
+    if (newPythonSource.endsWith(".py"))
+      {
+      pyRes = PyRun_String(QString("execfile('%1')").arg(newPythonSource).toLatin1(),
+                           Py_file_input, global_dict, global_dict);
+      }
+    else if (newPythonSource.endsWith(".pyc"))
+      {
+      pyRes = PyRun_String(
+            QString("with open('%1', 'rb') as f:import imp;imp.load_module('__main__', f, '%1', ('.pyc', 'rb', 2))").arg(newPythonSource).toLatin1(),
+            Py_file_input, global_dict, global_dict);
+      }
     if (!pyRes)
       {
       PythonQt::self()->handleError();

Base/QTGUI/qSlicerScriptedLoadableModuleFactory.cxx
@@ -156,8 +156,26 @@ qSlicerScriptedLoadableModuleFactory::~qSlicerScriptedLoadableModuleFactory()
 bool qSlicerScriptedLoadableModuleFactory::isValidFile(const QFileInfo& file)const
 {
   // Skip if current file isn't a python file
-  return ctkAbstractFileBasedFactory<qSlicerAbstractCoreModule>::isValidFile(file) &&
-    file.suffix().compare("py", Qt::CaseInsensitive) == 0;
+  if(!ctkAbstractFileBasedFactory<qSlicerAbstractCoreModule>::isValidFile(file))
+    {
+    return false;
+    }
+  // Accept if current file is a python script
+  if (file.suffix().compare("py", Qt::CaseInsensitive) == 0)
+    {
+    return true;
+    }
+  // Accept if current file is a pyc file and there is no associated py file
+  if (file.suffix().compare("pyc", Qt::CaseInsensitive) == 0)
+    {
+    int length = file.filePath().size();
+    QString pyFilePath = file.filePath().remove(length - 1, 1);
+    if (!QFile::exists(pyFilePath))
+      {
+      return true;
+      }
+    }
+  return false;
 }
 
 //----------------------------------------------------------------------------

Base/QTGUI/qSlicerScriptedLoadableModuleWidget.cxx
@@ -120,7 +120,10 @@ bool qSlicerScriptedLoadableModuleWidget::setPythonSource(const QString& newPyth
     return false;
     }
 
-  Q_ASSERT(newPythonSource.endsWith(".py"));
+  if (!newPythonSource.endsWith(".py") && !newPythonSource.endsWith(".pyc"))
+    {
+    return false;
+    }
 
   // Extract moduleName from the provided filename
   QString classNameToLoad = className;
@@ -142,8 +145,18 @@ bool qSlicerScriptedLoadableModuleWidget::setPythonSource(const QString& newPyth
   PyObject * classToInstantiate = PyDict_GetItemString(global_dict, classNameToLoad.toLatin1());
   if (!classToInstantiate)
     {
-    PyObject * pyRes = PyRun_String(QString("execfile('%1')").arg(newPythonSource).toLatin1(),
-                                    Py_file_input, global_dict, global_dict);
+    PyObject* pyRes = 0;
+    if (newPythonSource.endsWith(".py"))
+      {
+      pyRes = PyRun_String(QString("execfile('%1')").arg(newPythonSource).toLatin1(),
+                           Py_file_input, global_dict, global_dict);
+      }
+    else if (newPythonSource.endsWith(".pyc"))
+      {
+      pyRes = PyRun_String(
+            QString("with open('%1', 'rb') as f:import imp;imp.load_module('__main__', f, '%1', ('.pyc', 'rb', 2))").arg(newPythonSource).toLatin1(),
+            Py_file_input, global_dict, global_dict);
+      }
     if (!pyRes)
       {
       PythonQt::self()->handleError();

commit 34e2c3c02b0177be7eb57e495f7f890b446edded

COMP: Fix build error introduced in commit r23061
git-svn-id: http://svn.slicer.org/Slicer4/trunk@23062 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Logic/vtkSlicerScriptedLoadableModuleLogic.cxx
@@ -199,13 +199,13 @@ bool vtkSlicerScriptedLoadableModuleLogic::SetPythonSource(const std::string& py
   if (!classToInstantiate)
     {
     PyObject * pyRes = 0;
-    if (newPythonSource.endsWith(".py"))
+    if (pythonSource.find(".py") != std::string::npos)
       {
       std::string pyRunStr = std::string("execfile('") + pythonSource + std::string("')");
       pyRes = PyRun_String(pyRunStr.c_str(),
                            Py_file_input, global_dict, global_dict);
       }
-    else if (newPythonSource.endsWith(".pyc"))
+    else if (pythonSource.find(".pyc") != std::string::npos)
       {
       std::string pyRunStr = std::string("with open('") + pythonSource +
           std::string("', 'rb') as f:import imp;imp.load_module('__main__', f, '") + pythonSource +

Libs/MRML/DisplayableManager/vtkMRMLScriptedDisplayableManager.cxx
@@ -279,7 +279,7 @@ void vtkMRMLScriptedDisplayableManager::SetPythonSource(const std::string& pytho
   if(pythonSource.find(".py") == std::string::npos &&
      pythonSource.find(".pyc") == std::string::npos)
     {
-    return false;
+    return;
     }
 
   // Extract filename - It should match the associated python class
@@ -295,13 +295,13 @@ void vtkMRMLScriptedDisplayableManager::SetPythonSource(const std::string& pytho
   if (!classToInstantiate)
     {
     PyObject * pyRes = 0;
-    if (newPythonSource.endsWith(".py"))
+    if (pythonSource.find(".py") != std::string::npos)
       {
       std::string pyRunStr = std::string("execfile('") + pythonSource + std::string("')");
       pyRes = PyRun_String(pyRunStr.c_str(),
                            Py_file_input, global_dict, global_dict);
       }
-    else if (newPythonSource.endsWith(".pyc"))
+    else if (pythonSource.find(".pyc") != std::string::npos)
       {
       std::string pyRunStr = std::string("with open('") + pythonSource +
           std::string("', 'rb') as f:import imp;imp.load_module('__main__', f, '") + pythonSource +