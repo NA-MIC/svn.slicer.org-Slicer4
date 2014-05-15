NA-MIC/svn.slicer.org-Slicer4

commit e92f330a0508b81b5a2ba40333ce965207677ae4

ENH: Reduce package size by not installing export libraries. Fixes #2397
This commit explicitly installs the needed component and skips the
development component if not explicitly requested.

Since on DLL platform, export libraries (.lib) are considered as archive [1]
and are associated with the development component, they won't be installed
in the package.

[1] http://www.cmake.org/cmake/help/v2.8.12/cmake.html#command:install

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23158 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerCPack.cmake
@@ -125,7 +125,12 @@ foreach(additional_project ${additional_projects})
 endforeach()
 
 # Install Slicer
-set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;ALL;/")
+set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;Runtime;/")
+set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;RuntimeLibraries;/")
+set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;RuntimePlugins;/")
+if(NOT Slicer_INSTALL_NO_DEVELOPMENT)
+  set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;Development;/")
+endif()
 
 # -------------------------------------------------------------------------
 # Package properties

commit 775e0f8139620afb0915372327ab19351dd58225

BUG: Fix MacOSX packaging
This commit ensures the MacOSX packaging script is executed last
by installing the 'Runtime' component last and should ensure the
packaged Slicer application can be executed without crashing.

This particular issue was introduced by commit r23158 where two
things happened:

(1) updated of the build system to explicitly install Slicer components.
The component where specified in the order 'Runtime', 'RuntimeLibraries'
and 'RuntimePlugins'

(2) the MacOSX packaging script was associated with the 'Rumtime' component

The consequence was that Slicer libraries and plugins where installed after
the script was executed and the plugins and libraries where no properly
fixed up. It then prevented the Slicer application from being executed.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23183 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerCPack.cmake
@@ -125,12 +125,13 @@ foreach(additional_project ${additional_projects})
 endforeach()
 
 # Install Slicer
-set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;Runtime;/")
 set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;RuntimeLibraries;/")
 set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;RuntimePlugins;/")
 if(NOT Slicer_INSTALL_NO_DEVELOPMENT)
   set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;Development;/")
 endif()
+# Installation of 'Runtime' should be last to ensure the 'SlicerCPackBundleFixup.cmake' is executed last.
+set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${Slicer_BINARY_DIR};Slicer;Runtime;/")
 
 # -------------------------------------------------------------------------
 # Package properties