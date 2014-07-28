NA-MIC/svn.slicer.org-Slicer4

commit bf063984c2b3f1a17cddd7dc8e0d07f6ebbf761f

ENH: move duplicated code into volumes logic
Both the Editor and LabelStatistics modules had code to check
that volume geometry matched for a label map.  The duplicated
code is removed and there is a test in LabelStatistics.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23406 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Volumes/Logic/vtkSlicerVolumesLogic.cxx
@@ -863,6 +863,84 @@ vtkSlicerVolumesLogic::FillLabelVolumeFromTemplate(vtkMRMLScene *scene,
 }
 
 //----------------------------------------------------------------------------
+std::string
+vtkSlicerVolumesLogic::CheckForLabelVolumeValidity(vtkMRMLScalarVolumeNode *volumeNode,
+                                          vtkMRMLScalarVolumeNode *labelNode)
+{
+  std::stringstream warnings;
+  warnings << "";
+  if (!volumeNode || !labelNode)
+    {
+    warnings << "Null volume node pointer\n";
+    }
+  else
+    {
+    if (!labelNode->GetLabelMap())
+      {
+      warnings << "Label node volume does not have LabelMap property\n";
+      }
+    else
+      {
+      vtkImageData *volumeImage = volumeNode->GetImageData();
+      vtkImageData *labelImage  = labelNode->GetImageData();
+      if (!volumeImage || !labelImage)
+        {
+        warnings << "Null image data pointer\n";
+        }
+      else
+        {
+        int row, column;
+        double volumeValue, labelValue;
+        for (row = 0; row < 3; row++)
+          {
+          volumeValue = volumeImage->GetDimensions()[row];
+          labelValue = labelImage->GetDimensions()[row];
+
+          if (volumeValue != labelValue)
+            {
+            warnings << "Dimension mismatch at row [" << row << "] (" << volumeValue << " != " << labelValue << ")\n";
+            }
+
+          volumeValue = volumeImage->GetSpacing()[row];
+          labelValue = labelImage->GetSpacing()[row];
+          if (volumeValue != labelValue)
+            {
+            warnings << "Spacing mismatch at row [" << row << "] (" << volumeValue << " != " << labelValue << ")\n";
+            }
+
+          volumeValue = volumeImage->GetOrigin()[row];
+          labelValue = labelImage->GetOrigin()[row];
+          if (volumeValue != labelValue)
+            {
+            warnings << "Origin mismatch at row [" << row << "] (" << volumeValue << " != " << labelValue << ")\n";
+            }
+
+          }
+        vtkMatrix4x4 *volumeIJKToRAS = vtkMatrix4x4::New();
+        vtkMatrix4x4 *labelIJKToRAS = vtkMatrix4x4::New();
+        volumeNode->GetIJKToRASMatrix(volumeIJKToRAS);
+        labelNode->GetIJKToRASMatrix(labelIJKToRAS);
+        for (row = 0; row < 4; row++)
+          {
+          for (column = 0; column < 4; column++)
+            {
+            volumeValue = volumeIJKToRAS->GetElement(row,column);
+            labelValue = labelIJKToRAS->GetElement(row,column);
+            if (volumeValue != labelValue)
+              {
+              warnings << "IJKToRAS mismatch at [" << row << ", " << column << "] (" << volumeValue << " != " << labelValue << ")\n";
+              }
+            }
+          }
+        volumeIJKToRAS->Delete();
+        labelIJKToRAS->Delete();
+        }
+      }
+    }
+  return (warnings.str());
+}
+
+//----------------------------------------------------------------------------
 vtkMRMLScalarVolumeNode*
 vtkSlicerVolumesLogic::CloneVolume(vtkMRMLVolumeNode *volumeNode, const char *name)
 {

Modules/Loadable/Volumes/Logic/vtkSlicerVolumesLogic.h
@@ -180,6 +180,18 @@ public:
                                                        vtkMRMLScalarVolumeNode *labelNode,
                                                        vtkMRMLVolumeNode *templateNode);
 
+  /// Generate a string listing any warnings about the spatial validity of
+  /// the labelmap with respect to the volume.  An empty string indicates
+  /// that the two volumes are identical samplings of the same spatial region.
+  /// Checks include:
+  ///  Valid image data.
+  ///  Same dimensions.
+  ///  Same spacing.
+  ///  Same origin.
+  ///  Same IJKtoRAS.
+  std::string CheckForLabelVolumeValidity(vtkMRMLScalarVolumeNode *volumeNode,
+                                          vtkMRMLScalarVolumeNode *labelNode);
+
 
   /// Create a deep copy of a \a volumeNode and add it to the current scene
   /// \sa GetMRMLScene()

Modules/Scripted/EditorLib/HelperBox.py
@@ -106,39 +106,6 @@ class HelperBox(object):
       self.setMergeVolume( merge )
     self.select()
 
-  def checkForVolumeWarnings(self,master,merge):
-    """Verify that volumes are compatible with label calculation
-    algorithm assumptions.  Returns warning text of empty
-    string if none.
-    """
-    warnings = ""
-    if not master:
-      warnings = "Missing master volume"
-    if merge:
-      if not master.GetImageData() or not merge.GetImageData():
-        return "Missing image data"
-      if master.GetImageData().GetDimensions() != merge.GetImageData().GetDimensions():
-        warnings += "Volume dimensions do not match\n"
-        warnings += str(master.GetImageData().GetDimensions())
-        warnings += " vs "
-        warnings += str(merge.GetImageData().GetDimensions())
-      if master.GetImageData().GetSpacing() != merge.GetImageData().GetSpacing():
-        warnings += "Volume spacings do not match\n"
-      if master.GetImageData().GetOrigin() != merge.GetImageData().GetOrigin():
-        warnings += "Volume spacings do not match\n"
-      if merge.GetImageData().GetScalarType() != vtk.VTK_SHORT:
-        warnings += "Label map must be of type Short (Use Cast Scalar Volume to fix)\n"
-      masterIJKToRAS = vtk.vtkMatrix4x4()
-      mergeIJKToRAS = vtk.vtkMatrix4x4()
-      master.GetIJKToRASMatrix(masterIJKToRAS)
-      merge.GetIJKToRASMatrix(mergeIJKToRAS)
-      for row in range(4):
-        for column in range(4):
-          if masterIJKToRAS.GetElement(row,column) != mergeIJKToRAS.GetElement(row,column):
-            warnings += "Volume geometry does not match (Resample to fix)\n"
-            break
-    return warnings
-
   def select(self):
     """select master volume - load merge volume if one with the correct name exists"""
 
@@ -165,9 +132,10 @@ class HelperBox(object):
     self.mergeName.setText( mergeText )
     self.updateStructures()
 
-    if self.master or merge:
-      warnings = self.checkForVolumeWarnings(self.master,self.merge)
+    if self.master and merge:
+      warnings = self.volumesLogic.CheckForLabelVolumeValidity(self.master,self.merge)
       if warnings != "":
+        warnings = "Geometry of master and merge volumes do not match.\n\n" + warnings
         self.errorDialog( "Warning: %s" % warnings )
 
     # trigger a modified event on the parameter node so that other parts of the GUI

Modules/Scripted/LabelStatistics/CMakeLists.txt
@@ -17,3 +17,12 @@ slicerMacroBuildScriptedModule(
   RESOURCES ${MODULE_PYTHON_RESOURCES}
   WITH_GENERIC_TESTS
   )
+
+#-----------------------------------------------------------------------------
+if(BUILD_TESTING)
+
+  # Register the unittest subclass in the main script as a ctest.
+  # Note that the test will also be available at runtime.
+  slicer_add_python_unittest(SCRIPT ${MODULE_NAME}.py)
+
+endif()

Modules/Scripted/LabelStatistics/LabelStatistics.py
@@ -1,3 +1,4 @@
+import unittest
 from __main__ import vtk, qt, ctk, slicer
 
 #
@@ -146,23 +147,15 @@ class LabelStatisticsWidget:
     self.labelNode = node
     self.applyButton.enabled = bool(self.grayscaleNode) and bool(self.labelNode)
 
-  def volumesAreValid(self):
-    """Verify that volumes are compatible with label calculation
-    algorithm assumptions"""
-    if not self.grayscaleNode or not self.labelNode:
-      return False
-    if not self.grayscaleNode.GetImageData() or not self.labelNode.GetImageData():
-      return False
-    if self.grayscaleNode.GetImageData().GetDimensions() != self.labelNode.GetImageData().GetDimensions():
-      return False
-    return True
-
   def onApply(self):
     """Calculate the label statistics
     """
-    if not self.volumesAreValid():
+
+    volumesLogic = slicer.modules.volumes.logic()
+    warnings = volumesLogic.CheckForLabelVolumeValidity(self.grayscaleNode, self.labelNode)
+    if warnings != "":
       qt.QMessageBox.warning(slicer.util.mainWindow(),
-          "Label Statistics", "Volumes do not have the same geometry.")
+          "Label Statistics", "Volumes do not have the same geometry.\n%s" % warnings)
       return
 
     self.applyButton.text = "Working..."
@@ -398,6 +391,71 @@ class LabelStatisticsLogic:
     fp.write(self.statsAsCSV())
     fp.close()
 
+class LabelStatisticsTest(unittest.TestCase):
+  """
+  This is the test case.
+  """
+
+  def delayDisplay(self,message,msec=1000):
+    """This utility method displays a small dialog and waits.
+    This does two things: 1) it lets the event loop catch up
+    to the state of the test so that rendering and widget updates
+    have all taken place before the test continues and 2) it
+    shows the user/developer/tester the state of the test
+    so that we'll know when it breaks.
+    """
+    print(message)
+    self.info = qt.QDialog()
+    self.infoLayout = qt.QVBoxLayout()
+    self.info.setLayout(self.infoLayout)
+    self.label = qt.QLabel(message,self.info)
+    self.infoLayout.addWidget(self.label)
+    qt.QTimer.singleShot(msec, self.info.close)
+    self.info.exec_()
+
+  def setUp(self):
+    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+    slicer.mrmlScene.Clear(0)
+
+  def runTest(self,scenario=None):
+    """Run as few or as many tests as needed here.
+    """
+    self.setUp()
+    self.test_LabelStatisticsBasic()
+
+  def test_LabelStatisticsBasic(self):
+    """
+    This tests some aspects of the label statistics
+    """
+
+    self.delayDisplay("Starting test_LabelStatisticsBasic")
+    #
+    # first, get some data
+    #
+    import SampleData
+    sampleDataLogic = SampleData.SampleDataLogic()
+    mrHead = sampleDataLogic.downloadMRHead()
+    ctChest = sampleDataLogic.downloadCTChest()
+    self.delayDisplay('Two data sets loaded')
+
+    volumesLogic = slicer.modules.volumes.logic()
+
+    mrHeadLabel = volumesLogic.CreateAndAddLabelVolume( slicer.mrmlScene, mrHead, "mrHead-label" )
+
+    warnings = volumesLogic.CheckForLabelVolumeValidity(ctChest, mrHeadLabel)
+
+    self.delayDisplay("Warnings for mismatch:\n%s" % warnings)
+
+    self.assertTrue( warnings != "" )
+
+    warnings = volumesLogic.CheckForLabelVolumeValidity(mrHead, mrHeadLabel)
+
+    self.delayDisplay("Warnings for match:\n%s" % warnings)
+
+    self.assertTrue( warnings == "" )
+
+    self.delayDisplay('test_LabelStatisticsBasic passed!')
 
 class Slicelet(object):
   """A slicer slicelet is a module widget that comes up in stand alone mode

commit 01b4a86ecea2a8d2284da5e56ef1d6c112ad1f3c

BUG: Fix CheckForLabelVolumeValidity warning. Fixes #3776
This commit fixes a regression introduced in r23406 [1]
where calling CheckForLabelVolumeValidity with two volumes that have
the same dimension was returning a wrong warning, e.g. "IJKToRAS mismatch
at [0, 3] (-89.6207 != -89.6207)", "IJKToRAS mismatch
at [1, 1] (0.000542851 != 0.000542851)", and "IJKToRAS mismatch
at [2, 0] (4.32616e-09 != 4.32616e-09) IJKToRAS mismatch
at [2, 2] (0.0752821 != 0.0752821)", and "IJKToRAS mismatch
at [2, 3] (176.197 != 176.197)".

Thanks to "safir" for reporting the problem and suggesting a solution.

[1] http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23406

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23488 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Volumes/Logic/vtkSlicerVolumesLogic.cxx
@@ -40,6 +40,7 @@
 #include <vtkCallbackCommand.h>
 #include <vtkImageData.h>
 #include <vtkImageThreshold.h>
+#include <vtkMathUtilities.h>
 #include <vtkMatrix4x4.h>
 #include <vtkNew.h>
 #include <vtkObjectFactory.h>
@@ -926,7 +927,7 @@ vtkSlicerVolumesLogic::CheckForLabelVolumeValidity(vtkMRMLScalarVolumeNode *volu
             {
             volumeValue = volumeIJKToRAS->GetElement(row,column);
             labelValue = labelIJKToRAS->GetElement(row,column);
-            if (volumeValue != labelValue)
+            if (!vtkMathUtilities::FuzzyCompare<double>(volumeValue, labelValue))
               {
               warnings << "IJKToRAS mismatch at [" << row << ", " << column << "] (" << volumeValue << " != " << labelValue << ")\n";
               }