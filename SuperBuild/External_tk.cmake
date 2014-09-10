NA-MIC/svn.slicer.org-Slicer4

commit e9fbe8df35b0b1c7a8a52b76783b13a38c9c34c3

COMP: Update version of tcl/tk/incrTcl used on Linux/MacOSX. Issue #3809
In an attempt to help addressing #3809, this commit updates the ancient
version of Tcl from 8.4 to 8.6 and also update incrTcl from 3.2 to 4.0.

This should fix potential issues specific to MacOSX 10.9 and above.

Also remove unused CMake script 'SlicerBlockStringFindReplace.cmake'

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23613 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_tk.cmake
@@ -74,8 +74,8 @@ ExternalProject_Execute(${proj} \"install\" make install)
 
     ExternalProject_Add(${proj}
       ${${proj}_EP_ARGS}
-      SVN_REPOSITORY "http://svn.slicer.org/Slicer3-lib-mirrors/trunk/tcl/tk"
-      SVN_REVISION -r "114"
+      URL "http://slicer.kitware.com/midas3/download/item/155631/tk8.6.1-src.tar.gz"
+      URL_MD5 "63f21c3a0e0cefbd854b4eb29b129ac6"
       UPDATE_COMMAND "" # Disable update
       SOURCE_DIR tcl/tk
       BUILD_IN_SOURCE 1

CMake/SlicerBlockStringFindReplace.cmake
@@ -1,6 +1,0 @@
-file(READ ${in} code)
-string(REPLACE "${find}" "${replace}" code_updated "${code}")
-# Write output file only if required
-if(NOT "${code}" STREQUAL "${code_updated}")
-  file(WRITE ${out} "${code_updated}")
-endif()

SuperBuild/External_incrTcl.cmake
@@ -13,23 +13,12 @@ ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj
 
 if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
-  set(INCR_TCL_VERSION_DOT "3.2")
-  set(INCR_TCL_VERSION "32")
-  set(incrTcl_SVN_REPOSITORY "http://svn.slicer.org/Slicer3-lib-mirrors/trunk/tcl/incrTcl")
-  set(incrTcl_SVN_REVISION -r "4")
-  set(incrTcl_PATCH_COMMAND "")
-
-  if(APPLE)
-    set(incrTcl_configure ${tcl_base}/incrTcl/itcl/configure)
-    set(incrTcl_configure_find "*.c | *.o | *.obj) \;\;")
-    set(incrTcl_configure_replace "*.c | *.o | *.obj | *.dSYM | *.gnoc ) \;\;")
-
-    set(script ${CMAKE_CURRENT_SOURCE_DIR}/CMake/SlicerBlockStringFindReplace.cmake)
-    set(in ${incrTcl_configure})
-    set(out ${incrTcl_configure})
-
-    set(incrTcl_PATCH_COMMAND ${CMAKE_COMMAND} -Din=${in} -Dout=${out} -Dfind=${incrTcl_configure_find} -Dreplace=${incrTcl_configure_replace} -P ${script})
-  endif()
+  set(INCR_TCL_VERSION_DOT "4.0")
+  set(INCR_TCL_VERSION "40")
+  set(incrTcl_DOWNLOAD_COMMAND
+    URL "http://slicer.kitware.com/midas3/download/item/155632/itcl4.0.1.tar.gz"
+    URL_MD5 "8d71d4247b19ad5d6bcbb776590ced55"
+    )
 
   if(UNIX)
 
@@ -67,12 +56,10 @@ ExternalProject_Execute(${proj} \"install\" make install)
 
     ExternalProject_Add(${proj}
       ${${proj}_EP_ARGS}
-      SVN_REPOSITORY ${incrTcl_SVN_REPOSITORY}
-      SVN_REVISION ${incrTcl_SVN_REVISION}
+      ${incrTcl_DOWNLOAD_COMMAND}
       UPDATE_COMMAND "" # Disable update
       SOURCE_DIR tcl/incrTcl
       BUILD_IN_SOURCE 1
-      PATCH_COMMAND ${incrTcl_PATCH_COMMAND}
       CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${_configure_script}
       BUILD_COMMAND ${CMAKE_COMMAND} -P ${_build_script}
       INSTALL_COMMAND ${CMAKE_COMMAND} -P ${_install_script}

SuperBuild/External_tcl.cmake
@@ -30,8 +30,7 @@ if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   set(tcl_base ${CMAKE_CURRENT_BINARY_DIR}/tcl)
   set(tcl_build ${CMAKE_CURRENT_BINARY_DIR}/tcl-build)
 
-  set(TCL_TK_VERSION_DOT "8.4")
-  set(TCL_TK_VERSION "84")
+  set(tcl_DOWNLOAD_COMMAND)
 
   if(WIN32)
     if("${CMAKE_SIZEOF_VOID_P}" EQUAL 8)
@@ -42,21 +41,32 @@ if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       set(tcl_SVN_REPOSITORY "http://svn.slicer.org/Slicer3-lib-mirrors/trunk/Binaries/Windows/tcl85-x64-build")
       set(tcl_SVN_REVISION -r "184")
     else()
+      set(TCL_TK_VERSION_DOT "8.4")
+      set(TCL_TK_VERSION "84")
       set(INCR_TCL_VERSION_DOT "3.2")
       set(INCR_TCL_VERSION "32")
       set(tcl_SVN_REPOSITORY "http://svn.slicer.org/Slicer3-lib-mirrors/trunk/Binaries/Windows/tcl-build")
       set(tcl_SVN_REVISION -r "176")
     endif()
+    set(tcl_DOWNLOAD_COMMAND
+      SVN_REPOSITORY ${tcl_SVN_REPOSITORY}
+      SVN_REVISION ${tcl_SVN_REVISION}
+      )
     set(tcl_SOURCE_DIR tcl-build)
     mark_as_superbuild(
       INCR_TCL_VERSION_DOT:STRING
       INCR_TCL_VERSION:STRING
       )
   else()
-    set(tcl_SVN_REPOSITORY "http://svn.slicer.org/Slicer3-lib-mirrors/trunk/tcl/tcl")
-    set(tcl_SVN_REVISION -r "81")
+    set(TCL_TK_VERSION_DOT "8.6")
+    set(TCL_TK_VERSION "86")
     set(tcl_SOURCE_DIR tcl/tcl)
     set(tcl_BUILD_IN_SOURCE 1)
+
+    set(tcl_DOWNLOAD_COMMAND
+      URL "http://slicer.kitware.com/midas3/download/item/155630/tcl8.6.1-src.tar.gz"
+      URL_MD5 "aae4b701ee527c6e4e1a6f9c7399882e"
+      )
 
     include(ExternalProjectForNonCMakeProject)
 
@@ -106,8 +116,7 @@ ExternalProject_Execute(${proj} \"install\" make install)
 
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
-    SVN_REPOSITORY ${tcl_SVN_REPOSITORY}
-    SVN_REVISION ${tcl_SVN_REVISION}
+    ${tcl_DOWNLOAD_COMMAND}
     UPDATE_COMMAND "" # Disable update
     SOURCE_DIR ${tcl_SOURCE_DIR}
     BUILD_IN_SOURCE ${tcl_BUILD_IN_SOURCE}

commit 272ba4921c19497679359f9a69fa6557590b80e0

BUG: Fix packaging of libtk8.6.dylib on MacOSX. Fixes #3822
This commit fixes a regression introduced by r23613 (attempts to
address #3809 by updating tcl version)

Tk 8.6 build system has a known problem building the tk shared library.
To ensure fixup_bundle properly process the _tkinter.so python module,
we implemented the following workaround to fix the library id.
For more details
  http://sourceforge.net/p/tcl/mailman/message/30354096/
  http://na-mic.org/Mantis/view.php?id=3822

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23667 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_tk.cmake
@@ -97,17 +97,29 @@ ExternalProject_Execute(${proj} \"install\" make install)
       )
 
     #-----------------------------------------------------------------------------
-    # Since fixup_bundle expects the library to be writable, let's add an extra step
-    # to make sure it's the case.
+    # Extra steps to work-around tk build system issues
     if(APPLE)
       foreach(var tcl_build TCL_TK_VERSION_DOT)
         if(NOT DEFINED ${var})
           message(FATAL_ERROR "error: ${var} is not defined !")
         endif()
       endforeach()
+      set(_tk_library ${tcl_build}/lib/libtk${TCL_TK_VERSION_DOT}.dylib)
+      # XXX Since fixup_bundle expects the library to be writable, let's add an
+      # extra step to make sure it's the case.
       ExternalProject_Add_Step(${proj} tk_install_chmod_library
-        COMMAND chmod u+xw ${tcl_build}/lib/libtk${TCL_TK_VERSION_DOT}.dylib
+        COMMAND chmod u+xw ${_tk_library}
         DEPENDEES install
+        )
+      # XXX Tk build system has a known problem building the tk shared library.
+      # To ensure fixup_bundle properly process the _tkinter.so python module,
+      # we implemented the following workaround to fix the library id.
+      # For more details
+      #   http://sourceforge.net/p/tcl/mailman/message/30354096/
+      #   http://na-mic.org/Mantis/view.php?id=3822
+      ExternalProject_Add_Step(${proj} tk_install_fix_library_id
+        COMMAND install_name_tool -id ${_tk_library} ${_tk_library}
+        DEPENDEES tk_install_chmod_library
         )
     endif()
 