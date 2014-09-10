NA-MIC/svn.slicer.org-Slicer4

commit bbe2cf91f6b5d4d8506dee66728458d8d7d27ae9

BUG: fixes for 3808 fiducial picking issue
See http://na-mic.org/Bug/view.php?id=3808

Note that fixing this requires the corresponding fixes
to VTKv6 and LandmarkRegistration

https://github.com/pieper/LandmarkRegistration/commit/d9393f515b4798d9904fdd0383d959b91fc1edfb

https://github.com/pieper/VTK/commit/f3d6cc2503375724e60dd73d9a93ba9c764be26e

COMP: fix repository url and error macro usage

From: Steve Pieper <pieper@bwh.harvard.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23648 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Markups/MRMLDM/vtkMRMLMarkupsFiducialDisplayableManager2D.cxx
@@ -148,7 +148,7 @@ public:
       vtkSeedRepresentation * representation = vtkSeedRepresentation::SafeDownCast(this->Widget->GetRepresentation());
       if (!representation)
         {
-        std::cerr << "Representation is null.\n";
+        vtkErrorWithObjectMacro(this->Widget, "Representation is null.");
         return;
         }
 
@@ -254,6 +254,9 @@ vtkAbstractWidget * vtkMRMLMarkupsFiducialDisplayableManager2D::CreateWidget(vtk
     }
 
   vtkNew<vtkSeedRepresentation> rep;
+
+  vtkDebugMacro("making handle for fiducialNode " << fiducialNode->GetName());
+  vtkDebugMacro(" for sliceNode " << this->GetSliceNode()->GetName());
 
   if (!this->IsInLightboxMode())
     {
@@ -670,6 +673,7 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::SetNthSeed(int n, vtkMRMLMarkup
       {
       handleRep->VisibilityOn();
       handleRep->HandleVisibilityOn();
+      handleRep->EnablePicking();
       if (textString.compare("") != 0)
         {
         handleRep->LabelVisibilityOn();
@@ -687,7 +691,12 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::SetNthSeed(int n, vtkMRMLMarkup
       handleRep->VisibilityOff();
       handleRep->HandleVisibilityOff();
       handleRep->LabelVisibilityOff();
-      seedWidget->GetSeed(n)->EnabledOff();
+      handleRep->DisablePicking();
+      vtkSeedRepresentation *seedRepresentation = vtkSeedRepresentation::SafeDownCast(seedWidget->GetRepresentation());
+      if (seedRepresentation)
+        {
+        seedRepresentation->GetHandleRepresentation()->DisablePicking();
+        }
 
       // if the widget is not shown on the slice, show the intersection
       if (fiducialNode &&
@@ -778,6 +787,8 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::SetNthSeed(int n, vtkMRMLMarkup
 
               if (handleRep)
                 {
+                handleRep->DisablePicking();
+
                 vtkNew<vtkMarkupsGlyphSource2D> glyphSource;
                 glyphSource->SetGlyphType(glyphType);
                 glyphSource->SetScale(glyphScale);
@@ -862,6 +873,7 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::SetNthSeed(int n, vtkMRMLMarkup
     }
   else if (pointHandleRep)
     {
+    pointHandleRep->DisablePicking();
     // set the glyph type - TBD, swapping isn't working
     // set the color
     if (fiducialNode->GetNthFiducialSelected(n))
@@ -992,11 +1004,30 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::PropagateMRMLToWidget(vtkMRMLMa
 
   vtkDebugMacro("Fids PropagateMRMLToWidget, node num markups = " << numberOfFiducials);
 
+
+  if (numberOfFiducials == 0)
+    {
+    handleRep->DisablePicking();
+    int seed = 0;
+    vtkHandleWidget *handleWidget;
+    while ( (handleWidget = seedWidget->GetSeed(seed)) )
+      {
+      vtkHandleRepresentation *handleRepresentation = handleWidget->GetHandleRepresentation();
+      handleRepresentation->DisablePicking();
+      seed++;
+      }
+    }
+  else
+    {
+    handleRep->EnablePicking();
+    }
+
   for (int n = 0; n < numberOfFiducials; n++)
     {
     // std::cout << "Fids PropagateMRMLToWidget: n = " << n << std::endl;
     this->SetNthSeed(n, fiducialNode, seedWidget);
     }
+
 
   // now update the position of all the seeds - done in SetNthSeed now
   //this->UpdatePosition(widget, node);

SuperBuild.cmake
@@ -293,7 +293,7 @@ list_conditional_append(Slicer_BUILD_CompareVolumes Slicer_REMOTE_DEPENDENCIES C
 
 Slicer_Remote_Add(LandmarkRegistration
   GIT_REPOSITORY "${git_protocol}://github.com/pieper/LandmarkRegistration"
-  GIT_TAG "d56d91aad9813c48e67e07afabde72cec4660146"
+  GIT_TAG "d9393f515b4798d9904fdd0383d959b91fc1edfb"
   OPTION_NAME Slicer_BUILD_LandmarkRegistration
   OPTION_DEPENDS "Slicer_BUILD_CompareVolumes;Slicer_USE_PYTHONQT"
   LABELS REMOTE_MODULE

SuperBuild/External_VTKv6.cmake
@@ -93,8 +93,8 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT ${CMAKE_PROJECT_N
     set(VTK_ENABLE_KITS 1)
   endif()
 
-  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY "github.com/Slicer/VTK.git" CACHE STRING "Repository from which to get VTK" FORCE)
-  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG "003cbd4c4459c289d7593ae93f90bdbe7880438d" CACHE STRING "VTK git tag to use" FORCE)
+  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY "github.com/pieper/VTK.git" CACHE STRING "Repository from which to get VTK" FORCE)
+  set(${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG "3808-fiducial-picking" CACHE STRING "VTK git tag to use" FORCE)
 
   mark_as_advanced(${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG)
 

commit 00c49c277aaa5ea0228e46f022495d5bf94571d3

BUG: Proceed to MarkupsFiducial picking manager configuration only for VTK6.
This commit fixes regression introduced in r23648 that was preventing
Slicer from being compiled against VTK5.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23665 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Markups/MRMLDM/vtkMRMLMarkupsFiducialDisplayableManager2D.cxx
@@ -673,7 +673,9 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::SetNthSeed(int n, vtkMRMLMarkup
       {
       handleRep->VisibilityOn();
       handleRep->HandleVisibilityOn();
+#if (VTK_MAJOR_VERSION >= 6)
       handleRep->EnablePicking();
+#endif
       if (textString.compare("") != 0)
         {
         handleRep->LabelVisibilityOn();
@@ -691,12 +693,16 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::SetNthSeed(int n, vtkMRMLMarkup
       handleRep->VisibilityOff();
       handleRep->HandleVisibilityOff();
       handleRep->LabelVisibilityOff();
+#if (VTK_MAJOR_VERSION >= 6)
       handleRep->DisablePicking();
       vtkSeedRepresentation *seedRepresentation = vtkSeedRepresentation::SafeDownCast(seedWidget->GetRepresentation());
       if (seedRepresentation)
         {
         seedRepresentation->GetHandleRepresentation()->DisablePicking();
         }
+#else
+      seedWidget->GetSeed(n)->EnabledOff();
+#endif
 
       // if the widget is not shown on the slice, show the intersection
       if (fiducialNode &&
@@ -787,7 +793,9 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::SetNthSeed(int n, vtkMRMLMarkup
 
               if (handleRep)
                 {
+#if (VTK_MAJOR_VERSION >= 6)
                 handleRep->DisablePicking();
+#endif
 
                 vtkNew<vtkMarkupsGlyphSource2D> glyphSource;
                 glyphSource->SetGlyphType(glyphType);
@@ -873,7 +881,9 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::SetNthSeed(int n, vtkMRMLMarkup
     }
   else if (pointHandleRep)
     {
+#if (VTK_MAJOR_VERSION >= 6)
     pointHandleRep->DisablePicking();
+#endif
     // set the glyph type - TBD, swapping isn't working
     // set the color
     if (fiducialNode->GetNthFiducialSelected(n))
@@ -1004,7 +1014,7 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::PropagateMRMLToWidget(vtkMRMLMa
 
   vtkDebugMacro("Fids PropagateMRMLToWidget, node num markups = " << numberOfFiducials);
 
-
+#if (VTK_MAJOR_VERSION >= 6)
   if (numberOfFiducials == 0)
     {
     if (handleRep)
@@ -1030,6 +1040,7 @@ void vtkMRMLMarkupsFiducialDisplayableManager2D::PropagateMRMLToWidget(vtkMRMLMa
       handleRep->EnablePicking();
       }
     }
+#endif
 
   for (int n = 0; n < numberOfFiducials; n++)
     {