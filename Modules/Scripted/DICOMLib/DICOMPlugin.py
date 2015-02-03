NA-MIC/svn.slicer.org-Slicer4

commit 64c82e879cd6e0908ff72dc1a0f790668011112a

ENH: Transfer referencedInstanceUIDs to the MRML attribute
If DICOM import plugin wants to store referenced instance UIDs, then it fills referencedInstanceUIDs attribute in the loadable. This commit makes sure this is transferred to the loaded subject hierarchy node so that the referenced nodes are highlighted in subject hierarchy.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23939 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DICOMLib/DICOMPlugin.py
@@ -200,6 +200,14 @@ class DICOMPlugin(object):
       instanceUIDs += uid + " "
     instanceUIDs = instanceUIDs[:-1]  # strip last space
     seriesNode.AddUID(slicer.vtkMRMLSubjectHierarchyConstants.GetDICOMInstanceUIDName(), instanceUIDs)
+    
+    # Set referenced instance UIDs from loadable to series
+    referencedInstanceUIDs = ""
+    if hasattr(loadable,'referencedInstanceUIDs'):
+      for instanceUID in loadable.referencedInstanceUIDs:
+      referencedInstanceUIDs += instanceUID + " "
+    referencedInstanceUIDs = referencedInstanceUIDs[:-1]  # strip last space
+    seriesNode.SetAttribute(vtkMRMLSubjectHierarchyConstants.GetDICOMReferencedInstanceUIDsAttributeName(), referencedInstanceUIDs)
 
     # Add series node to hierarchy under the right study and patient nodes. If they are present then used, if not, then created
     patientId = slicer.dicomDatabase.fileValue(firstFile,tags['patientID'])

commit 6c524ce98171986809d3fcb4f009a66cb348bc74

BUG: Fix IndentationError in DICOMPlugin.py
This commit fixes regression introduced in r23939 (ENH: Transfer
referencedInstanceUIDs to the MRML attribute).

Runtime error:

// -------------
Fail to instantiate module "DICOMSlicerDataBundlePlugin"
Traceback (most recent call last):
  File "<string>", line 1, in <module>
  File "/path/to/Slicer-build/lib/Slicer-4.4/qt-scripted-modules/MultiVolumeImporterPlugin.py", line 4, in <module>
    from DICOMLib import DICOMPlugin
  File "/path/to/Slicer-build/lib/Slicer-4.4/qt-scripted-modules/DICOMLib/__init__.py", line 5, in <module>
    from DICOMPlugin import *
  File "/path/to/Slicer-build/lib/Slicer-4.4/qt-scripted-modules/DICOMLib/DICOMPlugin.py", line 208
    referencedInstanceUIDs += instanceUID + " "
                         ^
IndentationError: expected an indented bloc
// -------------

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23945 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DICOMLib/DICOMPlugin.py
@@ -205,7 +205,7 @@ class DICOMPlugin(object):
     referencedInstanceUIDs = ""
     if hasattr(loadable,'referencedInstanceUIDs'):
       for instanceUID in loadable.referencedInstanceUIDs:
-      referencedInstanceUIDs += instanceUID + " "
+        referencedInstanceUIDs += instanceUID + " "
     referencedInstanceUIDs = referencedInstanceUIDs[:-1]  # strip last space
     seriesNode.SetAttribute(vtkMRMLSubjectHierarchyConstants.GetDICOMReferencedInstanceUIDsAttributeName(), referencedInstanceUIDs)
 