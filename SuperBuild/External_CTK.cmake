NA-MIC/svn.slicer.org-Slicer4

commit dd75e2d42e1295c25375c3b9fac4736fb0e645ee

ENH: Update CTK to include python decorator update and external project fix
$ git shortlog e23537d..d8cd14e --no-merges
Jean-Christophe Fillion-Robin (2):
      Improve ctkVTKHistogram metaobject for better pythonqt wrapping
      Extend ctkTransferFunction*Item for better pythonqt wrapping

Sascha Zelzer (5):
      Removed left-over method from git merge without implementation.
      Support CMAKE_DEBUG_POSTFIX for CTK superbuild and external DCMTK.
      Ensure find_package() is called in CTKConfig.cmake for external projects.
      Some systems need PYTHON_INCLUDE_DIR2 for FindPythonLibs.cmake.
      Added custom moc options again when moc'ing plug-in headers.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23958 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "e23537d146048845b092ad2320f0f42f659aa838"
+    GIT_TAG "d8cd14e7cd431732fa80206aadca5e6488417578"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS

CMake/SlicerBlockInstallDCMTKApps.cmake
@@ -8,7 +8,7 @@ if(CMAKE_CONFIGURATION_TYPES)
   set(int_dir "Release/")
 endif()
 foreach(dcmtk_App ${DCMTK_Apps})
-  install(PROGRAMS ${CTK_DCMTK_DIR}/bin/${int_dir}${dcmtk_App}${CMAKE_EXECUTABLE_SUFFIX}
+  install(PROGRAMS ${DCMTK_DIR}/bin/${int_dir}${dcmtk_App}${CMAKE_EXECUTABLE_SUFFIX}
     DESTINATION ${Slicer_INSTALL_BIN_DIR}
     COMPONENT Runtime
     )

CMake/UseSlicer.cmake.in
@@ -139,7 +139,7 @@ else()
 endif()
 
 # Set '<PROJECTNAME>_DIR' variable(s) so that find_package(<PROJECTNAME>) works as expected.
-set(PYTHONQT_INSTALL_DIR ${CTK_PYTHONQT_INSTALL_DIR})
+# NA
 
 # Find PythonQt package so that PYTHONQT_USE_RELEASE_PYTHON_FALLBACK is defined
 # See https://github.com/commontk/PythonQt/issues/7

CMakeLists.txt
@@ -739,15 +739,6 @@ find_package(CTK REQUIRED)
 include(${CTK_USE_FILE})
 
 #-----------------------------------------------------------------------------
-# QtTesting
-#-----------------------------------------------------------------------------
-if(Slicer_USE_QtTesting)
-  set(QtTesting_INSTALL_DIR ${CTK_QtTesting_INSTALL_DIR})
-  set(QtTesting_DIR ${CTK_QtTesting_DIR})
-  find_package(QtTesting REQUIRED)
-endif()
-
-#-----------------------------------------------------------------------------
 # Update CMake module path
 #-----------------------------------------------------------------------------
 set(CMAKE_MODULE_PATH
@@ -782,7 +773,6 @@ endif()
 # PythonQt
 #-----------------------------------------------------------------------------
 if(Slicer_USE_PYTHONQT)
-  set(PYTHONQT_INSTALL_DIR ${CTK_PYTHONQT_INSTALL_DIR})
   # Find PythonQt package so that PYTHONQT_USE_RELEASE_PYTHON_FALLBACK is defined
   # See https://github.com/commontk/PythonQt/issues/7
   # and https://github.com/commontk/PythonQt/issues/8

commit 29ed7e6482d442e8b104757b16e9a4efbf3a05ee

COMP: Update CTK to fix Slicer packaging issue on windows and linux
This commit fixes a regression introduced in r23958 and discussed in [1]

[1] http://slicer-devel.65872.n3.nabble.com/Windows-and-Linux-nightly-packages-tp4033451p4033452.html

// -------------
Author: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
Date:   Mon Feb 9 17:02:42 2015 -0500

    Ensure PYTHONQT_INSTALL_DIR is not reset in project including CTK

    This commit is an attempt to fix a regression introduced by commontk/CTK@fc92239
    that was causing the Slicer packaging to fail (mainly because the
    variable PYTHONQT_INSTALL_DIR was reset to an empty value after
    "find_path" was called in FindPythonQt.cmake)

    Note that this a workaround, the issue is being discussed with CMake
    developer.
// -------------

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23968 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTK.cmake
@@ -58,7 +58,7 @@ if(NOT DEFINED CTK_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${git_protocol}://github.com/commontk/CTK.git"
-    GIT_TAG "0592a4ec6d3a217dab09d40787dc9cd58a959c62"
+    GIT_TAG "59dfc7f24b0355be907a030a351fbd9f065794bf"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${proj}-build
     CMAKE_CACHE_ARGS