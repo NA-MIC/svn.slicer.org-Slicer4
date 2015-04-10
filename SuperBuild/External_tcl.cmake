NA-MIC/svn.slicer.org-Slicer4

commit 3a8c932527fe2c95c2fd316df42aa4f53015da45

BUG: Create zip archive and move tcl binaries onto slicer.kitware.com
This should help address issue preventing binaries from being downloaded
on some version of Windows. The error reported was:

// -----------
1>------ Build started: Project: tcl, Configuration: Release x64 ------

1>Performing download step (SVN checkout) for 'tcl'

1>svn: E175002: Unable to connect to a repository at URL 'http://svn.slicer.org/Slicer3-lib-mirrors/trunk/Binaries/Windows/tcl85-x64-build'

1>svn: E175002: Unexpected HTTP status 413 'Request Entity Too Large' on '/Slicer3-lib-mirrors/trunk/Binaries/Windows/tcl85-x64-build'

1>Project : error PRJ0019: A tool returned an error code from "Performing download step (SVN checkout) for 'tcl'"

1>Build log was saved at "file://c:\build\s3d\tcl.dir\Release\BuildLog.htm"
// --------------

For more details, see http://slicer-devel.65872.n3.nabble.com/SVN-TCL-donwload-issue-tt4032460.html

Thanks to "Bradley Lowekamp <blowekamp@mail.nih.gov>" for reporting the problem.

Solution suggested by "Steve Pieper <pieper@bwh.harvard.edu>"

git-svn-id: http://svn.slicer.org/Slicer4/trunk@23702 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_tcl.cmake
@@ -38,19 +38,19 @@ if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       set(TCL_TK_VERSION "85")
       set(INCR_TCL_VERSION_DOT "3.4")
       set(INCR_TCL_VERSION "34")
-      set(tcl_SVN_REPOSITORY "http://svn.slicer.org/Slicer3-lib-mirrors/trunk/Binaries/Windows/tcl85-x64-build")
-      set(tcl_SVN_REVISION -r "184")
+      set(tcl_URL "http://slicer.kitware.com/midas3/download/item/159433/tcl85-x64-build.zip")
+      set(tcl_MD5 "10f0a12590acaaef924dc22e80c9d70a")
     else()
       set(TCL_TK_VERSION_DOT "8.4")
       set(TCL_TK_VERSION "84")
       set(INCR_TCL_VERSION_DOT "3.2")
       set(INCR_TCL_VERSION "32")
-      set(tcl_SVN_REPOSITORY "http://svn.slicer.org/Slicer3-lib-mirrors/trunk/Binaries/Windows/tcl-build")
-      set(tcl_SVN_REVISION -r "176")
+      set(tcl_URL "http://slicer.kitware.com/midas3/download/item/159432/tcl85-build.zip")
+      set(tcl_MD5 "07401cf7128a9a79403c8d9b745024a6")
     endif()
     set(tcl_DOWNLOAD_COMMAND
-      SVN_REPOSITORY ${tcl_SVN_REPOSITORY}
-      SVN_REVISION ${tcl_SVN_REVISION}
+      URL ${tcl_URL}
+      URL_MD5 ${tcl_MD5}
       )
     set(tcl_SOURCE_DIR tcl-build)
     mark_as_superbuild(

commit 08929c30cb79e8b07c7e9e4cc93f45310a82dac8

COMP: Fix build of tcl on Windows 32-bit (See #3984)
This commit fixes regression introduced by r23702 (BUG: Create zip
archive and move tcl binaries onto slicer.kitware.com)

Suggested-by: Christopher Mullins <christopherrmullins@gmail.com>
Tested-by: Sharp, Gregory C. <GCSHARP@mgh.harvard.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24168 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_tcl.cmake
@@ -33,18 +33,14 @@ if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   set(tcl_DOWNLOAD_COMMAND)
 
   if(WIN32)
+    set(TCL_TK_VERSION_DOT "8.5")
+    set(TCL_TK_VERSION "85")
+    set(INCR_TCL_VERSION_DOT "3.4")
+    set(INCR_TCL_VERSION "34")
     if("${CMAKE_SIZEOF_VOID_P}" EQUAL 8)
-      set(TCL_TK_VERSION_DOT "8.5")
-      set(TCL_TK_VERSION "85")
-      set(INCR_TCL_VERSION_DOT "3.4")
-      set(INCR_TCL_VERSION "34")
       set(tcl_URL "http://slicer.kitware.com/midas3/download/item/159433/tcl85-x64-build.zip")
       set(tcl_MD5 "10f0a12590acaaef924dc22e80c9d70a")
     else()
-      set(TCL_TK_VERSION_DOT "8.4")
-      set(TCL_TK_VERSION "84")
-      set(INCR_TCL_VERSION_DOT "3.2")
-      set(INCR_TCL_VERSION "32")
       set(tcl_URL "http://slicer.kitware.com/midas3/download/item/159432/tcl85-build.zip")
       set(tcl_MD5 "07401cf7128a9a79403c8d9b745024a6")
     endif()