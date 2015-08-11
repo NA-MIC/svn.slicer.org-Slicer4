NA-MIC/svn.slicer.org-Slicer4

commit 22af780c693624e7d3f2a92113b2326c1b382fbe

BUG: Fixed extension creation in version-controlled repository
It was only possible to create extension in a non-version-controlled directory.
The problem was that extension was created in repositoryRoot/extensionName directory
but when an extension is opened in a version-controlled directory then the extension
is expected to be found in repositoryRoot.

Changed extension creation so that if it is created in a version-controlled directory
then files are added to repositoryRoot (instead of repositoryRoot/extensionName).

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24452 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/ExtensionWizard/ExtensionWizard.py
@@ -203,10 +203,34 @@ class ExtensionWizardWidget:
     dlg.setTemplates(self.templateManager.templates("extensions"))
 
     while dlg.exec_() == qt.QDialog.Accepted:
+        
+        # If the selected destination is in a repository then use the root of that repository
+        # as destination
       try:
+        repo = SlicerWizard.Utilities.getRepo(dlg.destination)
+
+        createInSubdirectory = True
+        requireEmptyDirectory = True
+        
+        if repo is None:
+          destination = os.path.join(dlg.destination, dlg.componentName)
+          if os.path.exists(dlg.destination):
+            raise IOError("create extension: refusing to overwrite"
+                          " existing directory '%s'" % dlg.destination)
+
+        else:
+          destination = SlicerWizard.Utilities.localRoot(repo)
+          cmakeFile = os.path.join(destination, "CMakeLists.txt")
+          createInSubdirectory = False # create the files in the destination directory
+          requireEmptyDirectory = False # we only check if no CMakeLists.txt file exists
+          if os.path.exists(cmakeFile):
+            raise IOError("create extension: refusing to overwrite"
+                          " directory containing CMakeLists.txt file at '%s'" % dlg.destination)
+
         path = self.templateManager.copyTemplate(
-                 dlg.destination, "extensions",
-                 dlg.componentType, dlg.componentName)
+                 destination, "extensions",
+                 dlg.componentType, dlg.componentName,
+                 createInSubdirectory, requireEmptyDirectory)
 
       except:
         md = qt.QMessageBox(self.parent.window())

Utilities/Scripts/SlicerWizard/TemplateManager.py
@@ -115,7 +115,7 @@ class TemplateManager(object):
       fp.write(contents)
 
   #---------------------------------------------------------------------------
-  def copyTemplate(self, destination, category, kind, name):
+  def copyTemplate(self, destination, category, kind, name, createInSubdirectory = True, requireEmptyDirectory = True):
     """Copy (instantiate) a template.
 
     :param destination: Directory in which to create the template copy.
@@ -126,6 +126,10 @@ class TemplateManager(object):
     :type kind: :class:`basestring`
     :param name: Name for the instantiated template.
     :type name: :class:`basestring`
+    :param createInSubdirectory: If True then files are copied to destination/name/, else destination/.
+    :type name: :class:`bool`
+    :param requireEmptyDirectory: If True then files are only copied if the target directory is empty.
+    :type name: :class:`bool`
 
     :return:
       Path to the new instance (``os.path.join(destination, name)``).
@@ -153,8 +157,10 @@ class TemplateManager(object):
 
     kind = kind.lower()
 
+    if createInSubdirectory:
     destination = os.path.join(destination, name)
-    if os.path.exists(destination):
+
+    if requireEmptyDirectory and os.path.exists(destination):
       raise IOError("create %s: refusing to overwrite"
                     " existing directory '%s'" % (category, destination))
 

commit 04aadc09b14ec3f4041fc317e650ca7189910344

BUG: fix creating new extension
Wrong variable was being checked for directory existence.

Creates new directory in selected target.

This commit fixes a regression introduced in r24452 (BUG: Fixed
extension creation in version-controlled repository )

see - https://github.com/Slicer/Slicer/pull/320

From: Steve Pieper <pieper@bwh.harvard.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24492 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/ExtensionWizard/ExtensionWizard.py
@@ -214,9 +214,10 @@ class ExtensionWizardWidget:
         
         if repo is None:
           destination = os.path.join(dlg.destination, dlg.componentName)
-          if os.path.exists(dlg.destination):
+          if os.path.exists(destination):
             raise IOError("create extension: refusing to overwrite"
-                          " existing directory '%s'" % dlg.destination)
+                          " existing directory '%s'" % destination)
+          createInSubdirectory = False
 
         else:
           destination = SlicerWizard.Utilities.localRoot(repo)