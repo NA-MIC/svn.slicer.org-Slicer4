NA-MIC/svn.slicer.org-Slicer4

commit 748ce7d05f3e86e9c26f607e33839c7b2169fba0

ENH: allow self tests to set a default message display
The default message delay for self.delayDisplay for a scripted
module test is still 1 second, but can be set to a smaller value
to speed up testing.  It's still possible to override the delay
time on a per-call basis, for example to highlight transitions
between test sections by giving a longer timeout.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24514 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Python/slicer/ScriptedLoadableModule.py
@@ -238,9 +238,14 @@ class ScriptedLoadableModuleLogic():
 class ScriptedLoadableModuleTest(unittest.TestCase):
   """
   Base class for module tester class.
+  Setting messageDelay to something small, like 50 allows
+  faster development time.
   """
 
-  def delayDisplay(self,message,msec=1000):
+  def __init__(self, messageDelay=1000):
+    self.messageDelay = messageDelay
+
+  def delayDisplay(self,message,requestedDelay=None):
     """
     Display messages to the user/tester during testing.
     This method can be temporarily overridden to allow tests running
@@ -259,6 +264,9 @@ class ScriptedLoadableModuleTest(unittest.TestCase):
     Error messages should be logged by logging.error() function
     and displayed to user by slicer.util.errorDisplay function.
     """
+    msec = self.messageDelay
+    if requestedDelay:
+      msec = requestedDelay
     slicer.util.delayDisplay(message, msec)
 
   def runTest(self):

commit 587e15f948994123cd78f10d253a0a921cae6701

BUG: Fix constructor of ScriptedLoadableModuleTest
This commit fixes a regression introduced in r24514 (ENH: allow self
tests to set a default message display) where the constructor
of the ScriptedLoadableModuleTest test case was specialized to set
a default value for messageDelay ivar.

Since the original constructor of a unittest.TestCase accept both
regular arguments and keyword arguments, constructor in derived class
have to use the form "*args, **kwargs".

This commit fixes the following tests:

  py_DTINotReproducibleIssue3977
  py_ColorsScalarBarSelfTest

Associated error was of the form:

//----------------
Traceback (most recent call last):
  File "/home/kitware/Dashboards/Nightly/Slicer-0-build/Slicer-build/lib/Slicer-4.4/qt-scripted-modules/ColorsScalarBarSelfTest.py", line 120, in runTest
    self.test_ColorsScalarBarSelfTest1()
  File "/home/kitware/Dashboards/Nightly/Slicer-0-build/Slicer-build/lib/Slicer-4.4/qt-scripted-modules/ColorsScalarBarSelfTest.py", line 124, in test_ColorsScalarBarSelfTest1
    self.delayDisplay("Starting the scalarbar test")
  File "/home/kitware/Dashboards/Nightly/Slicer-0-build/Slicer-build/bin/Python/slicer/ScriptedLoadableModule.py", line 269, in delayDisplay
    slicer.util.delayDisplay(message, msec)
  File "/home/kitware/Dashboards/Nightly/Slicer-0-build/Slicer-build/bin/Python/slicer/util.py", line 637, in delayDisplay
    qt.QTimer.singleShot(autoCloseMsec, messagePopup.close)
  File "/home/kitware/Dashboards/Nightly/Slicer-0-build/Slicer-build/bin/Python/slicer/slicerqt.py", line 74, in _singleShot
    timer.start( msec )
ValueError: Could not find matching overload for given arguments:
('test_ColorsScalarBarSelfTest1',)
 The following slots are available:
start(int msec)
start()
//----------------

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24523 3bd1e089-480b-0410-8dfb-8563597acbee

Base/Python/slicer/ScriptedLoadableModule.py
@@ -240,9 +240,11 @@ class ScriptedLoadableModuleTest(unittest.TestCase):
   faster development time.
   """
 
-  def __init__(self, messageDelay=1000):
-    unittest.TestCase.__init__(self)
-    self.messageDelay = messageDelay
+  def __init__(self, *args, **kwargs):
+    super(ScriptedLoadableModuleTest, self).__init__(*args, **kwargs)
+    self.messageDelay = 1000
+    if 'messageDelay' in kwargs:
+      self.messageDelay = kwargs['messageDelay']
 
   def delayDisplay(self,message,requestedDelay=None):
     """