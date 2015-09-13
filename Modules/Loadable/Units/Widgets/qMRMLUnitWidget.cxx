NA-MIC/svn.slicer.org-Slicer4

commit a80e94d023dac4afa1373059e614b32bb3ea7467

ENH: Improve qMRMLUnitWidget interface
QFlags were added to this widget so one can choose what properties are
visible or not. This is what is done in the qSlicerUnitsSettingsPanel to
hide the advanced properties of the unit to the user. By default, the user
can only modify the precision property.
If he choose to show more properties, a warning stating that only display
values are changed when modifying an unit is shown along with the other
properties.

Style wise, the groupbox title now has its first letter in uppercase to
follow the GUI conventions and a horizontale line in the
qMRMLUnitNodeWidget between the precision and the rest of the properties.

See Issue #3137

From: Johan Andruejohl <johan.andruejohl@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@22190 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Units/Widgets/qMRMLUnitWidget.cxx
@@ -18,24 +18,15 @@
 
 ==============================================================================*/
 
-// CTK includes
-#include <ctkComboBox.h>
-
-// Logic includes
-#include "vtkSlicerUnitsLogic.h"
-
 // Qt includes
 #include <QDebug>
+
 
 // SlicerQt includes
 #include "qMRMLUnitWidget.h"
 #include "ui_qMRMLUnitWidget.h"
-#include "qMRMLWidget.h"
 
 // MRML includes
-#include "vtkMRMLNode.h"
-#include "vtkMRMLScene.h"
-#include "vtkMRMLSelectionNode.h"
 #include "vtkMRMLUnitNode.h"
 
 //-----------------------------------------------------------------------------
@@ -51,8 +42,11 @@ public:
 
   virtual void setupUi(qMRMLUnitWidget*);
   void clear();
+  void updatePropertyWidgets();
 
   vtkMRMLUnitNode* CurrentUnitNode;
+  qMRMLUnitWidget::UnitProperties DisplayFlags;
+  qMRMLUnitWidget::UnitProperties EditableProperties;
 };
 
 //-----------------------------------------------------------------------------
@@ -64,6 +58,9 @@ qMRMLUnitWidgetPrivate::qMRMLUnitWidgetPrivate(
   : q_ptr(&object)
 {
   this->CurrentUnitNode = 0;
+  this->DisplayFlags = qMRMLUnitWidget::All;
+  this->EditableProperties = qMRMLUnitWidget::All;
+  this->EditableProperties &= ~qMRMLUnitWidget::Quantity;
 }
 
 //-----------------------------------------------------------------------------
@@ -71,15 +68,23 @@ void qMRMLUnitWidgetPrivate::setupUi(qMRMLUnitWidget* q)
 {
   this->Ui_qMRMLUnitWidget::setupUi(q);
 
-  QObject::connect(this->SuffixLineEdit, SIGNAL(textChanged(QString)),
-    q, SLOT(setSuffix(QString)));
-  QObject::connect(this->SuffixLineEdit, SIGNAL(textChanged(QString)),
-    q, SIGNAL(suffixChanged(QString)));
+  QObject::connect(this->NameLineEdit, SIGNAL(textChanged(QString)),
+    q, SLOT(setName(QString)));
+  QObject::connect(this->NameLineEdit, SIGNAL(textChanged(QString)),
+    q, SIGNAL(nameChanged(QString)));
+  QObject::connect(this->QuantityLineEdit, SIGNAL(textChanged(QString)),
+    q, SLOT(setQuantity(QString)));
+  QObject::connect(this->QuantityLineEdit, SIGNAL(textChanged(QString)),
+    q, SIGNAL(quantityChanged(QString)));
 
   QObject::connect(this->PrefixLineEdit, SIGNAL(textChanged(QString)),
     q, SLOT(setPrefix(QString)));
   QObject::connect(this->PrefixLineEdit, SIGNAL(textChanged(QString)),
     q, SIGNAL(prefixChanged(QString)));
+  QObject::connect(this->SuffixLineEdit, SIGNAL(textChanged(QString)),
+    q, SLOT(setSuffix(QString)));
+  QObject::connect(this->SuffixLineEdit, SIGNAL(textChanged(QString)),
+    q, SIGNAL(suffixChanged(QString)));
 
   QObject::connect(this->PrecisionSpinBox, SIGNAL(valueChanged(int)),
     q, SLOT(setPrecision(int)));
@@ -90,25 +95,108 @@ void qMRMLUnitWidgetPrivate::setupUi(qMRMLUnitWidget* q)
     q, SLOT(setMinimum(double)));
   QObject::connect(this->MinimumSpinBox, SIGNAL(valueChanged(double)),
     q, SIGNAL(minimumChanged(double)));
-
   QObject::connect(this->MaximumSpinBox, SIGNAL(valueChanged(double)),
     q, SLOT(setMaximum(double)));
   QObject::connect(this->MaximumSpinBox, SIGNAL(valueChanged(double)),
-    q, SIGNAL(maximumChanged(double)));
+    q, SIGNAL(minimumChanged(double)));
 
   QObject::connect(this->PresetNodeComboBox,
     SIGNAL(currentNodeChanged(vtkMRMLNode*)),
     q, SLOT(setUnitFromPreset(vtkMRMLNode*)));
+  this->updatePropertyWidgets();
 }
 
 //-----------------------------------------------------------------------------
 void qMRMLUnitWidgetPrivate::clear()
 {
-  this->PrefixLineEdit->setText("");
-  this->SuffixLineEdit->setText("");
+  this->NameLineEdit->clear();
+  this->QuantityLineEdit->clear();
+  this->PrefixLineEdit->clear();
+  this->SuffixLineEdit->clear();
   this->PrecisionSpinBox->setValue(3);
   this->MinimumSpinBox->setValue(-1000);
   this->MaximumSpinBox->setValue(1000);
+}
+
+//-----------------------------------------------------------------------------
+void qMRMLUnitWidgetPrivate::updatePropertyWidgets()
+{
+  this->PresetNodeComboBox->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Preset));
+  this->PresetNodeComboBox->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Preset));
+  this->PresetLabel->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Preset));
+  this->PresetLabel->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Preset));
+
+  this->SeparationLine->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Preset)
+    && this->DisplayFlags > qMRMLUnitWidget::Preset);
+
+  this->NameLineEdit->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Name));
+  this->NameLineEdit->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Name));
+  this->NameLabel->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Name));
+  this->NameLabel->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Name));
+
+  this->QuantityLineEdit->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Quantity));
+  this->QuantityLineEdit->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Quantity));
+  this->QuantityLabel->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Quantity));
+  this->QuantityLabel->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Quantity));
+
+  this->PrefixLineEdit->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Prefix));
+  this->PrefixLineEdit->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Prefix));
+  this->PrefixLabel->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Prefix));
+  this->PrefixLabel->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Prefix));
+
+  this->SuffixLineEdit->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Suffix));
+  this->SuffixLineEdit->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Suffix));
+  this->SuffixLabel->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Suffix));
+  this->SuffixLabel->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Suffix));
+
+  this->PrecisionSpinBox->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Precision));
+  this->PrecisionSpinBox->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Precision));
+  this->PrecisionLabel->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Precision));
+  this->PrecisionLabel->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Precision));
+
+  this->MinimumSpinBox->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Minimum));
+  this->MinimumSpinBox->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Minimum));
+  this->MinimumValueLabel->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Minimum));
+  this->MinimumValueLabel->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Minimum));
+
+  this->MaximumSpinBox->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Maximum));
+  this->MaximumSpinBox->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Maximum));
+  this->MaximumValueLabel->setVisible(
+    this->DisplayFlags.testFlag(qMRMLUnitWidget::Maximum));
+  this->MaximumValueLabel->setEnabled(
+    this->EditableProperties.testFlag(qMRMLUnitWidget::Maximum));
+
 }
 
 //-----------------------------------------------------------------------------
@@ -129,9 +217,21 @@ qMRMLUnitWidget::~qMRMLUnitWidget()
 }
 
 //-----------------------------------------------------------------------------
+qMRMLUnitWidget::UnitProperties qMRMLUnitWidget::displayedProperties() const
+{
+  Q_D(const qMRMLUnitWidget);
+  return d->DisplayFlags;
+}
+
+//-----------------------------------------------------------------------------
+qMRMLUnitWidget::UnitProperties qMRMLUnitWidget::editableProperties() const
+{
+  Q_D(const qMRMLUnitWidget);
+  return d->EditableProperties;
+}
+//-----------------------------------------------------------------------------
 void qMRMLUnitWidget::setMRMLScene(vtkMRMLScene* scene)
 {
-  Q_D(qMRMLUnitWidget);
   this->Superclass::setMRMLScene(scene);
   this->updateWidgetFromNode();
 }
@@ -161,12 +261,13 @@ void qMRMLUnitWidget::updateWidgetFromNode()
 {
   Q_D(qMRMLUnitWidget);
 
+  d->PresetNodeComboBox->setEnabled(d->CurrentUnitNode != 0);
+  d->NameLineEdit->setEnabled(d->CurrentUnitNode != 0);
   d->PrefixLineEdit->setEnabled(d->CurrentUnitNode != 0);
   d->SuffixLineEdit->setEnabled(d->CurrentUnitNode != 0);
   d->PrecisionSpinBox->setEnabled(d->CurrentUnitNode != 0);
   d->MinimumSpinBox->setEnabled(d->CurrentUnitNode != 0);
   d->MaximumSpinBox->setEnabled(d->CurrentUnitNode != 0);
-  d->PresetNodeComboBox->setEnabled(d->CurrentUnitNode != 0);
 
   if (!d->CurrentUnitNode)
     {
@@ -182,20 +283,49 @@ void qMRMLUnitWidget::updateWidgetFromNode()
   d->PresetNodeComboBox->setCurrentNode(0);
   d->PresetNodeComboBox->blockSignals(modifying);
 
-  // Suffix
+  d->NameLineEdit->setText(d->CurrentUnitNode->GetName());
+  d->QuantityLineEdit->setText(d->CurrentUnitNode->GetQuantity());
   d->SuffixLineEdit->setText(d->CurrentUnitNode->GetSuffix());
-
-  // Prefix
   d->PrefixLineEdit->setText(QString(d->CurrentUnitNode->GetPrefix()));
-
-  // Precision
   d->PrecisionSpinBox->setValue(d->CurrentUnitNode->GetPrecision());
-
-  // Min
   d->MinimumSpinBox->setValue(d->CurrentUnitNode->GetMinimumValue());
-
-  // Max
   d->MaximumSpinBox->setValue(d->CurrentUnitNode->GetMaximumValue());
+}
+
+//-----------------------------------------------------------------------------
+QString qMRMLUnitWidget::name() const
+{
+  Q_D(const qMRMLUnitWidget);
+  return d->NameLineEdit->text();
+}
+
+//-----------------------------------------------------------------------------
+void qMRMLUnitWidget::setName(const QString& newName)
+{
+  Q_D(qMRMLUnitWidget);
+
+  if (d->CurrentUnitNode)
+    {
+    d->CurrentUnitNode->SetName(newName.toLatin1());
+    }
+}
+
+//-----------------------------------------------------------------------------
+QString qMRMLUnitWidget::quantity() const
+{
+  Q_D(const qMRMLUnitWidget);
+  return d->QuantityLineEdit->text();
+}
+
+//-----------------------------------------------------------------------------
+void qMRMLUnitWidget::setQuantity(const QString& newQuantity)
+{
+  Q_D(qMRMLUnitWidget);
+
+  if (d->CurrentUnitNode)
+    {
+    d->CurrentUnitNode->SetQuantity(newQuantity.toLatin1());
+    }
 }
 
 //-----------------------------------------------------------------------------
@@ -206,16 +336,16 @@ QString qMRMLUnitWidget::prefix() const
 }
 
 //-----------------------------------------------------------------------------
-void qMRMLUnitWidget::setPrefix(const QString& prefix)
+void qMRMLUnitWidget::setPrefix(const QString& newPrefix)
 {
   Q_D(qMRMLUnitWidget);
 
   if (d->CurrentUnitNode)
     {
-    d->CurrentUnitNode->SetPrefix(prefix.toLatin1());
+    d->CurrentUnitNode->SetPrefix(newPrefix.toLatin1());
     }
-  d->MaximumSpinBox->setPrefix(prefix);
-  d->MinimumSpinBox->setPrefix(prefix);
+  d->MaximumSpinBox->setPrefix(newPrefix);
+  d->MinimumSpinBox->setPrefix(newPrefix);
 }
 
 //-----------------------------------------------------------------------------
@@ -226,16 +356,16 @@ QString qMRMLUnitWidget::suffix() const
 }
 
 //-----------------------------------------------------------------------------
-void qMRMLUnitWidget::setSuffix(const QString& suffix)
+void qMRMLUnitWidget::setSuffix(const QString& newSuffix)
 {
   Q_D(qMRMLUnitWidget);
 
   if (d->CurrentUnitNode)
     {
-    d->CurrentUnitNode->SetSuffix(suffix.toLatin1());
+    d->CurrentUnitNode->SetSuffix(newSuffix.toLatin1());
     }
-  d->MaximumSpinBox->setSuffix(suffix);
-  d->MinimumSpinBox->setSuffix(suffix);
+  d->MaximumSpinBox->setSuffix(newSuffix);
+  d->MinimumSpinBox->setSuffix(newSuffix);
 }
 
 //-----------------------------------------------------------------------------
@@ -318,3 +448,34 @@ void qMRMLUnitWidget::setUnitFromPreset(vtkMRMLNode* presetNode)
   d->CurrentUnitNode->SetMaximumValue(presetUnitNode->GetMaximumValue());
   d->CurrentUnitNode->EndModify(disabledModify);
 }
+
+//-----------------------------------------------------------------------------
+void qMRMLUnitWidget
+::setDisplayedProperties(qMRMLUnitWidget::UnitProperties flag)
+{
+  Q_D(qMRMLUnitWidget);
+
+  if (d->DisplayFlags == flag)
+    {
+    return;
+    }
+
+  d->DisplayFlags = flag;
+  d->updatePropertyWidgets();
+}
+
+//-----------------------------------------------------------------------------
+void qMRMLUnitWidget
+::setEditableProperties(qMRMLUnitWidget::UnitProperties properties)
+{
+  Q_D(qMRMLUnitWidget);
+
+  if (d->EditableProperties == properties)
+    {
+    return;
+    }
+
+  d->EditableProperties = properties;
+  d->updatePropertyWidgets();
+}
+

Modules/Loadable/Units/Logic/vtkSlicerUnitsLogic.cxx
@@ -135,30 +135,30 @@ void vtkSlicerUnitsLogic::AddBuiltInUnits(vtkMRMLScene* scene)
 
   // Add defaults nodes here
   this->AddUnitNodeToScene(scene,
-    "metre", "length", "", "m", 3);
+    "Meter", "length", "", "m", 3);
   this->AddUnitNodeToScene(scene,
-    "centimetre", "length", "", "cm", 3);
+    "Centimeter", "length", "", "cm", 3);
   this->AddUnitNodeToScene(scene,
-    "millimetre", "length", "", "mm", 3);
+    "Millimeter", "length", "", "mm", 3);
   this->AddUnitNodeToScene(scene,
-    "micrometre", "length", "", "µm", 3);
+    "Micrometer", "length", "", "µm", 3);
   this->AddUnitNodeToScene(scene,
-    "nanometre", "length", "", "nm", 3);
+    "Nanometer", "length", "", "nm", 3);
 
   this->AddUnitNodeToScene(scene,
-    "year", "time", "", "year", 3);
+    "Year", "time", "", "year", 3);
   this->AddUnitNodeToScene(scene,
-    "month", "time", "", "month", 3);
+    "Month", "time", "", "month", 3);
   this->AddUnitNodeToScene(scene,
-    "day", "time", "", "day", 3);
+    "Day", "time", "", "day", 3);
   this->AddUnitNodeToScene(scene,
-    "hour", "time", "", "h", 3);
+    "Hour", "time", "", "h", 3);
   this->AddUnitNodeToScene(scene,
-    "second", "time", "", "s", 3);
+    "Second", "time", "", "s", 3);
   this->AddUnitNodeToScene(scene,
-    "millisecond", "time", "", "ms", 3);
+    "Millisecond", "time", "", "ms", 3);
   this->AddUnitNodeToScene(scene,
-    "microsecond", "time", "", "µs", 3);
+    "Microsecond", "time", "", "µs", 3);
 }
 
 //-----------------------------------------------------------------------------

Modules/Loadable/Units/Resources/UI/qSlicerUnitsSettingsPanel.ui
@@ -6,17 +6,67 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>345</width>
-    <height>107</height>
+    <width>166</width>
+    <height>77</height>
    </rect>
   </property>
   <property name="windowTitle">
    <string>Units Settings</string>
   </property>
-  <layout class="QFormLayout" name="PanelLayout">
-   <property name="fieldGrowthPolicy">
-    <enum>QFormLayout::AllNonFixedFieldsGrow</enum>
-   </property>
+  <layout class="QGridLayout" name="GridLayout">
+   <item row="0" column="0">
+    <widget class="QLabel" name="WarningLabel">
+     <property name="sizePolicy">
+      <sizepolicy hsizetype="Preferred" vsizetype="Preferred">
+       <horstretch>0</horstretch>
+       <verstretch>0</verstretch>
+      </sizepolicy>
+     </property>
+     <property name="text">
+      <string>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600; color:#ff0000;&quot;&gt;Warning:&lt;/span&gt; Changing the properties of the unit only change the display, not the value !&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+     </property>
+    </widget>
+   </item>
+   <item row="0" column="1">
+    <widget class="QCheckBox" name="ShowAllCheckBox">
+     <property name="sizePolicy">
+      <sizepolicy hsizetype="Fixed" vsizetype="Fixed">
+       <horstretch>0</horstretch>
+       <verstretch>0</verstretch>
+      </sizepolicy>
+     </property>
+     <property name="toolTip">
+      <string>Show all the units' properties.
+This should only used by advanced users who understand the consequences of changing a unit's property.</string>
+     </property>
+     <property name="text">
+      <string>Show advanced options</string>
+     </property>
+     <property name="checked">
+      <bool>true</bool>
+     </property>
+    </widget>
+   </item>
+   <item row="2" column="0" colspan="2">
+    <spacer name="verticalSpacer">
+     <property name="orientation">
+      <enum>Qt::Vertical</enum>
+     </property>
+     <property name="sizeHint" stdset="0">
+      <size>
+       <width>20</width>
+       <height>40</height>
+      </size>
+     </property>
+    </spacer>
+   </item>
+   <item row="1" column="0" colspan="2">
+    <layout class="QVBoxLayout" name="QuantitiesLayout"/>
+   </item>
   </layout>
  </widget>
  <customwidgets>
@@ -28,5 +78,22 @@
   </customwidget>
  </customwidgets>
  <resources/>
- <connections/>
+ <connections>
+  <connection>
+   <sender>ShowAllCheckBox</sender>
+   <signal>toggled(bool)</signal>
+   <receiver>WarningLabel</receiver>
+   <slot>setVisible(bool)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>139</x>
+     <y>16</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>49</x>
+     <y>19</y>
+    </hint>
+   </hints>
+  </connection>
+ </connections>
 </ui>

commit 2f3acb1ccc7f4a046d43536bdb630b0edd6889a1

BUG: MRMLUnitWidget: Connect correct signal
This commit fixes an issue introduced in r22190 (ENH: Improve
qMRMLUnitWidget interface).

From: Davide Punzo <punzodavide@hotmail.it>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24535 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Units/Widgets/qMRMLUnitWidget.cxx
@@ -98,7 +98,7 @@ void qMRMLUnitWidgetPrivate::setupUi(qMRMLUnitWidget* q)
   QObject::connect(this->MaximumSpinBox, SIGNAL(valueChanged(double)),
     q, SLOT(setMaximum(double)));
   QObject::connect(this->MaximumSpinBox, SIGNAL(valueChanged(double)),
-    q, SIGNAL(minimumChanged(double)));
+    q, SIGNAL(maximumChanged(double)));
 
   QObject::connect(this->CoefficientSpinBox, SIGNAL(valueChanged(double)),
     q, SLOT(setCoefficient(double)));