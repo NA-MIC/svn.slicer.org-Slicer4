NA-MIC/svn.slicer.org-Slicer4

commit 0135c28d077ffd59fb7242cdf24979fb576c4e06

ENH: Backwards compatible FixedParameters
This patch provides alternatives for both
defining the m_FixedParameters of a transfor
as either the old-style ParametersType or
preferably the new-style FixedParametersType
when it is available in ITK.

Prepare for post ITK4.8 TransformFix

This patch makes Slicer prepared for future changes to
ITK that can not be backwards compatible.

http://review.source.kitware.com/#/c/19921/

From: Hans Johnson <hans-johnson@uiowa.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24338 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkITKTransformConverter.h
@@ -622,7 +622,11 @@ template <typename BSplineTransformType> bool vtkITKTransformConverter::SetITKBS
   // * mesh origin X, Y, Z (position of the boundary node before the grid)
   // * mesh spacing X, Y, Z
   // * mesh direction 3x3 matrix (first row, second row, third row)
+#if defined(ITK_FIXED_PARAMETERS_ARE_DOUBLE) // After 4.8.1
+  typename BSplineTransformType::FixedParametersValueType transformFixedParamsItk;
+#else                                         //Pre 4.8.1
   typename BSplineTransformType::ParametersType transformFixedParamsItk;
+#endif
   const unsigned int numberOfFixedParameters=VTKDimension*(VTKDimension+3);
   transformFixedParamsItk.SetSize(numberOfFixedParameters);
 

commit 35dace46819461414a366a92883eb064f0d9a635

BUG: Fix vtkITKTransformConverter to support future ITK >= 4.8.1
Anticipating transition to newer version of ITK, this commit fixes
a build error (most likely copy-n-paste error) originally introduced in
r24338 (ENH: Backwards compatible FixedParameters) by changing
"FixedParametersValueType" into "FixedParametersType" to consider ITK
change InsightSoftwareConsortium/ITK@3592834 (BUG: FixedParameters
should be double precision)

Tested-by: Hans Johnson <hans-johnson@uiowa.edu>

From: Hans Johnson <hans-johnson@uiowa.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24605 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkITKTransformConverter.h
@@ -623,7 +623,7 @@ template <typename BSplineTransformType> bool vtkITKTransformConverter::SetITKBS
   // * mesh spacing X, Y, Z
   // * mesh direction 3x3 matrix (first row, second row, third row)
 #if defined(ITK_FIXED_PARAMETERS_ARE_DOUBLE) // After 4.8.1
-  typename BSplineTransformType::FixedParametersValueType transformFixedParamsItk;
+  typename BSplineTransformType::FixedParametersType transformFixedParamsItk;
 #else                                         //Pre 4.8.1
   typename BSplineTransformType::ParametersType transformFixedParamsItk;
 #endif