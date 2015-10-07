NA-MIC/svn.slicer.org-Slicer4

commit a4fb3ca430ed886d8dd4e2c9c33ae93111eb810b

BUG: remove node SceneRootDir, use RootDirectory on MRML scene
The node ivar scene root directory wasn't staying updated when the root directory was changing on the scene. Updated the code to refer to the scene for the root directory.
One important change is that in the XML parser, the scene has to be set on a storage node before reading/writing it so that the absolute path can be determined.
 Also set the scene root dir more consistently
Updated the fiber bundle node test to use the testing macros since it was reproducing code.

Issue #3956

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24393 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkMRMLParser.cxx
@@ -103,12 +103,10 @@ void vtkMRMLParser::StartElement(const char* tagName, const char** atts)
     return;
     }
 
-  // It is needed to have the scene root dir set before ReadXMLAttributes is
+  // It is needed to have the scene set before ReadXMLAttributes is
   // called on storage nodes.
-  if (this->GetMRMLScene())
-    {
-    node->SetSceneRootDir(this->GetMRMLScene()->GetRootDirectory());
-    }
+  node->SetScene(this->GetMRMLScene());
+
   node->ReadXMLAttributes(atts);
 
   // Slicer3 snap shot nodes were hidden by default, show them so that

Libs/MRML/Core/Testing/vtkMRMLFiberBundleNodeTest1.cxx
@@ -19,7 +19,7 @@ int vtkMRMLFiberBundleNodeTest1(int , char * [] )
 {
   vtkNew< vtkMRMLFiberBundleNode> node1;
 
-  EXERCISE_BASIC_OBJECT_METHODS(node1.GetPointer());
+  EXERCISE_BASIC_DISPLAYABLE_MRML_METHOD(vtkMRMLFiberBundleNode, node1.GetPointer());
 
   node1->UpdateReferences();
 
@@ -47,19 +47,6 @@ int vtkMRMLFiberBundleNodeTest1(int , char * [] )
     std::cerr << "Error in Set/GetAttribute() " << std::endl;
     return EXIT_FAILURE;
     }
-
-  TEST_SET_GET_BOOLEAN( node1, HideFromEditors );
-  TEST_SET_GET_BOOLEAN( node1, Selectable );
-
-  TEST_SET_GET_STRING( node1, Description );
-  TEST_SET_GET_STRING( node1, SceneRootDir );
-  TEST_SET_GET_STRING( node1, Name );
-  TEST_SET_GET_STRING( node1, SingletonTag );
-
-  TEST_SET_GET_BOOLEAN( node1, ModifiedSinceRead );
-  TEST_SET_GET_BOOLEAN( node1, SaveWithScene );
-  TEST_SET_GET_BOOLEAN( node1, AddToScene );
-  TEST_SET_GET_BOOLEAN( node1, Selected );
 
   node1->Modified();
 

Libs/MRML/Core/vtkMRMLCoreTestingMacros.h
@@ -428,7 +428,6 @@
     TEST_SET_GET_BOOLEAN( node, HideFromEditors );                      \
     TEST_SET_GET_BOOLEAN( node, Selectable );                           \
     TEST_SET_GET_STRING( node, Description );                           \
-    TEST_SET_GET_STRING( node, SceneRootDir );                          \
     TEST_SET_GET_STRING( node, Name );                                  \
     TEST_SET_GET_STRING( node, SingletonTag );                          \
     TEST_SET_GET_BOOLEAN( node, SaveWithScene );                        \

Libs/MRML/Core/vtkMRMLNode.cxx
@@ -47,7 +47,6 @@ vtkMRMLNode::vtkMRMLNode()
 
   this->SingletonTag = NULL;
 
-  this->SceneRootDir = NULL;
   this->Scene = NULL;
 
   this->HideFromEditors = 0;
@@ -103,11 +102,6 @@ vtkMRMLNode::~vtkMRMLNode()
     {
     delete [] this->ID;
     this->ID = NULL;
-    }
-  if (this->SceneRootDir)
-    {
-    delete [] this->SceneRootDir;
-    this->SceneRootDir = NULL;
     }
   if (this->MRMLObserverManager)
     {
@@ -573,7 +567,6 @@ void vtkMRMLNode::SetScene(vtkMRMLScene* scene)
   if (this->Scene)
     {
     this->SetSceneReferences();
-    this->SetSceneRootDir(scene->GetRootDirectory());
     // We must not call UpdateNodeReferences() here yet, because referenced node IDs may change
     // due to conflict with node IDs existing in the scene.
     }

Libs/MRML/Core/vtkMRMLNode.h
@@ -316,11 +316,7 @@ public:
   vtkSetStringMacro(Description);
   vtkGetStringMacro(Description);
 
-  /// Root directory of MRML scene.
-  vtkSetStringMacro(SceneRootDir);
-  vtkGetStringMacro(SceneRootDir);
-
-  /// Name of this node, to be set by the user.
+  /// Name of this node, to be set by the user
   vtkSetStringMacro(Name);
   vtkGetStringMacro(Name);
 
@@ -820,7 +816,6 @@ protected:
   int InMRMLCallbackFlag;
 
   char *Description;
-  char *SceneRootDir;
   char *Name;
   char *ID;
   int Indent;

Libs/MRML/Core/vtkMRMLScene.cxx
@@ -877,6 +877,9 @@ int vtkMRMLScene::Commit(const char* url)
   else
     {
     os = &ofs;
+    // set the root directory from the URL
+    this->RootDirectory = vtksys::SystemTools::GetParentDirectory(url);
+
     // Open file
 #ifdef _WIN32
     ofs.open(url, std::ios::out | std::ios::binary);
@@ -1017,7 +1020,6 @@ vtkMRMLNode*  vtkMRMLScene::AddNodeNoNotify(vtkMRMLNode *n)
   int wasModifying = n->StartModify();
 
   //TODO convert URL to Root directory
-  //n->SetSceneRootDir("");
 
   // check if node is a singletone
   if (n->GetSingletonTag() != NULL)
@@ -1820,7 +1822,6 @@ vtkMRMLNode* vtkMRMLScene::InsertAfterNode(vtkMRMLNode *item, vtkMRMLNode *n)
   int modifyStatus = n->GetDisableModifiedEvent();
   n->SetDisableModifiedEvent(1);
 
-  n->SetSceneRootDir(this->RootDirectory.c_str());
   if (n->GetName() == NULL|| n->GetName()[0] == '\0')
     {
     n->SetName(n->GetID());
@@ -1904,7 +1905,6 @@ vtkMRMLNode* vtkMRMLScene::InsertBeforeNode(vtkMRMLNode *item, vtkMRMLNode *n)
   int modifyStatus = n->GetDisableModifiedEvent();
   n->SetDisableModifiedEvent(1);
 
-  n->SetSceneRootDir(this->RootDirectory.c_str());
   if (n->GetName() == NULL|| n->GetName()[0] == '\0')
     {
     n->SetName(n->GetID());

Libs/MRML/Core/vtkMRMLStorageNode.cxx
@@ -209,9 +209,11 @@ void vtkMRMLStorageNode::ReadXMLAttributes(const char** atts)
 
       // convert to absolute filename
       std::string name;
-      if (this->GetSceneRootDir() && this->IsFilePathRelative(filename.c_str()))
+      if (this->GetScene() &&
+          this->GetScene()->GetRootDirectory() &&
+          this->IsFilePathRelative(filename.c_str()))
         {
-        name = this->GetSceneRootDir();
+        name = this->GetScene()->GetRootDirectory();
         if (name[name.size()-1] != '/')
           {
           name = name + std::string("/");
@@ -232,9 +234,11 @@ void vtkMRMLStorageNode::ReadXMLAttributes(const char** atts)
 
       // convert to absolute filename
       std::string name;
-      if (this->GetSceneRootDir() && this->IsFilePathRelative(filename.c_str()))
+      if (this->GetScene() &&
+          this->GetScene()->GetRootDirectory() &&
+          this->IsFilePathRelative(filename.c_str()))
         {
-        name = this->GetSceneRootDir();
+        name = this->GetScene()->GetRootDirectory();
         if (name[name.size()-1] != '/')
           {
           name = name + std::string("/");
@@ -976,13 +980,14 @@ const char *vtkMRMLStorageNode::GetAbsoluteFilePath(const char *inputPath)
     // the path is already absolute, return it
     return inputPath;
     }
-  if (!this->GetSceneRootDir())
+  if (!this->GetScene() ||
+      !this->GetScene()->GetRootDirectory())
     {
-    vtkErrorMacro("GetAbsoluteFilePath: have a relative path " << inputPath << " but no scene to find it from!");
+    vtkErrorMacro("GetAbsoluteFilePath: have a relative path " << inputPath << " but no scene or root directory to find it from!");
     return NULL;
     }
 
-  std::string path = this->GetSceneRootDir();
+  std::string path = this->GetScene()->GetRootDirectory();
   if (path.size() > 0 &&
       path[path.size()-1] != '/')
     {

Modules/Loadable/Data/qSlicerSceneWriter.cxx
@@ -45,6 +45,7 @@
 #include <vtkImageData.h>
 #include <vtkNew.h>
 #include <vtkSmartPointer.h>
+#include <vtksys/SystemTools.hxx>
 
 //----------------------------------------------------------------------------
 qSlicerSceneWriter::qSlicerSceneWriter(QObject* parentObject)
@@ -111,6 +112,14 @@ bool qSlicerSceneWriter::write(const qSlicerIO::IOProperties& properties)
 //----------------------------------------------------------------------------
 bool qSlicerSceneWriter::writeToMRML(const qSlicerIO::IOProperties& properties)
 {
+  // set the mrml scene url first
+  Q_ASSERT(!properties["fileName"].toString().isEmpty());
+  QString fileName = properties["fileName"].toString();
+
+  this->mrmlScene()->SetURL(fileName.toLatin1());
+  std::string parentDir = vtksys::SystemTools::GetParentDirectory(this->mrmlScene()->GetURL());
+  this->mrmlScene()->SetRootDirectory(parentDir.c_str());
+
   // save an explicit default scene view recording the state of the scene when
   // saved to file
   const char *defaultSceneName = "Master Scene View";
@@ -161,11 +170,9 @@ bool qSlicerSceneWriter::writeToMRML(const qSlicerIO::IOProperties& properties)
   // force a write
   sceneViewNode->GetStorageNode()->WriteData(sceneViewNode);
 
-  Q_ASSERT(!properties["fileName"].toString().isEmpty());
-  QString fileName = properties["fileName"].toString();
-
-  this->mrmlScene()->SetURL(fileName.toLatin1());
+  // write out the mrml file
   bool res = this->mrmlScene()->Commit();
+
   return res;
 }
 

commit 2e5fbff23f5a47da407cb29049e74b2700f2f10c

BUG: Do not reset reference when importing scene. Fixes #4040
This commit fixes a regression introduced in r24393 [1] where a node and its
referenced nodes would be modified if node with the same ID were imported.

The issue is due to the MRML scene being set to the node just before the
function "ReadXMLAttributes()" is called in "vtkMRMLParser::StartElement".

Indeed, since within "ReadXMLAttributes()", the method "AddNodeReferenceID()"
is called, when the ID of a node being imported conflicts with an existing
one in the scene, it causes the existing node to be updated. For example,
this problem is observed in test where a conflicting "vtkMRMLDisplayableNode"
is imported.

Other approach could be to ensure no events are invoked in the method
"vtkMRMLDisplayableNode::OnNodeReferenceAdded" when scene is being
imported.

[1] r24393 - BUG: remove node SceneRootDir, use RootDirectory on MRML scene

Co-authored-by: Nicole Aucoin <nicole@bwh.harvard.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24608 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkMRMLParser.cxx
@@ -15,6 +15,7 @@ Version:   $Revision: 1.8 $
 // MRML includes
 #include "vtkMRMLParser.h"
 #include "vtkMRMLScene.h"
+#include "vtkMRMLStorageNode.h"
 #include "vtkMRMLNode.h"
 #include "vtkTagTable.h"
 
@@ -105,7 +106,10 @@ void vtkMRMLParser::StartElement(const char* tagName, const char** atts)
 
   // It is needed to have the scene set before ReadXMLAttributes is
   // called on storage nodes.
-  node->SetScene(this->GetMRMLScene());
+  if (vtkMRMLStorageNode::SafeDownCast(node) != 0)
+    {
+    node->SetScene(this->GetMRMLScene());
+    }
 
   node->ReadXMLAttributes(atts);
 

Libs/MRML/Core/Testing/vtkMRMLSceneImportIDConflictTest.cxx
@@ -23,6 +23,9 @@
 #include "vtkMRMLModelNode.h"
 #include "vtkMRMLScene.h"
 
+// VTK includes
+#include <vtkPolyData.h>
+
 // STD includes
 #include <vtkNew.h>
 
@@ -34,6 +37,11 @@ int vtkMRMLSceneImportIDConflictTest(int vtkNotUsed(argc), char * vtkNotUsed(arg
   // Add displayable node
   vtkNew<vtkMRMLModelNode> modelNode;
   scene->AddNode(modelNode.GetPointer());
+
+  // add poly data
+  vtkNew<vtkPolyData> polyData;
+  modelNode->SetAndObservePolyData(polyData.GetPointer());
+  std::cout << "Polydata pointer = " << polyData.GetPointer() << std::endl;
 
   // Add display node
   vtkNew<vtkMRMLModelDisplayNode> modelDisplayNode;
@@ -97,6 +105,49 @@ int vtkMRMLSceneImportIDConflictTest(int vtkNotUsed(argc), char * vtkNotUsed(arg
     {
     std::cerr << "Failed to import scene - part2: "
               << "model #3: " << modelNode3 << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // check that the model nodes and model display nodes point to the right poly data
+  if (modelNode2->GetPolyData() != NULL)
+    {
+    std::cerr << "Line " << __LINE__
+              << " - Import failed: new model node should have null polydata: "
+              << modelNode2->GetPolyData()
+              << std::endl;
+    return EXIT_FAILURE;
+    }
+  if (vtkMRMLModelDisplayNode::SafeDownCast(modelNode2->GetDisplayNode())
+      ->GetInputPolyData() != NULL)
+    {
+    std::cerr << "Line " << __LINE__
+              << " - Import failed: new model node's display node should have null polydata: "
+              << vtkMRMLModelDisplayNode::SafeDownCast(modelNode2->GetDisplayNode())
+                   ->GetInputPolyData()
+              << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  if (modelNode->GetPolyData() == NULL)
+    {
+    std::cerr << "Line " << __LINE__
+              << " - Import failed: original model node should not have null polydata"
+              << std::endl;
+    return EXIT_FAILURE;
+    }
+  if (vtkMRMLModelDisplayNode::SafeDownCast(modelNode->GetDisplayNode())->GetInputPolyData() == NULL)
+    {
+    std::cerr << "Line " << __LINE__
+              << " - Import failed: original model display node should not have null polydata"
+              << std::endl;
+    return EXIT_FAILURE;
+    }
+  if (modelNode->GetPolyData() != vtkMRMLModelDisplayNode::SafeDownCast(
+        modelNode->GetDisplayNode())->GetInputPolyData())
+    {
+    std::cerr << "Line " << __LINE__
+              << " - Import failed: original model node and display node don't have the same poly data"
+              << std::endl;
     return EXIT_FAILURE;
     }
 

Libs/MRML/Core/Testing/vtkMRMLSceneImportIDModelHierarchyConflictTest.cxx
@@ -213,6 +213,7 @@ bool ImportIDModelHierarchyConflictTest()
               << " - Import failed: new model node should have null polydata: "
               << modelNode2->GetPolyData()
               << std::endl;
+    return false;
     }
   if (vtkMRMLModelDisplayNode::SafeDownCast(modelNode2->GetDisplayNode())
       ->GetInputPolyData() != NULL)