NA-MIC/svn.slicer.org-Slicer4

commit 8eabd72feeecadae833a5a7b16e4ad3df937f200

BUG: Fix SliceViewAnnotations AttributeError happening on closing Slicer
This commit fixes the following error introducing a weak pointer to keep
track of the layout manager.

8<----8<----8<----8<----8<----8<----8<----8<----8<----8<----8<----8<----
File "/home/jcfr/Projects/Slicer-Debug/Slicer-build/lib/Slicer-4.4/qt-scripted-modules/DataProbeLib/SliceViewAnnotations.py", line 628, in updateCornerAnnotations
  sliceViewNames = slicer.app.layoutManager().sliceViewNames()
AttributeError: 'NoneType' object has no attribute 'sliceViewNames'
8<----8<----8<----8<----8<----8<----8<----8<----8<----8<----8<----8<----

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24238 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DataProbe/DataProbeLib/SliceViewAnnotations.py
@@ -66,7 +66,6 @@ class SliceAnnotations(VTKObservationMixin):
       '7-TE':{'text':'','category':'A'}
       })
 
-    self.layoutManager = slicer.app.layoutManager()
     self.sliceCornerAnnotations = {}
 
     self.annotationsDisplayAmount = 0
@@ -431,6 +430,8 @@ class SliceAnnotations(VTKObservationMixin):
     elif self.level3RadioButton.checked:
       self.annotationsDisplayAmount = 2
 
+    layoutManager = slicer.app.layoutManager()
+
     if self.showSliceViewAnnotations:
       self.cornerTextParametersCollapsibleButton.enabled = True
       self.activateCornersGroupBox.enabled = True
@@ -441,7 +442,7 @@ class SliceAnnotations(VTKObservationMixin):
       self.colorScalarBarCollapsibleButton.enabled = True
 
       for sliceViewName in self.sliceViewNames:
-        sliceWidget = self.layoutManager.sliceWidget(sliceViewName)
+        sliceWidget = layoutManager.sliceWidget(sliceViewName)
         sl = sliceWidget.sliceLogic()
         self.makeAnnotationText(sl)
         self.makeScalingRuler(sl)
@@ -456,7 +457,7 @@ class SliceAnnotations(VTKObservationMixin):
       self.colorScalarBarCollapsibleButton.enabled = False
       # Remove Observers
       for sliceViewName in self.sliceViewNames:
-        sliceWidget = self.layoutManager.sliceWidget(sliceViewName)
+        sliceWidget = layoutManager.sliceWidget(sliceViewName)
         sl = sliceWidget.sliceLogic()
         self.makeScalingRuler(sl)
         self.makeColorScalarBar(sl)
@@ -487,7 +488,7 @@ class SliceAnnotations(VTKObservationMixin):
 
   def createCornerAnnotations(self):
     self.createGlobalVariables()
-    sliceViewNames = self.layoutManager.sliceViewNames()
+    sliceViewNames = slicer.app.layoutManager().sliceViewNames()
 
     for sliceViewName in sliceViewNames:
       self.sliceViewNames.append(sliceViewName)
@@ -496,7 +497,7 @@ class SliceAnnotations(VTKObservationMixin):
       self.createActors(sliceViewName)
 
   def addSliceViewObserver(self, sliceViewName):
-    sliceWidget = self.layoutManager.sliceWidget(sliceViewName)
+    sliceWidget = slicer.app.layoutManager().sliceWidget(sliceViewName)
     self.sliceWidgets[sliceViewName] = sliceWidget
     sliceView = sliceWidget.sliceView()
 
@@ -512,7 +513,7 @@ class SliceAnnotations(VTKObservationMixin):
 
 
   def createActors(self, sliceViewName):
-    sliceWidget = self.layoutManager.sliceWidget(sliceViewName)
+    sliceWidget = slicer.app.layoutManager().sliceWidget(sliceViewName)
     self.sliceWidgets[sliceViewName] = sliceWidget
     sliceView = sliceWidget.sliceView()
 
@@ -594,7 +595,10 @@ class SliceAnnotations(VTKObservationMixin):
     return scalarBar
 
   def updateCornerAnnotations(self,caller,event):
-    sliceViewNames = self.layoutManager.sliceViewNames()
+    layoutManager = slicer.app.layoutManager()
+    if layoutManager is None:
+      return
+    sliceViewNames = layoutManager.sliceViewNames()
     for sliceViewName in sliceViewNames:
       if sliceViewName not in self.sliceViewNames:
         self.sliceViewNames.append(sliceViewName)

Base/QTGUI/qSlicerApplication.cxx
@@ -117,7 +117,7 @@ public:
   /// ErrorLogModel - It should exist only one instance of the ErrorLogModel
   QSharedPointer<ctkErrorLogModel>            ErrorLogModel;
 
-  qSlicerLayoutManager*   LayoutManager;
+  QWeakPointer<qSlicerLayoutManager> LayoutManager;
   ctkToolTipTrapper*      ToolTipTrapper;
   ctkSettingsDialog*      SettingsDialog;
 #ifdef Slicer_BUILD_EXTENSIONMANAGER_SUPPORT
@@ -139,7 +139,6 @@ qSlicerApplicationPrivate::qSlicerApplicationPrivate(
     qSlicerIOManager * ioManager)
   : qSlicerCoreApplicationPrivate(object, commandOptions, ioManager), q_ptr(&object)
 {
-  this->LayoutManager = 0;
   this->ToolTipTrapper = 0;
   this->SettingsDialog = 0;
 #ifdef Slicer_USE_QtTesting
@@ -410,10 +409,10 @@ void qSlicerApplication::setLayoutManager(qSlicerLayoutManager* layoutManager)
   if (this->applicationLogic())
     {
     this->applicationLogic()->SetSliceLogics(
-      d->LayoutManager? d->LayoutManager->mrmlSliceLogics() : 0);
+      d->LayoutManager? d->LayoutManager.data()->mrmlSliceLogics() : 0);
     if (d->LayoutManager)
       {
-      d->LayoutManager->setMRMLColorLogic(this->applicationLogic()->GetColorLogic());
+      d->LayoutManager.data()->setMRMLColorLogic(this->applicationLogic()->GetColorLogic());
       }
     }
 }
@@ -422,7 +421,7 @@ void qSlicerApplication::setLayoutManager(qSlicerLayoutManager* layoutManager)
 qSlicerLayoutManager* qSlicerApplication::layoutManager()const
 {
   Q_D(const qSlicerApplication);
-  return d->LayoutManager;
+  return d->LayoutManager.data();
 }
 
 //-----------------------------------------------------------------------------

commit 25b379c0cfe67e28ac23303a5a40ae1a01bbaff7

BUG: Fix AttributeError in SliceViewAnnotations.updateViewAnnotations()
This commit fixes a regression introduced in r24238 (BUG: Fix
SliceViewAnnotations AttributeError happening on closing Slicer) where
the ivar sliceViewNames (of type 'list') is directly set with the
value returned by app.layoutManager().sliceViewNames() (of type 'tuple').

Considering that tuple are immutable and list mutable, this explained the
error in the function "updateViewAnnotations":

  AttributeError: 'tuple' object has no attribute 'append'

By converting the returned tuple to a list, it fixes the issue.

Reviewed-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

From: Christian Herz <christian.herz@mevis.fraunhofer.de>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@24684 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Scripted/DataProbe/DataProbeLib/SliceViewAnnotations.py
@@ -471,7 +471,7 @@ class SliceAnnotations(VTKObservationMixin):
 
   def createCornerAnnotations(self):
     self.createGlobalVariables()
-    self.sliceViewNames = self.layoutManager.sliceViewNames()
+    self.sliceViewNames = list(self.layoutManager.sliceViewNames())
     for sliceViewName in self.sliceViewNames:
       self.addSliceViewObserver(sliceViewName)
       self.createActors(sliceViewName)