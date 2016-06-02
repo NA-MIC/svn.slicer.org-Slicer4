NA-MIC/svn.slicer.org-Slicer4

commit b283dd9f24980ef12b7e0ea0a488cba91bc9719b

COMP: qMRMLSceneModel: Fix -Woverloaded-virtual and add comment.
This commit follows up previous commit (PERF: qMRMLSceneModel: track and
reuse index during populateScene).

By introducing the previous commit, loading time of "NAC Brain Atlas 2015"
incluyding ~1600 nodes was reduced by ~8%. Experiment consisted in
[Start Slicer, Load the atlas] three times. Loading time was the one
reported in the log.

It fixes the "-Woverloaded-virtual" warnings by moving the method
"insertNode(vtkMRMLNode*, int)" to the pimpl.

//----------------
In file included from /path/to/Slicer-Release/Slicer-build/Modules/Remote/OpenIGTLinkIF/GUI/moc_qMRMLIGTLIOModel.cxx:9:0:
/path/to/Slicer-Release/Slicer-build/Modules/Remote/OpenIGTLinkIF/GUI/../../../../../OpenIGTLinkIF/GUI/qMRMLIGTLIOModel.h:72:26: warning:   by ‘qMRMLIGTLIOModel::insertNode’ [-Woverloaded-virtual]
   virtual QStandardItem* insertNode(vtkMRMLNode* node);
                          ^

                 from /path/to/Slicer-Release/OpenIGTLinkIF/GUI/qMRMLIGTLIOModel.cxx:26:
/path/to/Slicer/Libs/MRML/Widgets/qMRMLSceneModel.h:272:26: warning: ‘virtual QStandardItem* qMRMLSceneModel::insertNode(vtkMRMLNode*, int)’ was hidden [-Woverloaded-virtual]
   virtual QStandardItem* insertNode(vtkMRMLNode* node, int index);
                          ^
In file included from /path/to/Slicer-Release/OpenIGTLinkIF/GUI/qMRMLIGTLIOModel.cxx:26:0:
/path/to/Slicer-Release/OpenIGTLinkIF/GUI/qMRMLIGTLIOModel.h:72:26: warning:   by ‘qMRMLIGTLIOModel::insertNode’ [-Woverloaded-virtual]
   virtual QStandardItem* insertNode(vtkMRMLNode* node);
//----------------

From: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25049 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Widgets/qMRMLSceneModel.cxx
@@ -778,7 +778,8 @@ void qMRMLSceneModel::populateScene()
        (node = (vtkMRMLNode*)d->MRMLScene->GetNodes()->GetNextItemAsObject(it)) ;)
     {
     index++;
-    this->insertNode(node, index);
+    Q_ASSERT(index == d->RowCache.count());
+    d->insertNode(node, index);
     }
   foreach(vtkMRMLNode* misplacedNode, d->MisplacedNodes)
     {
@@ -789,39 +790,40 @@ void qMRMLSceneModel::populateScene()
 //------------------------------------------------------------------------------
 QStandardItem* qMRMLSceneModel::insertNode(vtkMRMLNode* node)
 {
-  return this->insertNode(node, this->nodeIndex(node));
+  Q_D(qMRMLSceneModel);
+  return d->insertNode(node, this->nodeIndex(node));
 }
 
 //------------------------------------------------------------------------------
-QStandardItem* qMRMLSceneModel::insertNode(vtkMRMLNode* node, int nodeIndex)
+QStandardItem* qMRMLSceneModelPrivate::insertNode(vtkMRMLNode* node, int nodeIndex)
 {
-  Q_D(qMRMLSceneModel);
-  QStandardItem* nodeItem = this->itemFromNode(node);
+  Q_Q(qMRMLSceneModel);
+  QStandardItem* nodeItem = q->itemFromNode(node);
   if (nodeItem != 0)
     {
     // It is possible that the node has been already added if it is the parent
     // of a child node already inserted.
     return nodeItem;
     }
-  vtkMRMLNode* parentNode = this->parentNode(node);
+  vtkMRMLNode* parentNode = q->parentNode(node);
   QStandardItem* parentItem =
-    parentNode ? this->itemFromNode(parentNode) : this->mrmlSceneItem();
+    parentNode ? q->itemFromNode(parentNode) : q->mrmlSceneItem();
   if (!parentItem)
     {
     Q_ASSERT(parentNode);
-    parentItem = this->insertNode(parentNode);
+    parentItem = q->insertNode(parentNode);
     Q_ASSERT(parentItem);
     }
-  int min = this->preItems(parentItem).count();
-  int max = parentItem->rowCount() - this->postItems(parentItem).count();
+  int min = q->preItems(parentItem).count();
+  int max = parentItem->rowCount() - q->postItems(parentItem).count();
   int row = min + nodeIndex;
   if (row > max)
     {
-    d->MisplacedNodes << node;
+    this->MisplacedNodes << node;
     row = max;
     }
-  nodeItem = this->insertNode(node, parentItem, row);
-  Q_ASSERT(this->itemFromNode(node) == nodeItem);
+  nodeItem = q->insertNode(node, parentItem, row);
+  Q_ASSERT(q->itemFromNode(node) == nodeItem);
   return nodeItem;
 }
 

Libs/MRML/Widgets/qMRMLSceneModel.h
@@ -269,7 +269,6 @@ protected:
   virtual void updateScene();
   virtual void populateScene();
   virtual QStandardItem* insertNode(vtkMRMLNode* node);
-  virtual QStandardItem* insertNode(vtkMRMLNode* node, int index);
   virtual QStandardItem* insertNode(vtkMRMLNode* node, QStandardItem* parent, int row = -1);
 
   virtual bool isANode(const QStandardItem* item)const;

Libs/MRML/Widgets/qMRMLSceneModel_p.h
@@ -71,6 +71,12 @@ public:
   void listenNodeModifiedEvent();
   void reparentItems(QList<QStandardItem*>& children, int newIndex, QStandardItem* newParent);
 
+  /// This method is called by qMRMLSceneModel::populateScene() to speed up
+  /// the loading of large scene. By explicitly specifying the \a idnex, it
+  /// skips repetitive scene traversal calls caused by
+  /// qMRMLSceneModel::nodeIndex(vtkMRMLNode*).
+  QStandardItem* insertNode(vtkMRMLNode* node, int index);
+
   vtkSmartPointer<vtkCallbackCommand> CallBack;
   qMRMLSceneModel::NodeTypes ListenNodeModifiedEvent;
   bool LazyUpdate;

commit 2cad7bd0e359b674be2ac2dc97f42a7d3443cc6e

BUG: Remove invalid assert while populating the scene. Fixes #3445, #4199
This commit fixes a regression introduced in r25049 (COMP: qMRMLSceneModel:
Fix -Woverloaded-virtual and add comment.)

The assert was checking whether the index of the current node
matches the row count. However, while inserting nodes, the
logic adds a parent node if there isn't one. This insertion of
parent node increments the RowCache size. Next time the assert
was encountered, it would fail.

From: Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@25143 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Widgets/qMRMLSceneModel.cxx
@@ -778,7 +778,6 @@ void qMRMLSceneModel::populateScene()
        (node = (vtkMRMLNode*)d->MRMLScene->GetNodes()->GetNextItemAsObject(it)) ;)
     {
     index++;
-    Q_ASSERT(index == d->RowCache.count());
     d->insertNode(node, index);
     }
   foreach(vtkMRMLNode* misplacedNode, d->MisplacedNodes)