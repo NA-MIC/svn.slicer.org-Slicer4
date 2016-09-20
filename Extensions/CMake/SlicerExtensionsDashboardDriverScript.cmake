7944ba508e630b747e8e8d9eeea71c6aea3bdea9
jcfr <jcfr@3bd1e089-480b-0410-8dfb-8563597acbee>
2016-09-20 21:43:26 +0000

    BUG: Fix extension build ensuring it can read Midas credential from env
    This commit partially backports r24987
    
    git-svn-id: http://svn.slicer.org/Slicer4/branches/Slicer-4-5@25381 3bd1e089-480b-0410-8dfb-8563597acbee

--- Extensions/CMake/SlicerExtensionsDashboardDriverScript.cmake
+++ Extensions/CMake/SlicerExtensionsDashboardDriverScript.cmake
@@ -114,10 +114,37 @@ function(_get_slicer_revision headerfile varname)
 endfunction()
 
 
-# Macro allowing to set a variable to its default value only if not already defined
+# Macro allowing to set a variable to its default value if not already defined.
+# The default value is set with:
+#  (1) if set, the value environment variable <var>.
+#  (2) if set, the value of local variable variable <var>.
+#  (3) if none of the above, the value passed as a parameter.
+# Setting the optional parameter 'OBFUSCATE' will display 'OBFUSCATED' instead of the real value.
 macro(setIfNotDefined var defaultvalue)
+  set(_obfuscate FALSE)
+  foreach(arg ${ARGN})
+    if(arg STREQUAL "OBFUSCATE")
+      set(_obfuscate TRUE)
+    endif()
+  endforeach()
+  if(DEFINED ENV{${var}} AND NOT DEFINED ${var})
+    set(_value "$ENV{${var}}")
+    if(_obfuscate)
+      set(_value "OBFUSCATED")
+    endif()
+    message(STATUS "Setting '${var}' variable with environment variable value '${_value}'")
+    set(${var} $ENV{${var}})
+  endif()
   if(NOT DEFINED ${var})
+    set(_value "${defaultvalue}")
+    if(_obfuscate)
+      set(_value "OBFUSCATED")
+    endif()
+    message(STATUS "Setting '${var}' variable with default value '${_value}'")
     set(${var} "${defaultvalue}")
+  endif()
+  if(NOT _obfuscate)
+    list(APPEND variables ${var})
   endif()
 endmacro()
 
@@ -146,17 +173,11 @@ set(expected_variables
   CTEST_MEMORYCHECK_COMMAND
   CTEST_SVN_COMMAND
   CTEST_GIT_COMMAND
-  MIDAS_PACKAGE_URL
-  MIDAS_PACKAGE_EMAIL
-  MIDAS_PACKAGE_API_KEY
   Slicer_DIR
   )
 
 if(WITH_DOCUMENTATION)
   list(APPEND expected_variables DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY)
-endif()
-if(NOT DEFINED CTEST_PARALLEL_LEVEL)
-  set(CTEST_PARALLEL_LEVEL 8)
 endif()
 
 if(EXISTS "${CTEST_LOG_FILE}")
@@ -179,6 +200,15 @@ message(CTEST_SOURCE_DIRECTORY:${CTEST_SOURCE_DIRECTORY})
 if(NOT EXTENSIONS_BUILDSYSTEM_TESTING)
   set(Slicer_EXTENSION_DESCRIPTION_DIR "${CTEST_BINARY_DIRECTORY}/ExtensionsIndex")
 endif()
+
+#-----------------------------------------------------------------------------
+# Set default values
+#-----------------------------------------------------------------------------
+setIfNotDefined(CTEST_PARALLEL_LEVEL 8)
+setIfNotDefined(MIDAS_PACKAGE_URL "http://slicer.kitware.com/midas3")
+setIfNotDefined(MIDAS_PACKAGE_EMAIL "MIDAS_PACKAGE_EMAIL-NOTDEFINED" OBFUSCATE)
+setIfNotDefined(MIDAS_PACKAGE_API_KEY "MIDAS_PACKAGE_API_KEY-NOTDEFINED" OBFUSCATE)
+setIfNotDefined(CTEST_DROP_SITE "slicer.cdash.org")
 
 # Since having '/' in the build name confuses CDash (Separate entries are added for both
 # update and rest of the build), let's convert them into '-'.
