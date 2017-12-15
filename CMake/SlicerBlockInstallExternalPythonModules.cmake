NA-MIC/svn.slicer.org-Slicer4

commit 3390924e9db0e9859b5cd67dcf9c0a495642d234

ENH: Update VTK
List of changes for VTK7:

$ git shortlog 57d826c..c19ce17 --no-merges
Andras Lasso (1):
      Fix interactionState computation in SeedWidget

List of changes for VTK8:

$ git shortlog 71cf316..b796f88 --no-merges
Andras Lasso (1):
      Fix interactionState computation in SeedWidget

List of changes for VTK9:

$ git shortlog e3f97b0..27c57a1 --no-merges
Allison Vacanti (8):
      Restore Timing functionality for D3/PkTree.
      Replaced std::set with std::vector/sort/unique in vtkExtractCells.
      Optimize the PointMap lookup in vtkExtractCells (12.8% speedup in D3).
      Use a batch copy for points/pointdata in vtkExtractCells.
      Add a minimum ghost level option to D3.
      Fix MSVC warning C4512 in KdTree and D3 classes.
      Deprecate vtkHyperOctreeFractalSource.
      Deprecate everything that touches vtkHyperOctree.

Alvaro Sanchez (1):
      Fix various build issues of vtkRenderingOptiX.

Andras Lasso (1):
      Fix interactionState computation in SeedWidget

Andrew Bauer (4):
      Fixing vtkExodusIIReader issue with QA record name length
      Cleanup and improvements for vtkPUnstructuredGridGhostCellsGenerator
      Pre-incrementing stl iterators instead of post-incrementing
      Fixing using EGL when kits are enabled

Ben Boeckel (48):
      utf8: remove old import
      utf8: add an update script
      utf8: add module infrastructure
      cmake: use the vtkutf8 module
      vtkMakeInstantiator: remove support
      cmake: remove usage of the ABSTRACT property
      tcl: deprecate for 8.1
      vtkJavaUtil: support converting constant arrays to Java
      vtkOpenGLBufferObject: include missing <string> header
      vtkOpenGLGL2PSHelperImpl: also don't wrap in Python
      vtkWrapJava: make empty files for unwrappable classes
      Common/Core: fix source file property settings
      Java: follow Python
      Java: vtkRenderWindowPanel needs vtkCanvas
      Java: remove `-sourcepath` arguments
      Utilities/Rpm: remove

SuperBuild/External_VTKv9.cmake
@@ -120,11 +120,11 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT ${CMAKE_PROJECT_N
 
 set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "7")
-  set(_git_tag "57d826c1bcdf6cd201b1e77dd14a84d6930e3a55")
+  set(_git_tag "c19ce17b39cf2d72f920737f4611f899f7672938")
 elseif("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "71cf31677877a3a93a3d3f23ad4c400cf7dff770")
+  set(_git_tag "b796f88c53a616558eab61ece778f3960debbcad")
 elseif("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "9")
-  set(_git_tag "e3f97b0952160c7d7c599deacd8e630eb9ebfc26")
+  set(_git_tag "27c57a18d6fc7154de7d839fa0380b58c456969b")
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()

commit 1cd54c2ab2f055a773da259f603082a551b32fa2

BUG: Fix import of vtk modules when building against VTKv9 and Qt5
This commit updates the build system to ensure vtk can be imported from
a build or an install tree.

It fixes a regression introduced in r26697 where a newer version VTK9
is used. The Slicer build system is updated to adapt to changes
introduced the commit kitware/VTK@867d93c (Cleanup VTK's Python package.).

It also updates VTK9 to include the following patch:

$ git shortlog Slicer/VTK@27c57a1..Slicer/VTK@887c506 --no-merges
Jean-Christophe Fillion-Robin (1):
      cmake: re-associate RuntimeLibraries component with python install rules

also contributed upstream as https://gitlab.kitware.com/vtk/vtk/merge_requests/3708

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26719 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerBlockInstallExternalPythonModules.cmake
@@ -1,16 +1,27 @@
 
+if(NOT APPLE)
+  message(FATAL_ERROR "This module was designed for macOS")
+endif()
+
 # Install VTK python module
-set(VTK_PYTHON_MODULE "${VTK_DIR}/Wrapping/Python")
+if(Slicer_VTK_VERSION_MAJOR VERSION_GREATER "7")
+  set(VTK_PYTHON_MODULE "${VTK_DIR}/lib/python2.7/site-packages")
+else()
+  set(VTK_PYTHON_MODULE "${VTK_DIR}/Wrapping/Python")
+endif()
 install(DIRECTORY ${VTK_PYTHON_MODULE}/vtk
   DESTINATION ${Slicer_INSTALL_BIN_DIR}/Python
   USE_SOURCE_PERMISSIONS
-  COMPONENT Runtime)
+  COMPONENT Runtime
+  # VTK9: Add exclusions to avoid installing VTK's C++ Python modules
+  # These have their own install rules (see below).
+  PATTERN "vtk*Python.so" EXCLUDE)
 
 # Install external python runtime libraries that we don't link to (fixupbundle won't copy them)
-if(UNIX)
-  set(vtk_python_library_subdir "lib")
+if(Slicer_VTK_VERSION_MAJOR VERSION_GREATER "7")
+  set(vtk_python_library_subdir "lib/python2.7/site-packages/vtk")
 else()
-  set(vtk_python_library_subdir "bin")
+  set(vtk_python_library_subdir "lib")
 endif()
 file(GLOB vtk_python_modules "${VTK_DIR}/${vtk_python_library_subdir}/*Python.so")
 install(FILES ${vtk_python_modules}

SuperBuild/External_VTKv9.cmake
@@ -110,7 +110,7 @@ set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "7")
   set(_git_tag "43f6ee36f6e28c8347768bd97df4d767da6b4ce7")
 elseif("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "9")
-  set(_git_tag "27c57a18d6fc7154de7d839fa0380b58c456969b")
+  set(_git_tag "887c5065ec2220be04b105ce1d966c5162e729af")
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()
@@ -184,10 +184,23 @@ endif()
     )
 
   # pythonpath
-  set(${proj}_PYTHONPATH_LAUNCHER_BUILD
-    ${VTK_DIR}/Wrapping/Python
-    ${VTK_DIR}/${_library_output_subdir}/<CMAKE_CFG_INTDIR>
-    )
+  if(Slicer_VTK_VERSION_MAJOR VERSION_GREATER 7)
+    if(UNIX)
+      set(${proj}_PYTHONPATH_LAUNCHER_BUILD
+        ${VTK_DIR}/${_library_output_subdir}/python2.7/site-packages
+        )
+    else()
+      set(${proj}_PYTHONPATH_LAUNCHER_BUILD
+        ${VTK_DIR}/${_library_output_subdir}/<CMAKE_CFG_INTDIR>/Lib/site-packages
+        )
+    endif()
+  else()
+    set(${proj}_PYTHONPATH_LAUNCHER_BUILD
+      ${VTK_DIR}/Wrapping/Python
+      ${VTK_DIR}/${_library_output_subdir}/<CMAKE_CFG_INTDIR>
+      )
+  endif()
+
   mark_as_superbuild(
     VARS ${proj}_PYTHONPATH_LAUNCHER_BUILD
     LABELS "PYTHONPATH_LAUNCHER_BUILD"