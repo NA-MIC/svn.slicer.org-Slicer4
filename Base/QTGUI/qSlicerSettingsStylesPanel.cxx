NA-MIC/svn.slicer.org-Slicer4

commit b77256a3d1be38d77ae04019a6cb0ad25d9174c3

BUG: Fix Slicer style setting display
The neew Qt5 always returns lower case keys for each style.
Adding a method to convert the style to camel case makes sure the behavior
stays the same as with Qt4.
Also, make sure that when looking up a style name, we are doing it without
checking the case as qt would not either.

From: Johan Andruejol <johan.andruejol@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26829 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerSettingsStylesPanel.cxx
@@ -129,12 +129,12 @@ void qSlicerSettingsStylesPanelPrivate::init()
 int qSlicerSettingsStylesPanelPrivate
 ::styleIndex(const QString& styleName) const
 {
-  int styleIndex = this->StyleComboBox->findText(styleName);
+  int styleIndex = this->StyleComboBox->findText(styleName, Qt::MatchFixedString);
   if (styleIndex == -1)
     {
     // if not found (call setCurrentStyle with a wrong style for example),
     // default to slicer's style
-    styleIndex = this->StyleComboBox->findText("Slicer");
+    styleIndex = this->StyleComboBox->findText("Slicer", Qt::MatchFixedString);
     }
   return styleIndex;
 }
@@ -145,6 +145,32 @@ isQtStyle(const QString& styleName) const
 {
   // Styles are case insensitive
   return this->QtStyles.contains(styleName, Qt::CaseInsensitive);
+}
+
+namespace
+{
+QString toCamelCase(const QString& s)
+{
+  QString camelCase;
+  if (s.size() <= 0)
+    {
+    return camelCase;
+    }
+
+  camelCase = s[0].toUpper();
+  for (int i = 1; i < s.size(); ++i)
+    {
+    if (s[i - 1] == " ")
+      {
+      camelCase.append(s[i].toUpper());
+      }
+    else
+      {
+      camelCase.append(s[i]);
+      }
+    };
+  return camelCase;
+}
 }
 
 // --------------------------------------------------------------------------
@@ -159,7 +185,7 @@ void qSlicerSettingsStylesPanelPrivate::populateStyles()
     {
     if (! this->isQtStyle(style)) // check if not Qt's built in style
       {
-      this->StyleComboBox->addItem(style);
+      this->StyleComboBox->addItem(toCamelCase(style));
       }
     }
 

commit 4a374367b229c29a28b5d66a49fd8f3c8ab5e32d

COMP: Fix invalid operand build error in qSlicerSettingsStylesPanel
This commit fixes the following error introduced by r26829 (BUG: Fix
Slicer style setting display)

/path/to/Slicer/Base/QTGUI/qSlicerSettingsStylesPanel.cxx: In function ‘QString {anonymous}::toCamelCase(const QString&)’:
/path/to/Slicer/Base/QTGUI/qSlicerSettingsStylesPanel.cxx:163:18: error: no match for ‘operator==’ (operand types are ‘const QChar’ and ‘const char [2]’)
     if (s[i - 1] == " ")
                  ^

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26833 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerSettingsStylesPanel.cxx
@@ -160,7 +160,7 @@ QString toCamelCase(const QString& s)
   camelCase = s[0].toUpper();
   for (int i = 1; i < s.size(); ++i)
     {
-    if (s[i - 1] == " ")
+    if (s[i - 1] == ' ')
       {
       camelCase.append(s[i].toUpper());
       }