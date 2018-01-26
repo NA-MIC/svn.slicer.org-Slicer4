NA-MIC/svn.slicer.org-Slicer4

commit 7f9d046cc45a60793adaa498709dfd280714f15f

ENH: Allow loading STL files as segmentation node
In Add data dialog, users can now choose "Segmentation" for STL files.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26868 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Segmentations/qSlicerSegmentationsReader.cxx
@@ -22,8 +22,6 @@
 #include "qSlicerSegmentationsReader.h"
 #include "qSlicerSegmentationsIOOptionsWidget.h"
 
-#include "vtkMRMLSegmentationNode.h"
-
 // Qt includes
 #include <QFileInfo>
 
@@ -32,9 +30,13 @@
 
 // MRML includes
 #include <vtkMRMLScene.h>
+#include <vtkMRMLSegmentationNode.h>
+#include <vtkMRMLSegmentationDisplayNode.h>
+#include <vtkMRMLStorageNode.h>
 
 // VTK includes
 #include <vtkSmartPointer.h>
+#include <vtkSTLReader.h>
 
 //-----------------------------------------------------------------------------
 class qSlicerSegmentationsReaderPrivate
@@ -86,7 +88,8 @@ qSlicerIO::IOFileType qSlicerSegmentationsReader::fileType()const
 //-----------------------------------------------------------------------------
 QStringList qSlicerSegmentationsReader::extensions()const
 {
-  return QStringList() << "Segmentation (*.seg.nrrd)" << "Segmentation (*.seg.vtm)" << "Segmentation (*.nrrd)" << "Segmentation (*.vtm)";
+  return QStringList() << "Segmentation (*.seg.nrrd)" << "Segmentation (*.seg.vtm)"
+    << "Segmentation (*.nrrd)" << "Segmentation (*.vtm)" << "Segmentation (*.stl)";
 }
 
 //-----------------------------------------------------------------------------
@@ -96,7 +99,7 @@ qSlicerIOOptions* qSlicerSegmentationsReader::options()const
   options->setMRMLScene(this->mrmlScene());
   return options;
 }
- 
+
 //-----------------------------------------------------------------------------
 bool qSlicerSegmentationsReader::load(const IOProperties& properties)
 {
@@ -110,24 +113,67 @@ bool qSlicerSegmentationsReader::load(const IOProperties& properties)
     return false;
     }
 
-  bool autoOpacities = true;
-  if (properties.contains("autoOpacities"))
-    {
-    autoOpacities = properties["autoOpacities"].toBool();
-    }
+  std::string extension = vtkMRMLStorageNode::GetLowercaseExtensionFromFileName(fileName.toStdString());
 
-  vtkMRMLSegmentationNode* node = d->SegmentationsLogic->LoadSegmentationFromFile(fileName.toLatin1().constData(), autoOpacities);
-  if (!node)
+  if (extension.compare(".stl") == NULL)
     {
-    this->setLoadedNodes(QStringList());
-    return false;
-    }
+    // Create segmentation from STL file
+    vtkNew<vtkSTLReader> reader;
+    reader->SetFileName(fileName.toStdString().c_str());
+    reader->Update();
+    vtkPolyData* closedSurfaceRepresentation = reader->GetOutput();
+    if (!closedSurfaceRepresentation)
+      {
+      return false;
+      }
 
-  this->setLoadedNodes( QStringList(QString(node->GetID())) );
-  if (properties.contains("name"))
+    QString name = QFileInfo(fileName).baseName();
+    if (properties.contains("name"))
+      {
+      name = properties["name"].toString();
+      }
+
+    vtkNew<vtkSegment> segment;
+    segment->SetName(name.toLatin1().constData());
+    segment->AddRepresentation(vtkSegmentationConverter::GetSegmentationClosedSurfaceRepresentationName(), closedSurfaceRepresentation);
+
+    vtkMRMLSegmentationNode* segmentationNode = vtkMRMLSegmentationNode::SafeDownCast(
+      this->mrmlScene()->AddNewNodeByClass("vtkMRMLSegmentationNode", name.toLatin1().constData()));
+    segmentationNode->CreateDefaultDisplayNodes();
+    vtkMRMLSegmentationDisplayNode* displayNode = vtkMRMLSegmentationDisplayNode::SafeDownCast(segmentationNode->GetDisplayNode());
+    if (displayNode)
+      {
+      // Show slice intersections using closed surface representation (don't create binary labelmap for display)
+      displayNode->SetPreferredDisplayRepresentationName2D(vtkSegmentationConverter::GetSegmentationClosedSurfaceRepresentationName());
+      }
+
+    segmentationNode->GetSegmentation()->AddSegment(segment);
+
+    this->setLoadedNodes(QStringList(QString(segmentationNode->GetID())));
+    }
+  else
     {
-    std::string uname = this->mrmlScene()->GetUniqueNameByString(properties["name"].toString().toLatin1());
-    node->SetName(uname.c_str());
+    // Non-STL file, load using segmentation storage node
+    bool autoOpacities = true;
+    if (properties.contains("autoOpacities"))
+      {
+      autoOpacities = properties["autoOpacities"].toBool();
+      }
+
+    vtkMRMLSegmentationNode* node = d->SegmentationsLogic->LoadSegmentationFromFile(fileName.toLatin1().constData(), autoOpacities);
+    if (!node)
+      {
+      this->setLoadedNodes(QStringList());
+      return false;
+      }
+
+    this->setLoadedNodes( QStringList(QString(node->GetID())) );
+
+    if (properties.contains("name"))
+      {
+      std::string uname = this->mrmlScene()->GetUniqueNameByString(properties["name"].toString().toLatin1());
+      node->SetName(uname.c_str());
+      }
     }
 
   return true;

commit aff891fb8759ed4647952e84a0184bbb7f5dab6a

COMP: Fix build error related to qSlicerSegmentationsReader
This commit fixes build error and warning introduced by r26868 (ENH: Allow loading
STL files as segmentation node)

/path/to/Slicer/Modules/Loadable/Segmentations/qSlicerSegmentationsReader.cxx:121:5: error: ‘vtkNew’ was not declared in this scope
     vtkNew<vtkSTLReader> reader;
     ^
/path/to/Slicer/Modules/Loadable/Segmentations/qSlicerSegmentationsReader.cxx:121:24: error: expected primary-expression before ‘>’ token
     vtkNew<vtkSTLReader> reader;
                        ^
/path/to/Slicer/Modules/Loadable/Segmentations/qSlicerSegmentationsReader.cxx:121:26: error: ‘reader’ was not declared in this scope
     vtkNew<vtkSTLReader> reader;
                          ^
/path/to/Slicer/Modules/Loadable/Segmentations/qSlicerSegmentationsReader.cxx:136:22: error: expected primary-expression before ‘>’ token
     vtkNew<vtkSegment> segment;
                      ^
/path/to/Slicer/Modules/Loadable/Segmentations/qSlicerSegmentationsReader.cxx:136:24: error: ‘segment’ was not declared in this scope
     vtkNew<vtkSegment> segment;
                        ^


                                    ^

git-svn-id: http://svn.slicer.org/Slicer4/trunk@26870 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/Segmentations/qSlicerSegmentationsReader.cxx
@@ -35,6 +35,7 @@
 #include <vtkMRMLStorageNode.h>
 
 // VTK includes
+#include <vtkNew.h>
 #include <vtkSmartPointer.h>
 #include <vtkSTLReader.h>
 