NA-MIC/svn.slicer.org-Slicer4

commit 3d2270ba5649010f3998a6c943a9baefc74bfabb

BUG: Fix loading of Slicer style on macOS using installed version of Slicer
This commit partially reverts r26967 (ENH: Initialize application
style using its name) and explicitly instantiates and sets the style.

This is required because the Qt plugin providing the custom styles fails
to load before the application is constructed.

This is explained  because the hints allowing a successful load of the plugin
are either set in the application constructor or after it is constructed:

* qt.conf available in the application resources is read in the application constructor

* QT_PLUGIN_PATH is only set after the application is instantiated
  by using the logic reading the SlicerLauncherSettings.ini

For these reason, directly instantiating the style address the problem.

That said, to address the root cause and have the environment initialized
before constructing the application (and have QT_PLUGIN_PATH set already),
an other approach would be to:

1) Implement a method allowing to get the application file path on all platforms
   before the QApplication is constructed.

2) Move the logic reading the launcher settings before the application is initialized.

Useful references are:
* https://issues.slicer.org/view.php?id=4515
* http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=24465
* https://forum.qt.io/topic/50305/qt-5-3-and-5-4-mac-application-crashes-on-startup-after-using-macdeployqt-app-unable-to-load-plugin-cocoa/8


Reported-by: Ron Kikinis <kikinis@bwh.harvard.edu>
Reported-by: Steve Pieper <pieper@bwh.harvard.edu>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@27030 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTApp/qSlicerApplicationHelper.cxx
@@ -23,7 +23,6 @@
 // Qt includes
 #include <QFont>
 #include <QSettings>
-#include <QStyleFactory>
 #include <QSysInfo>
 
 // Slicer includes
@@ -37,7 +36,6 @@
 #include "qSlicerLoadableModuleFactory.h"
 #include "qSlicerModuleFactoryManager.h"
 #include "qSlicerModuleManager.h"
-#include "qSlicerSettingsStylesPanel.h"
 
 #ifdef Slicer_USE_PYTHONQT
 # include "qSlicerScriptedLoadableModuleFactory.h"
@@ -75,21 +73,6 @@ qSlicerApplicationHelper::qSlicerApplicationHelper(QObject * parent) : Superclas
 //----------------------------------------------------------------------------
 qSlicerApplicationHelper::~qSlicerApplicationHelper()
 {
-}
-
-//----------------------------------------------------------------------------
-void qSlicerApplicationHelper::preInitializeApplication(
-    const char* argv0, const QString& styleName)
-{
-  itk::itkFactoryRegistration();
-  ctkProxyStyle* style = qobject_cast<ctkProxyStyle*>(QStyleFactory::create(styleName));
-  if (!style)
-    {
-    qWarning() << "Failed to set" << styleName << "style: "
-               << "Available styles are"
-               << qSlicerSettingsStylesPanel::availableSlicerStyles().join(", ");
-    }
-  Self::preInitializeApplication(argv0, style);
 }
 
 //----------------------------------------------------------------------------

Applications/SlicerApp/Main.cxx
@@ -21,6 +21,7 @@
 // Slicer includes
 #include "qSlicerApplication.h"
 #include "qSlicerApplicationHelper.h"
+#include "qSlicerStyle.h"
 
 // SlicerApp includes
 #include "qSlicerAppMainWindow.h"
@@ -32,8 +33,9 @@ namespace
 int SlicerAppMain(int argc, char* argv[])
 {
   typedef qSlicerAppMainWindow SlicerMainWindowType;
+  typedef qSlicerStyle SlicerAppStyle;
 
-  qSlicerApplicationHelper::preInitializeApplication(argv[0], "Slicer");
+  qSlicerApplicationHelper::preInitializeApplication(argv[0], new SlicerAppStyle);
 
   qSlicerApplication app(argc, argv);
   if (app.returnCode() != -1)

Base/QTApp/qSlicerApplicationHelper.h
@@ -44,8 +44,6 @@ public:
   qSlicerApplicationHelper(QObject * parent = 0);
   virtual ~qSlicerApplicationHelper();
 
-  static void preInitializeApplication(const char* argv0, const QString& styleName);
-
   static void preInitializeApplication(const char* argv0, ctkProxyStyle* style);
 
   template<typename SlicerMainWindowType>

commit 055aa5eee21a7d1a144224dab5580c82a39af0c9

BUG: Fix loading of images ensuring the ITK IO factories are loaded
This commit fixes a regression introduced in r27030 (BUG: Fix loading of
Slicer style on macOS using installed version of Slicer) where the call to
"itk::itkFactoryRegistration()" was inadvertently removed during the
re-factoring.

Reported-by: Csaba Pinter <csaba.pinter@queensu.ca>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@27035 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTApp/qSlicerApplicationHelper.cxx
@@ -79,6 +79,7 @@ qSlicerApplicationHelper::~qSlicerApplicationHelper()
 void qSlicerApplicationHelper::preInitializeApplication(
     const char* argv0, ctkProxyStyle* style)
 {
+  itk::itkFactoryRegistration();
 #if QT_VERSION >= 0x040803
 #ifdef Q_OS_MACX
   if (QSysInfo::MacintoshVersion > QSysInfo::MV_10_8)