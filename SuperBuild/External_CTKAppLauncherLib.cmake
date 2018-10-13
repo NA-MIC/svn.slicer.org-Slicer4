NA-MIC/svn.slicer.org-Slicer4

commit 43c7930d17549d4deb1587979195f0fabf811314

ENH: Update CTKAppLauncher: Add support for interactive process
Add support for interactive process
See https://discourse.slicer.org/t/static-build-of-qt5-to-update-the-application-launcher-and-support-interactive-process/3801

Fixes #4595: multiple rules generate slicer (ninja error)
See https://issues.slicer.org/view.php?id=4595

git-svn-id: http://svn.slicer.org/Slicer4/trunk@27469 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTKAppLauncherLib.cmake
@@ -26,7 +26,7 @@ if(NOT DEFINED CTKAppLauncherLib_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "b0dd46a876a6620f900845c357b326725ed11db8"
+    "v0.1.25"
     QUIET
     )
 

SuperBuild/External_CTKAPPLAUNCHER.cmake
@@ -24,15 +24,15 @@ if(Slicer_USE_CTKAPPLAUNCHER)
   if(NOT DEFINED CTKAppLauncher_DIR)
 
     SlicerMacroGetOperatingSystemArchitectureBitness(VAR_PREFIX CTKAPPLAUNCHER)
-    set(launcher_version "0.1.24")
+    set(launcher_version "0.1.25")
     # On windows, use i386 launcher unconditionally
     if("${CTKAPPLAUNCHER_OS}" STREQUAL "win")
       set(CTKAPPLAUNCHER_ARCHITECTURE "i386")
-      set(md5 "fcc4ae69ceaebf2453f894e769585224")
+      set(md5 "1ee54235bde72e7ee7ab1d6d18ba5693")
     elseif("${CTKAPPLAUNCHER_OS}" STREQUAL "linux")
-      set(md5 "0d32c7d92eac2b87f6ec353ca67ea542")
+      set(md5 "d21d779b8cf4b7cf554775af2db2b494")
     elseif("${CTKAPPLAUNCHER_OS}" STREQUAL "macosx")
-      set(md5 "985fce5454b0d2528d519dd17007bef4")
+      set(md5 "6c4ec5b6d26e379da2e9c4381a21c26d")
     endif()
 
     set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj})

commit 13a7c4b43b5dac2afc2c0e254893ecbdecd125df

COMP: Update CTKAppLauncherLib to fix windows link error. Fix #4631
See https://issues.slicer.org/view.php?id=4631

This commit fixes a regression introduced in r27469 (ENH: Update
CTKAppLauncher: Add support for interactive process)

List of changes:

$ git shortlog v0.1.25..ec98a50 --no-merges
Jean-Christophe Fillion-Robin (5):
      Begin post-v0.1.25 development [ci skip]
      ci/appveyor: Ensure commands executed using Invoke-Expression report failure
      Fix link error calling Q_IMPORT_PLUGIN only if Qt build was build statically
      ci/appveyor: An other attempt at returning Invoke-Expression exit code
      Main.cpp: Fix check of QT_STATIC macro

git-svn-id: http://svn.slicer.org/Slicer4/trunk@27477 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_CTKAppLauncherLib.cmake
@@ -26,7 +26,7 @@ if(NOT DEFINED CTKAppLauncherLib_DIR AND NOT Slicer_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     Slicer_${proj}_GIT_TAG
-    "v0.1.25"
+    "ec98a50327644f6607b1a21bf016eba1027134d5"
     QUIET
     )
 