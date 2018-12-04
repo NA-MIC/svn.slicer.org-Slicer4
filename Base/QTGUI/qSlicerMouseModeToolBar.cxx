NA-MIC/svn.slicer.org-Slicer4

commit 5c859024ba9ef1d517f01518ecbfc99d304eb120

BUG: Update VTK backporting fixes
List of changes:

$ git shortlog 75414fe171..a3a5b7798e --no-merges
Andras Lasso (1):
      [backport] Simplify initialization of vtkAbstractPolyDataReader based readers

Cornelis Bockemühl (1):
      [backport] Avoid crashes with string arrays

Cory Quammen (3):
      [backport] Remove direct disable of GL_SCISSOR_TEST
      [backport] Silence viewport warnings
      [backport] Fix incomplete cherry pick in 8921e9a6

Dan Lipsa (3):
      [backport] Do not call kCFCoreFoundationVersionNumber for OSMesa.
      [backport] Fix typo.
      [backport] OpenGL2/vtkOpenGLGlyph3DMapper: Fix segfault when indexArray is 0.

Edwin Bennink (1):
      [backport] Fix ScaleContour function

Elvis Stansvik (1):
      [backport] Support fractional device pixel ratio in native Qt widget

Jean-Christophe Fillion-Robin (1):
      Revert "Revert "Added a mode in handle widget to allow showing disabled handles""

Ken Martin (3):
      [backport] OpenGL2/vtkOrderIndependentTranslucentPass: try a fix for osx gt120 systems
      [backport] OpenVR: minor fix to ensure depth is correct for controllers
      [backport] OpenVR: add event invokes for load and save camera state

Mathieu Westphal (3):
      [backport] OpenGL2: Scale and Opacity table fix for point gaussian representation
      [backport] MakeSure the PolyLineWidget representation is updated
      [backport] Fix long array in legacy reader/writer

Michael Migliore (1):
      [backport] Set FileName to null in abstract reader

Sankhesh Jhaveri (4):
      [backport] Invoke CursorChangedEvent to from vtkGenericOpenGLRenderWindow
      [backport] Allow widgets to change cursor when using QVTKOpenGLWindow
      [backport] Allow widgets to change cursor when using QVTKOpenGLNativeWidget
      [backport] Ensure widget state doesn't change when widget already enabled/disabled

Scott Wittenburg (1):
      [backport] PERF: vtkOpenGLContextDevice2D: Cache all drawn polydata instead of just the last one

SuperBuild/External_VTK.cmake
@@ -118,7 +118,7 @@ set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "7")
   set(_git_tag "43f6ee36f6e28c8347768bd97df4d767da6b4ce7")
 elseif("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "75414fe171e5ae2ed6e4df608deb1d578d9ec7c3")
+  set(_git_tag "a3a5b7798efd37e12219d80bfadd96286dfa2c7e")
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()

commit 463124aa034860544e7eed9dd1a6ee14abd507a5

BUG: Ensure mouse mode icon is always visible when placing annotations
This commit fixes a regression introduced by r27574 (BUG: Update VTK backporting
fixes) integrating VTK commit vtk/vtk@a9af5ea23b (Allow widgets to change
cursor when using QVTKOpenGLNativeWidget). See [1]

[1] https://gitlab.kitware.com/vtk/vtk/commit/a9af5ea23ba6831193267487b66f7bbaf5f42b55

Associated merge request is https://gitlab.kitware.com/vtk/vtk/merge_requests/4956

List of VTK changes:

$ git shortlog a3a5b779..4ab10d5674 --no-merges
Jean-Christophe Fillion-Robin (1):
      ENH: Support setting custom default cursor for QVTKOpenGL[Native]Widget

git-svn-id: http://svn.slicer.org/Slicer4/trunk@27590 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTGUI/qSlicerMouseModeToolBar.cxx
@@ -27,6 +27,7 @@
 #include "qSlicerLayoutManager.h"
 #include "qMRMLThreeDView.h"
 #include "qMRMLThreeDWidget.h"
+#include "qMRMLSliceView.h"
 #include "qMRMLSliceWidget.h"
 #include "qSlicerMouseModeToolBar_p.h"
 
@@ -463,6 +464,9 @@ void qSlicerMouseModeToolBar::changeCursorTo(QCursor cursor)
   for (int i=0; i < layoutManager->threeDViewCount(); ++i)
     {
     layoutManager->threeDWidget(i)->threeDView()->setCursor(cursor);
+#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 2)
+    layoutManager->threeDWidget(i)->threeDView()->VTKWidget()->setQVTKCursor(cursor);
+#endif
     }
 
   // the slice viewers
@@ -482,10 +486,13 @@ void qSlicerMouseModeToolBar::changeCursorTo(QCursor cursor)
     vtkMRMLSliceNode *sliceNode = vtkMRMLSliceNode::SafeDownCast(visibleViews->GetItemAsObject(v));
     if (sliceNode)
       {
-      qMRMLSliceWidget *sliceView = layoutManager->sliceWidget(sliceNode->GetName());
+      qMRMLSliceView *sliceView = layoutManager->sliceWidget(sliceNode->GetName())->sliceView();
       if (sliceView)
         {
         sliceView->setCursor(cursor);
+#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 2)
+        sliceView->VTKWidget()->setQVTKCursor(cursor);
+#endif
         }
       }
     }

SuperBuild/External_VTK.cmake
@@ -118,7 +118,7 @@ set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "7")
   set(_git_tag "43f6ee36f6e28c8347768bd97df4d767da6b4ce7")
 elseif("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "a3a5b7798efd37e12219d80bfadd96286dfa2c7e")
+  set(_git_tag "4ab10d5674090961d3fc48ac23319b3da97defec")
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()