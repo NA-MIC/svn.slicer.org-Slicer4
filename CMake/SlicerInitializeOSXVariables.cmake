NA-MIC/svn.slicer.org-Slicer4

commit 54f1706cc791c9efd9d421a1ebd66438c02eb364

COMP: Update build system removing support for C++98/Qt4/VTK7
This commit updates the build system removing support for building
Slicer with C++98, VTK7 and Qt4.

It is based on work done in PR #1060.

Co-authored-by: Hans Johnson <hans-johnson@uiowa.edu>
Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
Co-authored-by: Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>

From: Pablo Hernandez-Cerdan <pablo.hernandez@kitware.com>

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28000 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerInitializeOSXVariables.cmake
@@ -124,13 +124,11 @@ if(APPLE)
     endif()
   endif()
 
-  if(DEFINED Slicer_SOURCE_DIR AND ("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8" OR DEFINED Qt5_DIR))
+  if(DEFINED Slicer_SOURCE_DIR)
     # Require minimum deployment target of 10.9. In 10.9 libc++ replaces libstdc++
     # as the default runtime. Requiring this minimum ensures that all libraries
     # use libc++.
     set(required_deployment_target "10.9")
-  else()
-    set(required_deployment_target "10.6")
   endif()
   if(CMAKE_OSX_DEPLOYMENT_TARGET VERSION_LESS ${required_deployment_target})
     message(FATAL_ERROR "CMAKE_OSX_DEPLOYMENT_TARGET must be ${required_deployment_target} or greater.")

Base/QTCore/Testing/Cxx/CMakeLists.txt
@@ -52,15 +52,9 @@ if(BUILD_TESTING)
   endif()
 
   set(KIT_TEST_TARGET_LIBRARIES ${KIT})
-  if(CTK_QT_VERSION VERSION_LESS "5")
-    QT4_GENERATE_MOCS(${KIT_TEST_GENERATE_MOC_SRCS})
-    QT4_ADD_RESOURCES(Tests Resources/${KIT}Testing.qrc)
-    list(APPEND KIT_TEST_TARGET_LIBRARIES Qt4::QtTest)
-  else()
     QT5_GENERATE_MOCS(${KIT_TEST_GENERATE_MOC_SRCS})
     QT5_ADD_RESOURCES(Tests Resources/${KIT}Testing.qrc)
     list(APPEND KIT_TEST_TARGET_LIBRARIES Qt5::Test)
-  endif()
 
   if(Slicer_USE_PYTHONQT)
   list(APPEND KIT_TEST_TARGET_LIBRARIES

Base/QTGUI/Testing/Cxx/CMakeLists.txt
@@ -72,17 +72,10 @@ if(BUILD_TESTING)
       )
   endif()
 
-  if(CTK_QT_VERSION VERSION_LESS "5")
-    if(KIT_TEST_GENERATE_MOC_SRCS)
-      QT4_GENERATE_MOCS(${KIT_TEST_GENERATE_MOC_SRCS})
-    endif()
-    QT4_ADD_RESOURCES(Tests Resources/${KIT}Testing.qrc)
-  else()
     if(KIT_TEST_GENERATE_MOC_SRCS)
       QT5_GENERATE_MOCS(${KIT_TEST_GENERATE_MOC_SRCS})
     endif()
     QT5_ADD_RESOURCES(Tests Resources/${KIT}Testing.qrc)
-  endif()
 
   add_executable(${KIT}CxxTests ${Tests})
   target_link_libraries(${KIT}CxxTests ${KIT})

CMake/SlicerBlockFindQtAndCheckVersion.cmake
@@ -24,15 +24,6 @@
 #
 
 macro(__SlicerBlockFindQtAndCheckVersion_find_qt)
-  if(Slicer_REQUIRED_QT_VERSION VERSION_LESS "5")
-    find_package(Qt4)
-    set(_found_var QT4_FOUND)
-    set(_error_details "You probably need to set the QT_QMAKE_EXECUTABLE variable.")
-    set(_major ${QT_VERSION_MAJOR})
-    set(_minor ${QT_VERSION_MINOR})
-    set(_patch ${QT_VERSION_PATCH})
-    set(_found_prefix "QT_")
-  else()
     find_package(Qt5 COMPONENTS ${Slicer_REQUIRED_QT_MODULES})
     set(_found_var Qt5_FOUND)
     set(_error_details "You probably need to set the Qt5_DIR variable.")
@@ -40,7 +31,6 @@ macro(__SlicerBlockFindQtAndCheckVersion_find_qt)
     set(_minor ${Qt5_VERSION_MINOR})
     set(_patch ${Qt5_VERSION_PATCH})
     set(_found_prefix "Qt5")
-  endif()
   if(NOT ${_found_var})
     message(FATAL_ERROR "error: Qt ${Slicer_REQUIRED_QT_VERSION} was not found on your system."
                         "${_error_details}")
@@ -57,12 +47,6 @@ macro(__SlicerBlockFindQtAndCheckVersion_find_qt)
   foreach(module ${Slicer_REQUIRED_QT_MODULES})
     if(NOT "${${_found_prefix}${module}_FOUND}")
       message(FATAL_ERROR "error: Missing Qt module named \"${module}\"")
-    endif()
-    if(NOT ${module} STREQUAL "QTCORE"
-        AND NOT ${module} STREQUAL "QTGUI"
-        AND Slicer_REQUIRED_QT_VERSION VERSION_LESS "5"
-        )
-      set(QT_USE_${module} ON)
     endif()
     set(command_separated_module_list "${command_separated_module_list}${module}, ")
   endforeach()
@@ -81,37 +65,11 @@ endforeach()
 
 # Check QT_QMAKE_EXECUTABLE provided by VTK
 set(extra_error_message)
-if(DEFINED VTK_QT_QMAKE_EXECUTABLE AND Slicer_REQUIRED_QT_VERSION VERSION_LESS "5")
-  #message(STATUS "Checking VTK_QT_QMAKE_EXECUTABLE ...")
-  if(NOT EXISTS "${VTK_QT_QMAKE_EXECUTABLE}")
-    message(FATAL_ERROR "error: You should probably re-configure VTK. VTK_QT_QMAKE_EXECUTABLE points to a nonexistent executable: ${VTK_QT_QMAKE_EXECUTABLE}")
-  endif()
-  set(QT_QMAKE_EXECUTABLE ${VTK_QT_QMAKE_EXECUTABLE})
-  set(extra_error_message "You should probably reconfigure VTK.")
-  __SlicerBlockFindQtAndCheckVersion_find_qt()
-endif()
-
-# Check QT_QMAKE_EXECUTABLE provided by CTK
-if(DEFINED CTK_QT_QMAKE_EXECUTABLE AND Slicer_REQUIRED_QT_VERSION VERSION_LESS "5")
-  if(NOT EXISTS "${CTK_QT_QMAKE_EXECUTABLE}")
-    message(FATAL_ERROR "error: You should probably re-configure CTK. CTK_QT_QMAKE_EXECUTABLE points to a nonexistent executable: ${CTK_QT_QMAKE_EXECUTABLE}")
-  endif()
-  set(QT_QMAKE_EXECUTABLE ${CTK_QT_QMAKE_EXECUTABLE})
-  set(extra_error_message "You should probably reconfigure CTK.")
-  __SlicerBlockFindQtAndCheckVersion_find_qt()
-endif()
-
-if(Slicer_REQUIRED_QT_VERSION VERSION_GREATER "4.9")
   if(DEFINED CTK_Qt5_DIR AND NOT EXISTS "${CTK_Qt5_DIR}")
     message(FATAL_ERROR "error: You should probably re-configure CTK. CTK_Qt5_DIR points to a nonexistent directory: ${CTK_Qt5_DIR}")
   endif()
-endif()
 
 __SlicerBlockFindQtAndCheckVersion_find_qt()
-
-if(Slicer_REQUIRED_QT_VERSION VERSION_LESS "5")
-  include(${QT_USE_FILE})
-endif()
 
 set(_project_name ${Slicer_MAIN_PROJECT_APPLICATION_NAME})
 if(NOT Slicer_SOURCE_DIR)
@@ -121,7 +79,6 @@ message(STATUS "Configuring ${_project_name} with Qt ${_major}.${_minor}.${_patc
 
 # Since Qt5 does not set CMake variables for plugins and binary directories,
 # we explicitly set them here.
-if(Slicer_REQUIRED_QT_VERSION VERSION_GREATER "4.9")
   set(QT_PLUGINS_DIR "${Qt5_DIR}/../../../plugins")
   get_filename_component(QT_PLUGINS_DIR ${QT_PLUGINS_DIR} ABSOLUTE)
   message(STATUS "Setting QT_PLUGINS_DIR: ${QT_PLUGINS_DIR}")
@@ -129,4 +86,3 @@ if(Slicer_REQUIRED_QT_VERSION VERSION_GREATER "4.9")
   set(QT_BINARY_DIR "${Qt5_DIR}/../../../bin")
   get_filename_component(QT_BINARY_DIR ${QT_BINARY_DIR} ABSOLUTE)
   message(STATUS "Setting QT_BINARY_DIR: ${QT_BINARY_DIR}")
-endif()

CMake/SlicerBlockInstallExternalPythonModules.cmake
@@ -4,11 +4,7 @@ if(NOT APPLE)
 endif()
 
 # Install VTK python module
-if(Slicer_VTK_VERSION_MAJOR VERSION_GREATER "7")
-  set(VTK_PYTHON_MODULE "${VTK_DIR}/lib/python2.7/site-packages")
-else()
-  set(VTK_PYTHON_MODULE "${VTK_DIR}/Wrapping/Python")
-endif()
+set(VTK_PYTHON_MODULE "${VTK_DIR}/lib/python2.7/site-packages")
 set(_vtk_package "vtk")
 if(EXISTS ${VTK_PYTHON_MODULE}/vtkmodules)
   set(_vtk_package "vtkmodules") # Introduced in VTK9 kitware/vtk@2404228 on 2017.12.15
@@ -30,11 +26,7 @@ if(EXISTS ${VTK_PYTHON_MODULE}/vtk.py)
 endif()
 
 # Install external python runtime libraries that we don't link to (fixupbundle won't copy them)
-if(Slicer_VTK_VERSION_MAJOR VERSION_GREATER "7")
-  set(vtk_python_library_subdir "lib/python2.7/site-packages/${_vtk_package}")
-else()
-  set(vtk_python_library_subdir "lib")
-endif()
+set(vtk_python_library_subdir "lib/python2.7/site-packages/${_vtk_package}")
 file(GLOB vtk_python_modules "${VTK_DIR}/${vtk_python_library_subdir}/*Python.so")
 install(FILES ${vtk_python_modules}
         DESTINATION ${Slicer_INSTALL_LIB_DIR}

CMake/SlicerBlockInstallQt.cmake
@@ -3,33 +3,6 @@
 # -------------------------------------------------------------------------
 set(QT_INSTALL_LIB_DIR ${Slicer_INSTALL_LIB_DIR})
 
-if(Slicer_REQUIRED_QT_VERSION VERSION_LESS "5")
-
-  foreach(qtlib ${Slicer_REQUIRED_QT_MODULES})
-    if(QT_${qtlib}_LIBRARY_RELEASE)
-      if(APPLE)
-        install(DIRECTORY "${QT_${qtlib}_LIBRARY_RELEASE}"
-          DESTINATION ${QT_INSTALL_LIB_DIR} COMPONENT Runtime)
-      elseif(UNIX)
-        # Install .so and versioned .so.x.y
-        get_filename_component(QT_LIB_DIR_tmp ${QT_${qtlib}_LIBRARY_RELEASE} PATH)
-        get_filename_component(QT_LIB_NAME_tmp ${QT_${qtlib}_LIBRARY_RELEASE} NAME)
-        install(DIRECTORY ${QT_LIB_DIR_tmp}/
-          DESTINATION ${QT_INSTALL_LIB_DIR} COMPONENT Runtime
-          FILES_MATCHING PATTERN "${QT_LIB_NAME_tmp}*"
-          PATTERN "${QT_LIB_NAME_tmp}*.debug" EXCLUDE)
-      elseif(WIN32)
-        get_filename_component(QT_DLL_PATH_tmp ${QT_QMAKE_EXECUTABLE} PATH)
-        if(EXISTS "${QT_DLL_PATH_tmp}/${qtlib}4.dll")
-          install(FILES ${QT_DLL_PATH_tmp}/${qtlib}4.dll
-            DESTINATION bin COMPONENT Runtime)
-        endif()
-      endif()
-    endif()
-  endforeach()
-
-else()
-
   list(APPEND QT_LIBRARIES
     "Qt5::Gui"
     )
@@ -194,4 +167,3 @@ else()
       RENAME designer-real${CMAKE_EXECUTABLE_SUFFIX}
       )
   endif()
-endif()

CMake/SlicerCPack.cmake
@@ -36,11 +36,6 @@ if(NOT Slicer_USE_SYSTEM_QT)
     imageformats
     sqldrivers
     )
-  if(Slicer_REQUIRED_QT_VERSION VERSION_LESS "5")
-    list(APPEND SlicerBlockInstallQtPlugins_subdirectories
-      designer:qwebview
-      )
-  else()
     list(APPEND SlicerBlockInstallQtPlugins_subdirectories
       designer:webengineview
       )
@@ -58,7 +53,6 @@ if(NOT Slicer_USE_SYSTEM_QT)
         platforms:windows
         )
     endif()
-  endif()
   include(${Slicer_CMAKE_DIR}/SlicerBlockInstallQtPlugins.cmake)
 endif()
 
@@ -67,7 +61,7 @@ if(Slicer_BUILD_DICOM_SUPPORT AND NOT Slicer_USE_SYSTEM_DCMTK)
 endif()
 
 # Install Qt designer launcher
-if(Slicer_BUILD_QT_DESIGNER_PLUGINS AND ${Slicer_REQUIRED_QT_VERSION} VERSION_GREATER_EQUAL 5)
+if(Slicer_BUILD_QT_DESIGNER_PLUGINS)
   set(executablename "SlicerDesigner")
   set(build_designer_executable "${QT_BINARY_DIR}/designer${CMAKE_EXECUTABLE_SUFFIX}")
   if(APPLE)
@@ -182,12 +176,9 @@ else()
   set(VTK_LIBRARY_DIRS "${VTK_DIR}/lib")
 
   # Get Qt root directory
-  set(qt_root_dir "")
-  if(Slicer_REQUIRED_QT_VERSION VERSION_GREATER "4")
-    get_property(_filepath TARGET "Qt5::Core" PROPERTY LOCATION_RELEASE)
-    get_filename_component(_dir ${_filepath} PATH)
-    set(qt_root_dir "${_dir}/..")
-  endif()
+  get_property(_filepath TARGET "Qt5::Core" PROPERTY LOCATION_RELEASE)
+  get_filename_component(_dir ${_filepath} PATH)
+  set(qt_root_dir "${_dir}/..")
 
   #------------------------------------------------------------------------------
   # <ExtensionName>_FIXUP_BUNDLE_LIBRARY_DIRECTORIES

CMake/SlicerDashboardScript.TEMPLATE.cmake
@@ -35,11 +35,7 @@ dashboard_set(Slicer_BUILD_CLI    ON)
 dashboard_set(Slicer_USE_PYTHONQT ON)
 
 dashboard_set(QT_VERSION          "5.10.0")
-if(QT_VERSION VERSION_LESS 5)
-  dashboard_set(QT_QMAKE_EXECUTABLE "${DASHBOARDS_DIR}/Support/qt/qt-everywhere-opensource-build-${QT_VERSION}/bin/qmake")
-else()
-  dashboard_set(Qt5_DIR             "${DASHBOARDS_DIR}/Support/Qt${QT_VERSION}/${QT_VERSION}/gcc_64/lib/cmake/Qt5")
-endif()
+dashboard_set(Qt5_DIR             "${DASHBOARDS_DIR}/Support/Qt${QT_VERSION}/${QT_VERSION}/gcc_64/lib/cmake/Qt5")
 
 #   Source directory : <DASHBOARDS_DIR>/<Slicer_DASHBOARD_SUBDIR>/<Slicer_DIRECTORY_BASENAME>-<Slicer_DIRECTORY_IDENTIFIER>
 #   Build directory  : <DASHBOARDS_DIR>/<Slicer_DASHBOARD_SUBDIR>/<Slicer_DIRECTORY_BASENAME>-<Slicer_DIRECTORY_IDENTIFIER>-build

commit 01efe9f6b00a08edfbb3878bea9f6cd4d0bce51c

COMP: Fix configuration on macOS
This commit fixes a regression introduced in r28000 (COMP: Update build
system removing support for C++98/Qt4/VTK7).

Following commit r28000 the ivar "required_deployment_target" was never
set because the module SlicerInitializeOSXVariables CMake  module is
included from the top-level CMakeLists.txt before the "project()" statement
and in that case Slicer_SOURCE_DIR was not defined too.

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28009 3bd1e089-480b-0410-8dfb-8563597acbee

CMake/SlicerInitializeOSXVariables.cmake
@@ -124,12 +124,11 @@ if(APPLE)
     endif()
   endif()
 
-  if(DEFINED Slicer_SOURCE_DIR)
-    # Require minimum deployment target of 10.9. In 10.9 libc++ replaces libstdc++
-    # as the default runtime. Requiring this minimum ensures that all libraries
-    # use libc++.
-    set(required_deployment_target "10.9")
-  endif()
+  # Require minimum deployment target of 10.9. In 10.9 libc++ replaces libstdc++
+  # as the default runtime. Requiring this minimum ensures that all libraries
+  # use libc++.
+  set(required_deployment_target "10.9")
+
   if(CMAKE_OSX_DEPLOYMENT_TARGET VERSION_LESS ${required_deployment_target})
     message(FATAL_ERROR "CMAKE_OSX_DEPLOYMENT_TARGET must be ${required_deployment_target} or greater.")
   endif()