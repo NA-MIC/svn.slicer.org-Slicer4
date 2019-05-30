NA-MIC/svn.slicer.org-Slicer4

commit b6b6326dc65939c2a2c1d00d25a3bf416a4a9296

ENH: Update VTK to add support for rendering Qt Widget in OpenVR
List of changes:

$ git shortlog af60e8d3cb..037b1f7950 --no-merges
Ken Martin (2):
      [backport] some opengl related cleanups and features
      [backport] add qwidgets in a vtk rendering with event

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28273 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_VTK.cmake
@@ -122,7 +122,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
 set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "af60e8d3cb3de6259be8c5293eb30584a41a9072")
+  set(_git_tag "037b1f7950f5ba50ae5e61fb7e983ebb7c97e6bb")
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()

commit ad6f6b3e99207d0d24de7e448ee6113ae7e5b7f6

COMP: Update VTK to fix macOS link error related to vtkQWidgetTexture
This commit addresses a regression introduced in r28273 (ENH: Update VTK
to add support for rendering Qt Widget in OpenVR)

Thanks @pieper for reporting the issue.

List of VTK changes:

$ git shortlog 037b1f7950..0eb64c6d36 --no-merges
Jean-Christophe Fillion-Robin (1):
      [regression] COMP: vtkQWidgetTexture: Fix undefined _glPixelStorei symbol link error on macOS

git-svn-id: http://svn.slicer.org/Slicer4/trunk@28274 3bd1e089-480b-0410-8dfb-8563597acbee

SuperBuild/External_VTK.cmake
@@ -122,7 +122,7 @@ if((NOT DEFINED VTK_DIR OR NOT DEFINED VTK_SOURCE_DIR) AND NOT Slicer_USE_SYSTEM
 
 set(_git_tag)
 if("${Slicer_VTK_VERSION_MAJOR}" STREQUAL "8")
-  set(_git_tag "037b1f7950f5ba50ae5e61fb7e983ebb7c97e6bb")
+  set(_git_tag "0eb64c6d36d3c9f0fdfb11071b0cda702103c0a0")
 else()
   message(FATAL_ERROR "error: Unsupported Slicer_VTK_VERSION_MAJOR: ${Slicer_VTK_VERSION_MAJOR}")
 endif()